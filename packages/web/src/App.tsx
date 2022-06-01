bbd3451c36cc1ec079682766b0cc12b823cfc2c4
Alexander Mikhalchenko <alex@xfuturum.com>
2022-06-02 02:43:41 +0300

    Eslint

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [shift] = React.useState(0);
+  useEffect(() => {
+
+
+
+    console.error(shift);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and sdfffave to reload.
         </p>
+        {[1, 2, 3, 4, 5].map((x) => (
+          <p>
+            Edit <code>src/App.tsx</code> and save ffto reload.
+          </p>
+        ))}
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          Learn Reactdf
         </a>
       </header>
     </div>
