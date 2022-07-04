918f7ae855e3d1e4bef37d6ee20fdf1e4bf020e9
Christophe Wagner <chris.wagn22@gmail.com>
2022-07-04 14:57:54 +0700

    ft(Header): Add deposit menu

--- packages/web/src/components/Header/Header.tsx
+++ packages/web/src/components/Header/Header.tsx
@@ -1,14 +1,33 @@
-import { Button } from '@mui/material';
-import { IconButton, BrandImage, UserIcon } from 'zignaly-ui';
+import {
+  IconButton,
+  BrandImage,
+  UserIcon,
+  Select,
+  WalletIcon,
+  TextButton,
+} from 'zignaly-ui';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import useAuthenticate /*, { useLogout }*/ from '../../hooks/useAuthenticate';
 import useCurrentUser from '../../hooks/useCurrentUser';
 import UserBalanceListener from './UserBalanceListener';
-import { Header as ZIGHeader, ZigsBalance } from 'zignaly-ui';
+import { Header as ZIGHeader, ZigsBalance, Button } from 'zignaly-ui';
 import Navigation from './Navigation';
 import useBalance from '../../hooks/useBalance';
 import { ethers } from 'ethers';
+import { Box } from '@mui/system';
+import { styled } from '@mui/material/styles';
+
+const DepositSelect = styled(Select)`
+  width: auto;
+
+  > div {
+    > span {
+      color: ${({ theme }) => theme.highlighted};
+    }
+    padding: 0 !important;
+  }
+`;
 
 const Header: React.FC = () => {
   const { t } = useTranslation('global');
@@ -54,22 +73,45 @@ const Header: React.FC = () => {
           !loading &&
             (currentUser?.id ? (
               <React.Fragment key={'balance'}>
+                <Button
+                  variant='secondary'
+                  size='small'
+                  caption={t('insert-code')}
+                  // color='highlighted'
+                />
                 <ZigsBalance
                   balance={ethers.utils.parseEther(balance.toString())}
                 />
                 <UserBalanceListener />
+                <DepositSelect
+                  placeholder={
+                    <Box display='flex' gap='8px' marginLeft='10px'>
+                      <WalletIcon />
+                      {t('deposit')}
+                    </Box>
+                  }
+                  options={[
+                    {
+                      index: 0,
+                      caption: t('deposit'),
+                    },
+                    {
+                      index: 1,
+                      caption: t('disconnect'),
+                    },
+                  ]}
+                  onChange={() => {}}
+                />
               </React.Fragment>
             ) : (
-              <Button variant={'text'} onClick={authenticate}>
-                {t('log-in')}
-              </Button>
+              <TextButton onClick={authenticate} caption={t('log-in')} />
             )),
           <IconButton
             key={'user'}
             variant={'flat'}
             // @ts-ignore
             icon={<UserIcon color='#65647E' />}
-            renderDropDown={<div>DropDown Container</div>}
+            renderDropDown={<div>{t('settings')}</div>}
             dropDownOptions={{
               alignment: 'right',
               position: 'static',
