4ded1b70e43624a9989026d6eaac75c1f06292a4
Alexander Mikhalchenko <alex@xfuturum.com>
2022-07-15 11:29:39 +0300

    Started adding tests for the bid flow

---
+++ packages/server/src/entities/auctions/auction.test.ts
@@ -0,0 +1,34 @@
+import '../..';
+import {
+  createAlice,
+  createAuction,
+  giveMoney,
+  makeBid,
+  wipeOut,
+} from '../../util/test-utils';
+
+describe('Auctions', () => {
+  beforeAll(async () => {
+    // wait for the main app to be initialized
+    await new Promise((r) => setTimeout(r, 2000));
+  });
+
+  beforeEach(wipeOut);
+
+  it('Should not let bid without money', async () => {
+    const [, aliceToken] = await createAlice();
+    const auction = await createAuction();
+    const { body } = await makeBid(auction, aliceToken);
+    expect(body.errors[0].message).toBe('Insufficient funds');
+  });
+
+  it('Should withdraw money after makign bids', async () => {
+    const [alice, aliceToken] = await createAlice();
+    const auction = await createAuction();
+    await giveMoney(alice, 300); // fisting is three hundred bucks
+    const { body } = await makeBid(auction, aliceToken);
+    expect(body.data.bid.userBid.value).toBe('100');
+    const { body: body2 } = await makeBid(auction, aliceToken);
+    expect(body2.data.bid.userBid.value).toBe('102');
+  });
+});
--- packages/server/src/util/test-utils.ts
+++ packages/server/src/util/test-utils.ts
@@ -2,8 +2,55 @@ import supertest from 'supertest';
 import { User } from '../entities/users/model';
 import app from '../index';
 import { signJwtToken } from '../entities/users/util';
+import {
+  Auction,
+  AuctionBasketItem,
+  AuctionBid,
+} from '../entities/auctions/model';
+import { AuctionStatus, AuctionType } from '@zigraffle/shared/types';
+import { Transaction, TransactionType } from '../entities/transactions/model';
+import { isTest } from '../../config';
 
 const request = supertest(app);
+
+export async function createAuction(): Promise<Auction> {
+  return await Auction.create({
+    title: 'Test auction',
+    description: 'Test auction',
+    monetaryValue: '$100500',
+    startingBid: '100',
+    bidStep: '2',
+    status: AuctionStatus.Active,
+    basketItems: [],
+  });
+}
+
+export async function getAuctions(token: string): Promise<AuctionType[]> {
+  return makeRequest(AUCTIONS_QUERY, token);
+}
+
+export async function makeBid(auction: Auction, token: string): Promise<any> {
+  return makeRequest(
+    `
+   mutation {
+    bid(id: "${auction.id}") {
+      bids {
+        value
+        user {
+          id
+          username
+        }
+      }
+
+      userBid {
+        id
+        value
+      }
+    }
+  }`,
+    token,
+  );
+}
 
 export async function createAlice(): Promise<[User, string]> {
   const user = await User.create({
@@ -12,6 +59,17 @@ export async function createAlice(): Promise<[User, string]> {
     onboardingCompletedAt: Date.now(),
   });
   return [user, await signJwtToken(user)];
+}
+
+export async function giveMoney(user: User, money: number | string) {
+  await Transaction.create({
+    userId: user.id,
+    value: money.toString(),
+    block: 1,
+    auctionId: null,
+    txHash: 'privet' + Math.random(),
+    type: TransactionType.Deposit,
+  });
 }
 
 export async function createBob(): Promise<[User, string]> {
@@ -41,3 +99,53 @@ export const BALANCE_QUERY = `
     }
   }
 `;
+
+export const AUCTIONS_QUERY = `
+  query {
+    auctions {
+      id
+      title
+      createdAt
+      expiresAt
+      status
+      minimalBid
+      website
+      twitter
+      telegram
+      discord
+      bidFee
+      description
+      imageUrl
+      startingBid
+      basketItems {
+        ticker
+        amount
+      }
+      monetaryValue
+      bids {
+        id
+        position
+        value
+        user {
+          id
+          username
+        }
+      }
+
+      userBid {
+        id
+        value
+      }
+    }
+  }
+`;
+
+export async function wipeOut() {
+  if (isTest) {
+    // pls do not run it in prod lol
+    await AuctionBid.destroy({ where: {} });
+    await AuctionBasketItem.destroy({ where: {} });
+    await Auction.destroy({ where: {} });
+    await User.destroy({ where: {} });
+  }
+}
