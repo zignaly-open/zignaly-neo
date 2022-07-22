b64420ac38170c114840ca1d3003fb57a178e798
VetleFell <vetle.stubberud@gmail.com>
2022-07-22 19:13:35 +0100

    refactor: howItWorks closable

--- packages/web/src/components/Auctions/HowItWorks/index.tsx
+++ packages/web/src/components/Auctions/HowItWorks/index.tsx
@@ -1,9 +1,10 @@
 import { Box } from '@mui/system';
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography } from 'zignaly-ui';
-import { BulletPointItem, HowItWorksList } from './styles';
+import { BulletPointItem, ClickAbleBox, HowItWorksList } from './styles';
 
 const HowItWorks = () => {
+  const [closed, setClosed] = useState(true);
   return (
     <Box
       width='65%'
@@ -11,6 +12,7 @@ const HowItWorks = () => {
       marginBottom='20px'
       display='flex'
       flexDirection='column'
+      alignItems='center'
     >
       <Typography variant='body1' weight='regular' color='neutral100'>
         With the ‘ZigRaffle’ we are changing how the raffles on zignaly.com
@@ -19,48 +21,66 @@ const HowItWorks = () => {
         happening on the “Raffles Marketplace”. Be it the whitelist spots to
         your favorite NFT collection or that basket of tokens going at a deep
         discount, you can put your ZIG to work and place your bids at ZigRaffle.
-        <br />
-        <br />
-        <Box>
-          <Typography variant='h3' weight='demibold' color='neutral000'>
-            How It Works?
-          </Typography>
-        </Box>
-        <HowItWorksList>
-          <BulletPointItem>
-            Connect your wallet directly to the ZigRaffle site. We will ask for
-            your Discord usernames to share instructions with the auction
-            winners.
-          </BulletPointItem>
-          <br />
-          <BulletPointItem>
-            Deposit ZIG to your wallet and start placing bids on your favorite
-            auctions.
-          </BulletPointItem>
-          <br />
-          <BulletPointItem>
-            Since our model allows having more than one winner, we will be
-            displaying the names of “current winners” in real time under each
-            auction. As a new bid comes up, the “current winners” list will be
-            updated until the auction is over.
-          </BulletPointItem>
-          <br />
-          <BulletPointItem>
-            Once the auction ends, the winner(s) will pay the “Claim” price for
-            the product, directly from the connected wallet.
-          </BulletPointItem>
-          <br />
-          <BulletPointItem>
-            Users may opt to buy the product directly from the auction using the
-            “Buy now” button.
-          </BulletPointItem>
-          <br />
-          <BulletPointItem>
-            We are extending the range of offerings from NFT whitelist spots to
-            Crypto baskets and other items, moving on.{' '}
-          </BulletPointItem>
-        </HowItWorksList>
       </Typography>
+      {closed && (
+        <ClickAbleBox onClick={() => setClosed(!closed)}>
+          <Typography variant='body1' color='links'>
+            Read More
+          </Typography>
+        </ClickAbleBox>
+      )}
+      {!closed && (
+        <>
+          <br />
+          <br />
+          <Box>
+            <Typography variant='h3' weight='demibold' color='neutral000'>
+              How It Works?
+            </Typography>
+          </Box>
+          <HowItWorksList>
+            <Typography variant='body1' weight='regular' color='neutral100'>
+              <BulletPointItem>
+                Connect your wallet directly to the ZigRaffle site. We will ask
+                for your Discord usernames to share instructions with the
+                auction winners.
+              </BulletPointItem>
+              <br />
+              <BulletPointItem>
+                Deposit ZIG to your wallet and start placing bids on your
+                favorite auctions.
+              </BulletPointItem>
+              <br />
+              <BulletPointItem>
+                Since our model allows having more than one winner, we will be
+                displaying the names of “current winners” in real time under
+                each auction. As a new bid comes up, the “current winners” list
+                will be updated until the auction is over.
+              </BulletPointItem>
+              <br />
+              <BulletPointItem>
+                Once the auction ends, the winner(s) will pay the “Claim” price
+                for the product, directly from the connected wallet.
+              </BulletPointItem>
+              <br />
+              <BulletPointItem>
+                Users may opt to buy the product directly from the auction using
+                the “Buy now” button.
+              </BulletPointItem>
+              <br />
+              <BulletPointItem>
+                We are extending the range of offerings from NFT whitelist spots
+                to Crypto baskets and other items, moving on.{' '}
+              </BulletPointItem>
+            </Typography>
+          </HowItWorksList>
+          <ClickAbleBox paddingTop='20px' onClick={() => setClosed(!closed)}>
+            <Typography variant='body1' color='links'>
+              Read Less
+            </Typography>
+          </ClickAbleBox>
+        </>
+      )}
     </Box>
   );
 };
--- packages/web/src/components/Auctions/HowItWorks/styles.ts
+++ packages/web/src/components/Auctions/HowItWorks/styles.ts
@@ -1,7 +1,12 @@
 import styled from '@emotion/styled';
+import { Box } from '@mui/system';
 
 export const BulletPointItem = styled.li`
   margin: 3px;
 `;
 
 export const HowItWorksList = styled.ul``;
+
+export const ClickAbleBox = styled(Box)`
+  cursor: pointer;
+`;
