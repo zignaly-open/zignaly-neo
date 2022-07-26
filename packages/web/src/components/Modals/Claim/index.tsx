adfe2ef35d84ad37a989cf91dc3e8c8b6af40eaf
Christophe Wagner <chris.wagn22@gmail.com>
2022-07-26 16:34:41 +0700

    ft: Add claim modal

--- packages/web/src/components/Modals/Claim/index.tsx
+++ packages/web/src/components/Modals/Claim/index.tsx
@@ -1,22 +1,97 @@
-import React from 'react';
+import { useMutation } from '@apollo/client';
+import { Box } from '@mui/material';
+import { useEthers } from '@usedapp/core';
+import useCurrentUser from 'hooks/useCurrentUser';
+import { CLAIM } from 'queries/auctions';
+import React, { FormEvent, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Typography } from 'zignaly-ui';
+import { Button, ErrorMessage, InputText, Typography } from 'zignaly-ui';
 import DialogContainer from '../DialogContainer';
+import { Form } from './styles';
 import { ClaimModalProps } from './types';
 
-const ClaimModal = ({ ...props }: ClaimModalProps) => {
-  const { t } = useTranslation('auction');
+const ClaimModal = ({ auction, ...props }: ClaimModalProps) => {
+  const { t } = useTranslation(['claim', 'user-settings', 'global']);
+  const {
+    user: { discordName },
+  } = useCurrentUser();
+  const { account } = useEthers();
+  const [errorMessage, setErrorMessage] = useState('');
+  const [claim, { loading }] = useMutation(CLAIM);
+
+  const submit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await claim({
+        variables: { id: auction.id },
+      });
+    } catch (_) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
   return (
     <DialogContainer title={t('claim-instructions')} {...props}>
-      <Typography marginTop={18} color='neutral200'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Typography>
+      <Typography color='neutral200'>{t('congratulations')}</Typography>
+      <br />
+      <br />
+      <Typography color='neutral200'>{t('verify')}</Typography>
+      <Form onSubmit={submit}>
+        <Typography color='neutral200'>{t('connected-wallet')}</Typography>
+        <InputText
+          value={account}
+          // onChange={field.onChange}
+          placeholder={t('connected-wallet')}
+          // error={errors.username?.message}
+          disabled={true}
+          minHeight={1}
+        />
+        <Typography color='neutral200'>{t('discord-user')}</Typography>
+        <Box mt='-8px'>
+          <Typography color='neutral400' variant='h4'>
+            {t('change')}
+          </Typography>
+        </Box>
+        <InputText
+          value={discordName}
+          // onChange={field.onChange}
+          placeholder={t('please-enter-discord-user', {
+            ns: 'user-settings',
+          })}
+          // error={errors.username?.message}
+          disabled={true}
+          minHeight={1}
+        />
+        <Box
+          gap='12px'
+          display='flex'
+          justifyContent='center'
+          sx={{ flexDirection: { xs: 'column', sm: 'row' } }}
+          mt='40px'
+        >
+          <Button
+            caption={t('cancel', {
+              ns: 'global',
+            })}
+            variant='secondary'
+            size='large'
+            onClick={(e) => props.onClose(e, 'escapeKeyDown')}
+            type='button'
+          />
+          <Button
+            type='submit'
+            loading={loading}
+            caption={t('claim')}
+            size='large'
+            disabled={!discordName}
+          />
+        </Box>
+        {errorMessage && (
+          <Box mt='9px'>
+            <ErrorMessage text={errorMessage} />
+          </Box>
+        )}
+      </Form>
     </DialogContainer>
   );
 };
