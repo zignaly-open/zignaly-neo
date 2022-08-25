53760e3a49f4328585b66c6c2341e8982ff71ebd
German D. Schneck <german.schneck@zignaly.com>
2022-08-25 11:36:20 +0200

    feat(my-balances-flow): remove unused typos, because the data is dynamic.

--- packages/ps2/src/features/balances/types.ts
+++
@@ -1,7 +0,0 @@
-export type BalancesResponse = {
-  token: string;
-  ask2FA: boolean;
-  isUnknownDevice: boolean;
-  disabled: boolean;
-  emailUnconfirmed: boolean;
-};
--- packages/ps2/src/features/coins/use.ts
+++
@@ -1,8 +0,0 @@
-// Dependencies
-import { useSelector } from 'react-redux';
-import { RootState } from '../store';
-
-export function useSelectCoins(): object[] {
-  const coins = useSelector((state: RootState) => state.coins);
-  return coins;
-}
