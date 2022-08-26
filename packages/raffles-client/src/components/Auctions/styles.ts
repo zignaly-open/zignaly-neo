aaadb28675a420ce0f8fa893e9047d8232604ec8
Jeniffer Hurtado Coronado <62664863+Jennimar17@users.noreply.github.com>
2022-08-26 10:19:49 -0300

    style: made the howitworks responsive (#136)
    * style(index-page): make some style changes and responsive view
    
    * style(index-page): finish responsive view
    
    Co-authored-by: VetleFell <vetle.stubberud@gmail.com>

--- packages/raffles-client/src/components/Auctions/styles.ts
+++ packages/raffles-client/src/components/Auctions/styles.ts
@@ -1,8 +1,14 @@
 import styled from '@emotion/styled';
+import { Typography } from '@zignaly-open/ui';
 
 export const AuctionContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const HomeTitle = styled(Typography)`
+  position: relative;
+  top: 70px;
 `;
