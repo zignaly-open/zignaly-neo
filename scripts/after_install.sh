dc4b873d24cf0082744705948f197962bbbf210a
Christophe Wagner <chris.wagn22@gmail.com>
2022-09-06 22:13:26 +0200

    Cleanup

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,4 +5,3 @@ DEPLOYMENTPATH="{directory}"
 cd $DEPLOYMENTPATH
 sudo yarn --scope=@zignaly-open/raffles-client --include-dependencies
 sudo yarn lerna run build --scope=@zignaly-open/raffles-client --include-dependencies
-# sudo pm2 restart zigraffle-api
