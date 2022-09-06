7976a439e26d142071dc080b74e41150766c06a9
Christophe Wagner <chris.wagn22@gmail.com>
2022-09-07 01:40:29 +0200

    Fix after_install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,5 +6,6 @@ cd $DEPLOYMENTPATH/deploy
 sudo yarn --scope=@zignaly-open/raffles-client --include-dependencies
 sudo yarn lerna run build --scope=@zignaly-open/raffles-client --include-dependencies
 
+cd ..
 sudo cp -r $DEPLOYMENTPATH/deploy $DEPLOYMENTPATH/temp
 sudo mv -f $DEPLOYMENTPATH/temp $DEPLOYMENTPATH/release
