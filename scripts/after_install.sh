3cdbc5e8da7a90b4449c293ea962ca712d0108bf
Christophe Wagner <chris.wagn22@gmail.com>
2022-09-07 13:29:44 +0200

    Clean install script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,7 +6,9 @@ cd $DEPLOYMENTPATH/deploy
 sudo yarn --scope=@zignaly-open/raffles-client --include-dependencies
 sudo yarn run build --scope=@zignaly-open/raffles-client --include-dependencies
 
+# Copy build to keep a cache
 sudo cp -r $DEPLOYMENTPATH/deploy $DEPLOYMENTPATH/temp
+
 # Replace release
 sudo rm -rf $DEPLOYMENTPATH/release
 sudo mv $DEPLOYMENTPATH/temp $DEPLOYMENTPATH/release
