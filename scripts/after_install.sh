df10b311f241c83bedb40d1a6359431edfed7b77
Christophe Wagner <chris.wagn22@gmail.com>
2022-09-07 15:55:09 +0200

    Move deploy to temp

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,11 +2,12 @@
 
 DEPLOYMENTPATH="/zignaly/{directory}"
 
-cd $DEPLOYMENTPATH/deploy
+sudo rm -rf $DEPLOYMENTPATH/temp
+sudo mv $DEPLOYMENTPATH/deploy $DEPLOYMENTPATH/temp
+cd $DEPLOYMENTPATH/temp
 sudo yarn --scope=@zignaly-open/raffles-client --include-dependencies
 sudo yarn run build --scope=@zignaly-open/raffles-client --include-dependencies
 
-sudo cp -r $DEPLOYMENTPATH/deploy $DEPLOYMENTPATH/temp
 # Replace release
 sudo rm -rf $DEPLOYMENTPATH/release
 sudo mv $DEPLOYMENTPATH/temp $DEPLOYMENTPATH/release
