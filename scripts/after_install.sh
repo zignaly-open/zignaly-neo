8f15a078e45417ea38b45f2ac3bc1776ec51b230
Christophe Wagner <chris.wagn22@gmail.com>
2022-09-08 23:23:18 +0200

    Copy client

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,17 +2,15 @@
 
 DEPLOYMENTPATH="/zignaly/{directory}"
 
-# Move deploy folder to avoid it from being cleared out by codedeploy when deploying to another env.
-# mv $DEPLOYMENTPATH/deploy $DEPLOYMENTPATH/temp
-
-# Build
+# Build client
 cd $DEPLOYMENTPATH/deploy
 yarn --scope=@zignaly-open/raffles-client --include-dependencies
 yarn run build --scope=@zignaly-open/raffles-client --include-dependencies
 
-# Replace release
-# rm -rf $DEPLOYMENTPATH/release
-# mv $DEPLOYMENTPATH/temp $DEPLOYMENTPATH/release
+# Copy client
+cd ..
+sudo rm -rf client
+sudo mv deploy/packages/raffles-client/build client
 
-# Restart pm2
-# pm2 restart zigbids-api-{directory}
+# Restart server
+pm2 restart zigbids-api-{directory}
