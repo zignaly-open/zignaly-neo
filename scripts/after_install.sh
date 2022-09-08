02646297df971f3f94141a8f8ab18629a3075c96
Christophe Wagner <chris.wagn22@gmail.com>
2022-09-08 22:56:18 +0200

    Codedeploy caching

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,13 +2,17 @@
 
 DEPLOYMENTPATH="/zignaly/{directory}"
 
-sudo rm -rf $DEPLOYMENTPATH/temp
-sudo mv $DEPLOYMENTPATH/deploy $DEPLOYMENTPATH/temp
-cd $DEPLOYMENTPATH/temp
+# Move deploy folder to avoid it from being cleared out by codedeploy when deploying to another env.
+# mv $DEPLOYMENTPATH/deploy $DEPLOYMENTPATH/temp
+
 # Build
-sudo yarn --scope=@zignaly-open/raffles-client --include-dependencies
-sudo yarn run build --scope=@zignaly-open/raffles-client --include-dependencies
+# cd $DEPLOYMENTPATH/temp
+yarn --scope=@zignaly-open/raffles-client --include-dependencies
+yarn run build --scope=@zignaly-open/raffles-client --include-dependencies
 
 # Replace release
-sudo rm -rf $DEPLOYMENTPATH/release
-sudo mv $DEPLOYMENTPATH/temp $DEPLOYMENTPATH/release
+# rm -rf $DEPLOYMENTPATH/release
+# mv $DEPLOYMENTPATH/temp $DEPLOYMENTPATH/release
+
+# Restart pm2
+# pm2 restart zigbids-api-{directory}
