fddac8c3a0c8e2bfecb69fa28ff85ed42edbf43f
Christophe Wagner <chris.wagn22@gmail.com>
2022-09-08 23:54:27 +0200

    Final deploy config

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,14 +3,14 @@
 DEPLOYMENTPATH="/zignaly/{directory}"
 
 # Build client
-# cd $DEPLOYMENTPATH/deploy
-# yarn --scope=@zignaly-open/raffles-client --include-dependencies
-# yarn run build --scope=@zignaly-open/raffles-client --include-dependencies
+cd $DEPLOYMENTPATH/deploy
+yarn --scope=@zignaly-open/raffles-client --include-dependencies
+yarn run build --scope=@zignaly-open/raffles-client --include-dependencies
 
-# # Copy client
-# cd ..
-# sudo rm -rf client
-# sudo mv deploy/packages/raffles-client/build client
+# Copy client
+cd ..
+sudo rm -rf client
+sudo mv deploy/packages/raffles-client/build client
 
-# # Restart server
-# pm2 restart zigbids-api-{directory}
+# Restart server
+pm2 restart zigbids-api-{directory}
