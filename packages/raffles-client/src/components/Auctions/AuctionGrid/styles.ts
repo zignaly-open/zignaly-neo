615e1702c80d6e17b84eb45ba76f48e05f1d2634
Christophe Wagner <chris.wagn22@gmail.com>
2022-09-09 13:45:38 +0200

    Align filters and text

---
+++ packages/raffles-client/src/components/Auctions/AuctionGrid/styles.ts
@@ -0,0 +1,38 @@
+import { Box, styled } from '@mui/material';
+import { Select, Typography } from '@zignaly-open/ui';
+
+export const LayoutContainer = styled(Box)`
+  max-width: 1280px;
+  margin: 30px auto;
+  padding: 0 8px;
+`;
+
+export const FiltersContainer = styled('div')`
+  margin: 40px 0 18px;
+  display: flex;
+  position: relative;
+
+  ${({ theme }) => theme.breakpoints.down('md')} {
+    flex-direction: column-reverse;
+  }
+`;
+
+export const ProjectsTypography = styled(Typography)`
+  position: absolute;
+  bottom: 27px;
+  font-size: 18px !important;
+
+  ${({ theme }) => theme.breakpoints.down('md')} {
+    position: static;
+    margin-top: 14px !important;
+    text-align: center;
+  }
+`;
+
+export const StyledSelect = styled(Select)`
+  min-width: 150px;
+
+  > div {
+    text-align: left;
+  }
+`;
