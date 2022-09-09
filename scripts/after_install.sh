0f5b5a9b0f4430f1a126d046829dfde55079c16b
Christophe Wagner <chris.wagn22@gmail.com>
2022-09-10 01:15:24 +0200

    Fix install path

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,12 +3,12 @@
 DEPLOYMENTPATH="/zignaly/{directory}"
 
 # Install server
-cd $DEPLOYMENTPATH/deploy/raffles-server
+cd $DEPLOYMENTPATH/deploy/packages/raffles-server
 yarn
 
 # Move client
 sudo rm -rf $DEPLOYMENTPATH/client
-sudo mv $DEPLOYMENTPATH/deploy/raffles-client $DEPLOYMENTPATH/client
+sudo mv $DEPLOYMENTPATH/deploy/packages/raffles-client $DEPLOYMENTPATH/client
 
 # Restart server
 pm2 restart zigbids-api-{directory}
