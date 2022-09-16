454dbd2354af0049fbdbcad3bf3b7121caf5c1fb
Jeniffer Hurtado Coronado <jennimar17@gmail.com>
2022-09-15 22:52:29 -0300

    WIP-refactor(how-it-wor): add new icons

--- packages/raffles-client/src/components/Auctions/Auctions.tsx
+++ packages/raffles-client/src/components/Auctions/Auctions.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import AuctionGrid from './AuctionGrid/AuctionGrid';
-import { useTranslation } from 'react-i18next';
-import { AuctionContainer, HomeTitle } from './styles';
+import { AuctionContainer } from './styles';
 import HowItWorks from './HowItWorks';
 
 function Auctions() {
-  const { t } = useTranslation('global');
-
   return (
     <>
       <AuctionContainer>
-        <HomeTitle variant='h1' color='neutral000'>
-          {t('auction')}
-        </HomeTitle>
         <HowItWorks />
       </AuctionContainer>
       <AuctionGrid />
