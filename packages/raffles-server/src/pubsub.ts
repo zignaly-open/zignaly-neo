cdca3e0b9cf116443a912711027723724916ea5a
Christophe Wagner <chris.wagn22@gmail.com>
2022-09-26 16:34:41 +0200

    Comment

--- packages/raffles-server/src/pubsub.ts
+++ packages/raffles-server/src/pubsub.ts
@@ -5,6 +5,7 @@ import { BALANCE_CHANGED } from './entities/users/constants';
 
 const pubsub = new PostgresPubSub({
   connectionString: postgresUrl,
+  // Important: Add all subscriptions triggers here
   topics: [AUCTION_UPDATED, BALANCE_CHANGED],
 });
 pubsub.connect();
