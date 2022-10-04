a88790a0e5dd3364649fc14e970c2aba4216f709
Christophe Wagner <chris.wagn22@gmail.com>
2022-10-04 13:20:15 +0200

    Temporary revert prod deploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -9,5 +9,10 @@ sudo chown -R admin:admin $DEPLOYMENTPATH/deploy
 cd $DEPLOYMENTPATH/deploy/packages/raffles-server
 yarn
 
+# Move client build
+mv $DEPLOYMENTPATH/deploy/packages/raffles-client/build $DEPLOYMENTPATH/releases/$DEPLOYMENT_ID
+# Set new client release symlink 
+ln -nfs --relative $DEPLOYMENTPATH/releases/$DEPLOYMENT_ID $DEPLOYMENTPATH/client
+
 # Restart server
 pm2 restart zigbids-api-{directory}
