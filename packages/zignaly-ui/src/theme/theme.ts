18abcf651abd17ebe385b8273a2fab51f2480ea9
Alexander Mikhalchenko <alex@xfuturum.com>
2022-10-08 00:42:41 +0300

    chore: removed unused keys from the theme

--- packages/zignaly-ui/src/theme/theme.ts
+++ packages/zignaly-ui/src/theme/theme.ts
@@ -19,15 +19,9 @@ interface Theme {
   links: string;
   yellow: string;
   red: string;
-  tertiaryButton: string;
-  formInputBack: string;
-  formSelectBack: string;
   dropDownBackground: string;
   darkSpecial: string;
   almostWhite: string;
-  gradient1: string;
-  gradient2: string;
-  gradient3: string;
   errorToasterBg: string;
   errorToasterBorder: string;
   successToasterBg: string;
