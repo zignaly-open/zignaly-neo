9a5bb04b1cb3c024e9fa1c97c22333e116919278
Alexander Mikhalchenko <alex@xfuturum.com>
2022-10-10 00:47:02 +0300

    chore: ts fixes

--- packages/zignaly-ui/src/components/display/Table/styles.ts
+++ packages/zignaly-ui/src/components/display/Table/styles.ts
@@ -24,12 +24,12 @@ export const IconContainer = styled.div`
   right: 4px;
 `;
 
-export const HeaderRow = styled.div<{ align: "left" | "right" | "center" }>`
+export const HeaderRow = styled.div<{ align?: "left" | "right" | "center" }>`
   position: relative;
   display: flex;
   flex-direction: row;
   justify-content: ${(props) =>
-    ({ left: "flex-start", right: "flex-end", center: "center" }[props.align] || "center")};
+    ({ left: "flex-start", right: "flex-end", center: "center" }[props.align!] || "center")};
   padding-left: 5px;
   padding-right: 5px;
   width: inherit;
