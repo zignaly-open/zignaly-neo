607570d25b45424a548db11e98d5dba30d53f03f
Alexander Mikhalchenko <alex@xfuturum.com>
2022-10-11 22:57:36 +0300

    chore: fixed sigup page title

--- packages/ps2/src/views/Auth/Signup.tsx
+++ packages/ps2/src/views/Auth/Signup.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ComingSoon from '../../components/Stub/ComingSoon';
+import { useTranslation } from 'react-i18next';
+import { useTitle } from 'react-use';
 
 const Signup: React.FC = () => {
+  const { t } = useTranslation('pages');
+  useTitle(t('signup'));
+
   return (
     <>
       <ComingSoon />
