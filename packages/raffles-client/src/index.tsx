7bbaf1aa0f22354099829dbb77448ea8efbfe4d2
Christophe Wagner <chris.wagn22@gmail.com>
2022-10-19 10:54:32 +0200

    Fix push notif

--- packages/raffles-client/src/index.tsx
+++ packages/raffles-client/src/index.tsx
@@ -4,11 +4,21 @@ import './index.css';
 import './util/i18next';
 import EntryPoint from './EntryPoint';
 import reportWebVitals from './reportWebVitals';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
   <React.StrictMode>
     <React.Suspense fallback={null}>
-      <EntryPoint />
+      <HelmetProvider>
+        <Helmet>
+          <script
+            type='text/javascript'
+            src='scripts/firebase-messaging-sw.js'
+            defer
+          ></script>
+        </Helmet>
+        <EntryPoint />
+      </HelmetProvider>
     </React.Suspense>
   </React.StrictMode>,
   document.querySelector('#root') as HTMLElement,
