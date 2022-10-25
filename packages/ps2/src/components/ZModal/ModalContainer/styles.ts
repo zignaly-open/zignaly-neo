b862ede74b2b32bb21316ee8680f94a5060da0ec
Alexander Mikhalchenko <alex@xfuturum.com>
2022-10-26 01:14:04 +0300

    fix: scrollbar style

--- packages/ps2/src/components/ZModal/ModalContainer/styles.ts
+++ packages/ps2/src/components/ZModal/ModalContainer/styles.ts
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components';
-import { styledIf, Typography } from '@zignaly-open/ui';
+import { NiceScrollbar, styledIf, Typography } from '@zignaly-open/ui';
 
 export const Layout = styled.div<{ width: number }>`
   display: flex;
@@ -34,6 +34,9 @@ export const Body = styled.div`
   color: ${({ theme }) => theme.neutral200};
   max-height: calc(100vh - 150px);
   overflow-y: auto;
+
+  ${NiceScrollbar};
+  
   overflow-x: visible;
   margin-left: -40px;
   margin-right: -40px;
