4f9154369331a422cbea350f90312314f7ce686a
Christophe Wagner <chris.wagn22@gmail.com>
2022-10-31 14:05:57 +0100

    ft: custom transaction type

--- packages/raffles-server/scripts/cybavo-transfer.ts
+++ packages/raffles-server/scripts/cybavo-transfer.ts
@@ -1,3 +1,4 @@
+import { Sequelize } from 'sequelize';
 import { zignalySystemId } from '../config';
 import { internalTransfer } from '../src/cybavo';
 import '../src/db';
@@ -7,15 +8,30 @@ import { TransactionType } from '../src/types';
 (async () => {
   const args = process.argv.slice(2);
   const address = args[0];
-  const user = await User.findOne({ where: { publicAddress: address } });
+  const user = await User.findOne({
+    where: {
+      $and: Sequelize.where(
+        Sequelize.fn('lower', Sequelize.col('publicAddress')),
+        Sequelize.fn('lower', address),
+      ),
+    },
+  });
   if (!user) {
-    throw new Error('user not found');
+    throw new Error('User not found');
   }
+  const type = (args[2] as TransactionType) ?? TransactionType.Deposit;
+
+  const values = Object.values(TransactionType);
+
+  if (!values.includes(type as unknown as TransactionType)) {
+    throw new Error('Transaction Type not correct');
+  }
+
   const res = await internalTransfer(
     zignalySystemId,
     address.toLowerCase(),
     args[1],
-    TransactionType.Deposit,
+    type,
     false,
   );
 
