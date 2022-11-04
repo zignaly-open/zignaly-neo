8a6c07c5cc85bdf477217e9e7a56619a803865ae
Christophe Wagner <chris.wagn22@gmail.com>
2022-11-04 20:11:22 +0100

    fix: position sort

--- packages/raffles-server/src/entities/auctions/service.ts
+++ packages/raffles-server/src/entities/auctions/service.ts
@@ -166,6 +166,7 @@ export const generateService = (user: ContextUser) => ({
         model: AuctionBid,
         as: 'bids',
         include: [User],
+        order: ['position', 'asc'],
       },
     });
     auction.bids.forEach(
