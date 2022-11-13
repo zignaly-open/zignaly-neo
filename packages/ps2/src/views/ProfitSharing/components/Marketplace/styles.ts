77b2c75ce8f002e53704360e14c6be7d48fc4e27
Alexander Mikhalchenko <alex@xfuturum.com>
2022-11-13 04:15:42 +0400

    chore: minor fixes - removing styled-components
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/views/ProfitSharing/components/Marketplace/styles.ts
+++ packages/ps2/src/views/ProfitSharing/components/Marketplace/styles.ts
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
+import { styled } from '@mui/material';
 import { PageContainer } from '@zignaly-open/ui';
 
 export const Layout = styled(PageContainer)`
   padding-top: 120px;
 `;
 
-export const Inline = styled.div`
+export const Inline = styled('div')`
   text-align: left;
   padding: 0 22px;
 `;
