b0c7b8792e1054b81d1fe5111f0deda39ada4c50
Alexander Mikhalchenko <alex@xfuturum.com>
2022-11-15 02:19:57 +0400

    fix: deposit flow fixes
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/views/Dashboard/components/ManageInvestmentModals/forms/DepositForm/styles.ts
+++ packages/ps2/src/views/Dashboard/components/ManageInvestmentModals/forms/DepositForm/styles.ts
@@ -1,10 +1,14 @@
-import { CoinIcon } from '@zignaly-open/ui';
 import { styled } from '@mui/material';
 
 export const Form = styled('form')``;
 
-export const CoinIconWithMargins = styled(CoinIcon)`
-  margin-right: 12px;
+export const CoinIconWrapper = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  & > div {
+    margin-right: 11px;
+  }
 `;
 
 export const FullWidthSelect = styled('div')`
