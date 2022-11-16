7e44293fd91bbc80496c2b6dbe2e3242d6ef26ec
Christophe Wagner <chris.wagn22@gmail.com>
2022-11-16 11:21:24 +0700

    fix: fix tz ref

--- packages/raffles-client/src/components/Header/Header.tsx
+++ packages/raffles-client/src/components/Header/Header.tsx
@@ -72,7 +72,7 @@ const Header = () => {
   useEffect(() => {
     // Avoid making an unnecessary request if user id is loading
     if (!token || currentUser?.id) {
-      triggerTz(href, null, currentUser?.publicAddress);
+      triggerTz(href, currentUser?.publicAddress);
     }
   }, [href, currentUser, token]);
 
