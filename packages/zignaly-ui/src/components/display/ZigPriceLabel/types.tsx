59d77e9997164e599bec0c293cb1d9dad570e3a2
Alexander Mikhalchenko <alex@xfuturum.com>
2022-11-20 23:13:48 +0400

    fix: fixed dollar sign showing incorrectly
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/components/display/ZigPriceLabel/types.tsx
+++ packages/zignaly-ui/src/components/display/ZigPriceLabel/types.tsx
@@ -1,6 +1,9 @@
 import { TypographyTypeMap } from "@mui/material";
 
-type TypographyProps = Pick<TypographyTypeMap["props"], "color" | "variant" | "fontWeight" | "sx">;
+type TypographyProps = Pick<
+  TypographyTypeMap["props"],
+  "color" | "component" | "variant" | "fontWeight" | "sx"
+>;
 
 export type ZigPriceLabelProps = TypographyProps & {
   value: string | number;
