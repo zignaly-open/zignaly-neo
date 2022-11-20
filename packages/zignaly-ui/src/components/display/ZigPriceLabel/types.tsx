df7fb4d9c215f68cfb9b8bbe99e4644553341015
Alexander Mikhalchenko <alex@xfuturum.com>
2022-11-20 22:57:08 +0400

    feat: proper PriceLabel
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

---
+++ packages/zignaly-ui/src/components/display/ZigPriceLabel/types.tsx
@@ -0,0 +1,11 @@
+import { TypographyTypeMap } from "@mui/material";
+
+type TypographyProps = Pick<TypographyTypeMap["props"], "color" | "variant" | "fontWeight" | "sx">;
+
+export type ZigPriceLabelProps = TypographyProps & {
+  value: string | number;
+  coin?: string;
+  precision?: number;
+  usd?: boolean;
+  coinProps?: TypographyProps;
+};
