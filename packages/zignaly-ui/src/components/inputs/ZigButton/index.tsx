aed48f0fa55bf6318549937b7c1ca3d5f07d5b1e
Alexander Mikhalchenko <alex@xfuturum.com>
2022-11-21 17:55:33 +0400

    fix: fixed storybook
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

---
+++ packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@mui/material";
+import { styled } from "@mui/system";
+
+const ZigButton: typeof Button = styled(Button)`` as unknown as typeof Button;
+
+export default ZigButton;
