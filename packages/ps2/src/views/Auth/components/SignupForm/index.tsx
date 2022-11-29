a25a777ec008e63d8782f34015dbf6da394dba29
Christophe Wagner <chris.wagn22@gmail.com>
2022-11-29 18:17:20 +0700

    update ref property

--- packages/ps2/src/views/Auth/components/SignupForm/index.tsx
+++ packages/ps2/src/views/Auth/components/SignupForm/index.tsx
@@ -40,16 +40,16 @@ const SignupForm: React.FC = () => {
   const { state: locationState } = useLocation();
 
   useEffect(() => {
-    const ref = new URLSearchParams(window.location.search).get('invite');
-    if (ref) {
-      Cookies.set('ref', ref);
+    const refCode = new URLSearchParams(window.location.search).get('invite');
+    if (refCode) {
+      Cookies.set('refCode', refCode);
     }
   }, []);
 
   const onSubmit = (payload: LoginPayload) => {
     signup({
       ...payload,
-      ref: Cookies.get('ref'),
+      refCode: Cookies.get('refCode'),
     });
   };
 
