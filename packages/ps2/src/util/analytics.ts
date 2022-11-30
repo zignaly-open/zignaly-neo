9ed7b529bd66eb049ff4d7011a6e39559f80d83c
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-01 00:41:39 +0700

    feat: intercom data

--- packages/ps2/src/util/analytics.ts
+++ packages/ps2/src/util/analytics.ts
@@ -32,8 +32,18 @@ export const trackNewSession = (
 ) => {
   try {
     pushGtmEvent({ event: eventType, ...userData });
-    const { email, userId } = userData;
-    analytics?.identify(userId, { email });
+    const { email, userId, firstName, intercomHash, createdAt } = userData;
+    analytics?.identify(
+      userId,
+      { email, name: firstName, created_at: +createdAt },
+      {
+        integrations: {
+          Intercom: {
+            user_hash: intercomHash,
+          },
+        },
+      },
+    );
     Sentry.setUser({ email, id: userId });
     if (eventType === SessionsTypes.Signup) {
       analytics?.track('newUser', { userId });
