9384f60036fea7e3c5c2dd8ea58eb3c876f9cb73
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-01 22:15:10 +0700

    wip confirmation modal

--- packages/ps2/src/views/Dashboard/components/ManageInvestmentModals/forms/WithdrawConfirmForm/styles.ts
+++ packages/ps2/src/views/Dashboard/components/ManageInvestmentModals/forms/WithdrawConfirmForm/styles.ts
@@ -1,7 +1,7 @@
 import { styled } from '@mui/material';
 
-export const IconContainer = styled.div<{ marginRight: number }>`
-  margin-right: ${(props) => `${props.marginRight}`}px;
+export const IconContainer = styled('div')`
+  margin-right: 36px;
   width: 36px;
   height: 36px;
 `;
