f68cbed1b11a810a681dec9f1c81170a2ffb1f3a
Alexander Mikhalchenko <alex@xfuturum.com>
2022-12-03 22:58:49 +0400

    chore: chart - large
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/components/display/ZigChart/styles.ts
+++ packages/zignaly-ui/src/components/display/ZigChart/styles.ts
@@ -1,32 +1,7 @@
-import { css, styled } from "@mui/system";
-import { styledIf } from "utils/styled";
-import { ChartVariations } from "./types";
+import { styled } from "@mui/system";
 
-export const WideWrapper = styled("div")<{ wide: boolean }>`
-  ${(props) =>
-    props.wide &&
-    css`
-      width: 100%;
-    `}
-`;
-
-export const Layout = styled("div")<{ variant: keyof typeof ChartVariations }>`
-  ${(props) => `
-    ${styledIf(
-      props.variant === "large",
-      `
-       height: 400px;
-       width: auto;
-       `,
-    )}
-    ${styledIf(
-      props.variant === "small",
-      `
-      height: 100px;
-      width: auto;
-        `,
-    )}
-    `}
+export const ChartLayoutLarge = styled("div")`
+  width: 100%;
 `;
 
 export const ChartLayoutMini = styled("div")<{ height?: number }>`
