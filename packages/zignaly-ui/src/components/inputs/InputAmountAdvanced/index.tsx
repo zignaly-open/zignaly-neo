7ef40f5c294fe0d6ef5db63b91089018e067df17
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-05 12:43:03 +0700

    feat: use ZigPriceLabel in InputAmount

--- packages/zignaly-ui/src/components/inputs/InputAmountAdvanced/index.tsx
+++ packages/zignaly-ui/src/components/inputs/InputAmountAdvanced/index.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback } from "react";
-import NumberFormat from "react-number-format";
 import { useController } from "react-hook-form";
 import {
   InputContainer,
@@ -21,8 +20,8 @@ import Typography from "components/display/Typography";
 import CoinIcon, { CoinSizes } from "../../display/CoinIcon";
 import { InputAmountProps, TokenItem } from "./types";
 import { changeEvent } from "utils/event";
-import { getPrecisionForCoin } from "components/display/ZigPriceLabel/util";
 import { useUpdateEffect } from "react-use";
+import ZigPriceLabel from "components/display/ZigPriceLabel";
 
 // FIXME this component still needs Jesus
 function InputAmount({
@@ -143,15 +142,18 @@ function InputAmount({
           <BalanceLabel variant="body2" color="neutral200">
             {labelBalance}
           </BalanceLabel>
-          <Typography variant="body2" color="neutral000">
-            <NumberFormat
-              decimalScale={getPrecisionForCoin(value?.token?.id, value)}
-              value={value?.token.balance as string}
-              displayType={"text"}
-              suffix={value?.token ? ` ${value?.token?.id?.toUpperCase() ?? ""}` : ""}
-              thousandSeparator={true}
-            />
-          </Typography>
+          <ZigPriceLabel
+            value={value?.token.balance}
+            variant="body2"
+            component="span"
+            color="neutral000"
+            precision={8}
+            coin={value?.token.id}
+            coinProps={{
+              color: "neutral000",
+              fontWeight: 500,
+            }}
+          />
         </BalanceContainer>
       )}
 
