a2347ee236ca9236ab14a1c0db823b686b7d4b6b
Alexander Mikhalchenko <alex@xfuturum.com>
2022-12-08 15:03:36 +0400

    chore: fixed ts issues
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
+++ packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
@@ -2,10 +2,8 @@ import { LoadingButton, LoadingButtonProps } from "@mui/lab";
 import { styled } from "@mui/system";
 import React from "react";
 
-const ZigButton: React.FC<LoadingButtonProps & { active?: boolean }> = styled(
-  ({ active, ...props }: LoadingButtonProps & { active?: boolean }) => (
-    <LoadingButton {...props} className={active ? "MuiButton-active" : ""} />
-  ),
-)``;
+const ZigButton = styled(({ active, ...props }: LoadingButtonProps & { active?: boolean }) => (
+  <LoadingButton {...props} className={active ? "MuiButton-active" : ""} />
+))``;
 
 export default ZigButton;
