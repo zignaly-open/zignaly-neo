5e155c304411441c4c647207a16f63ba7e13438e
Alexander Mikhalchenko <alex@xfuturum.com>
2022-12-13 20:10:16 +0400

    chore: storybook updates
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/components/inputs/ZigButton/stories.tsx
+++ packages/zignaly-ui/src/components/inputs/ZigButton/stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "@mui/system";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import ZigButton from "./index";
@@ -22,6 +22,9 @@ const result: ComponentMeta<typeof ZigButton> = {
       },
       options: ["outlined", "contained"],
     },
+    tooltip: {
+      type: "string",
+    },
     size: {
       control: {
         type: "select",
@@ -38,19 +41,45 @@ const Template: ComponentStory<typeof ZigButton> = (args) => <ZigButton {...args
 export const Default: ComponentMeta<typeof ZigButton> = Template.bind({});
 Default.args = {
   children: "Amount to Withdraw",
+  tooltip: "Hello",
+  variant: "outlined",
 };
 
 const TemplateGroup: ComponentStory<typeof ZigButton> = ({ variant, size, ...args }) => {
+  const [value, setValue] = useState<number>(1);
   return (
-    <ButtonGroup variant={variant} size={size}>
-      <ZigButton {...args} />
-      <ZigButton {...args} />
-      <ZigButton {...args} />
+    <ButtonGroup variant="outlined" size={size}>
+      <ZigButton active={value === 1} onClick={() => setValue(1)} variant="outlined" {...args} />
+      <ZigButton active={value === 2} onClick={() => setValue(2)} variant="outlined" {...args} />
+      <ZigButton active={value === 3} onClick={() => setValue(3)} variant="outlined" {...args} />
     </ButtonGroup>
   );
 };
 
 export const Group: ComponentMeta<typeof ZigButton> = TemplateGroup.bind({});
 Group.args = {
+  children: "Amount to Withdraw",
+};
+
+const TemplateSizes: ComponentStory<typeof ZigButton> = ({ variant, size, ...args }) => {
+  return (
+    <>
+      <ZigButton size="small" {...args} />
+      <ZigButton size="medium" {...args} />
+      <ZigButton size="large" {...args} />
+      <br />
+      <ZigButton variant={"outlined"} size="small" {...args} />
+      <ZigButton variant={"outlined"} size="medium" {...args} />
+      <ZigButton variant={"outlined"} size="large" {...args} />
+      <br />
+      <ZigButton variant={"contained"} size="small" {...args} />
+      <ZigButton variant={"contained"} size="medium" {...args} />
+      <ZigButton variant={"contained"} size="large" {...args} />
+    </>
+  );
+};
+
+export const Sizes: ComponentMeta<typeof ZigButton> = TemplateSizes.bind({});
+Sizes.args = {
   children: "Amount to Withdraw",
 };
