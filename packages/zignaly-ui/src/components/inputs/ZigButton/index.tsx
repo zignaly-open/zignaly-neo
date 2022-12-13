0486d6b450daa837285d15c6f8e088ffc4cffd59
Alexander Mikhalchenko <alex@xfuturum.com>
2022-12-13 20:00:39 +0400

    chore: hide options that do not have enough data
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
+++ packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
@@ -1,11 +1,20 @@
 import { Button, ButtonProps } from "@mui/material";
 import { styled } from "@mui/system";
 import React from "react";
+import { Tooltip } from "@mui/material";
 
-const ZigButton: React.FC<ButtonProps & { active?: boolean }> = styled(
-  ({ active, ...props }: ButtonProps & { active?: boolean }) => (
-    <Button {...props} className={active ? "MuiButton-active" : ""} />
-  ),
+const ZigButton = styled(
+  ({ active, tooltip, ...props }: LoadingButtonProps & { tooltip?: string; active?: boolean }) => {
+    const button = <LoadingButton {...props} className={active ? "MuiButton-active" : ""} />;
+    return tooltip ? (
+      <Tooltip title={tooltip}>
+        {/* if we want to have a tooltip and have the button be disabled, we need a new container */}
+        {props.disabled ? <span>{button}</span> : button}
+      </Tooltip>
+    ) : (
+      button
+    );
+  },
 )``;
 
 export default ZigButton;
