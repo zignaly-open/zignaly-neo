d2765342cbad05ff46b895bc362303d7fbcff7fc
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-16 19:38:50 +0700

    fix: unmount on refetch to clear the table

--- packages/ps2/src/views/Balance/components/MyBalancesTable/index.tsx
+++ packages/ps2/src/views/Balance/components/MyBalancesTable/index.tsx
@@ -129,6 +129,7 @@ const MyBalancesTable = (): JSX.Element => {
 
   return (
     <LayoutContentWrapper
+      unmountOnRefetch={true}
       endpoint={[coinsEndpoint, balancesEndpoint]}
       content={([coins, balances]: [CoinDetails, CoinBalances]) => (
         <ZigTable
