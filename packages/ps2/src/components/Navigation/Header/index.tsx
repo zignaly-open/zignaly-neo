5fbbb065bd1a32a53dfbf5da5da8f828a2499f90
Alexander Mikhalchenko <alex@xfuturum.com>
2022-12-16 04:40:28 +0400

    feat: tracker as a separate package
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/components/Navigation/Header/index.tsx
+++ packages/ps2/src/components/Navigation/Header/index.tsx
@@ -3,16 +3,24 @@ import {
   HeaderLinksContainer,
   BrandImage,
 } from '@zignaly-open/ui';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { NavigationLink } from './atoms';
 import { ROUTE_DASHBOARD, ROUTE_PROFIT_SHARING } from '../../../routes';
 import { Link } from 'react-router-dom';
 import ExtraNavigationDropdown from '../ExtraNavigationDropdown';
 import AccountMenu from '../AccountMenu';
+import { track } from '@zignaly-open/tracker';
+import { useCurrentUser } from '../../../apis/user/use';
 
 const Header: React.FC = () => {
   const { t } = useTranslation('common');
+  const { userId } = useCurrentUser();
+
+  useEffect(() => {
+    track({ userId });
+  }, [window.location.href, userId]);
+
   return (
     <ZigHeader
       leftElements={[
