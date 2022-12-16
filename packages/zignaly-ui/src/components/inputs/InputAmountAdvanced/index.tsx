4dfb09400f0fe550c1a371ccfdb1bdf57454b578
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-16 21:01:29 +0700

    style: improve error padding

--- packages/zignaly-ui/src/components/inputs/InputAmountAdvanced/index.tsx
+++ packages/zignaly-ui/src/components/inputs/InputAmountAdvanced/index.tsx
@@ -138,7 +138,7 @@ function InputAmount({
       </Wrapper>
 
       {value?.token?.balance && showBalance && (
-        <BalanceContainer>
+        <Box mt={1}>
           <BalanceLabel variant="body2" color="neutral200">
             {labelBalance}
           </BalanceLabel>
