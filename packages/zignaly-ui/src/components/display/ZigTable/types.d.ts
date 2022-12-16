1cc94ebd2ea37bd003aedcc1aff11b8199abb334
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-16 10:54:10 +0700

    fix: fix table pagination

--- packages/zignaly-ui/src/components/display/ZigTable/types.d.ts
+++ packages/zignaly-ui/src/components/display/ZigTable/types.d.ts
@@ -1,8 +1,7 @@
 import { Row, TableOptions, TableState, ColumnMeta as ColumnMetaOrig } from "@tanstack/react-table";
 
 export interface ZigTableProps<T extends object> extends Omit<TableOptions<T>, "getCoreRowModel"> {
-  pagination?: TableState["pagination"];
-  enablePagination?: boolean;
+  pagination?: false | TableState["pagination"];
   loading?: boolean;
   columnVisibility?: boolean;
   renderSubComponent?: (props: { row: Row<T> }) => React.ReactElement;
