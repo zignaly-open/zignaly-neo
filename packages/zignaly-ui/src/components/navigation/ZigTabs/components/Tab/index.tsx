c7e4aeac1fae8ca733c1d4c4eb3284ca22512952
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-19 14:26:11 +0700

    feat: csv export

--- packages/zignaly-ui/src/components/navigation/ZigTabs/components/Tab/index.tsx
+++ packages/zignaly-ui/src/components/navigation/ZigTabs/components/Tab/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Tab as TabMui, TabProps } from "@mui/material";
 
-const Tab = (props: TabProps) => {
+const Tab = (
+  props: TabProps & {
+    asideComponent?: React.ReactNode;
+  },
+) => {
   return <TabMui {...props} data-text={props.label} />;
 };
 export default Tab;
