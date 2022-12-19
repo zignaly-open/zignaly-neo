80387b0af0bd0ec8bc698a91461eb6ac9442f609
Alexander Mikhalchenko <alex@xfuturum.com>
2022-12-20 02:20:09 +0400

    fix: track fixes
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/components/Navigation/Tracker/Tracker.tsx
+++ packages/ps2/src/components/Navigation/Tracker/Tracker.tsx
@@ -1,11 +1,28 @@
 import React, { useEffect } from 'react';
 import { useCurrentUser } from '../../../apis/user/use';
-import { track } from '@zignaly-open/tracker';
+import { track, trackCta } from '@zignaly-open/tracker';
 import { useLocation } from 'react-router-dom';
 
 const Tracker: React.FC = () => {
   const { userId } = useCurrentUser();
   const location = useLocation();
+
+  useEffect(() => {
+    const clickListener = (e: MouseEvent) => {
+      const node = e.target as HTMLElement;
+      if (['a', 'button'].includes(node?.tagName.toLocaleLowerCase())) {
+        const ctaId =
+          node.getAttribute('data-track-cta') || node.getAttribute('id');
+        ctaId &&
+          trackCta({
+            userId,
+            ctaId,
+          });
+      }
+    };
+    document.addEventListener('click', clickListener);
+    return () => document.removeEventListener('click', clickListener);
+  }, [userId]);
 
   useEffect(() => {
     track({ userId });
