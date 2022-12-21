d8917dab31481ed9cd4578079c324dfd42bb28fa
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-21 14:14:15 +0700

    feat: migrate dashboard to ZigTable

--- packages/zignaly-ui/src/components/display/ZigTable/types.d.ts
+++ packages/zignaly-ui/src/components/display/ZigTable/types.d.ts
@@ -1,14 +1,26 @@
-import { Row, TableOptions, TableState, ColumnMeta as ColumnMetaOrig } from "@tanstack/react-table";
+import {
+  Row,
+  TableOptions,
+  TableState,
+  ColumnMeta as ColumnMetaOrig,
+  ColumnDefBase as ColumnDefBaseOrig,
+} from "@tanstack/react-table";
 
 export interface ZigTableProps<T extends object> extends Omit<TableOptions<T>, "getCoreRowModel"> {
   pagination?: false | TableState["pagination"];
   loading?: boolean;
   columnVisibility?: boolean;
   renderSubComponent?: (props: { row: Row<T> }) => React.ReactElement;
+  emptyMessage?: string;
 }
 
 declare module "@tanstack/react-table" {
+  export interface ColumnDefBase extends ColumnDefBaseOrig {
+    style?: React.CSSProperties;
+  }
+
   export interface ColumnMeta extends ColumnMetaOrig {
     subtitle: string;
+    style?: React.CSSProperties;
   }
 }
