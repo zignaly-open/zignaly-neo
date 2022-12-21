67ff6650b2c65360492238735f9c839c28264df9
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-21 09:07:58 +0700

    fix: message fix

--- packages/ps2/src/views/Balance/components/TransactionsHistoryTable/atoms/TransactionDetails/index.tsx
+++ packages/ps2/src/views/Balance/components/TransactionsHistoryTable/atoms/TransactionDetails/index.tsx
@@ -22,18 +22,16 @@ const TransactionDetails = ({ transaction }: { transaction: Transaction }) => {
         <TypographyPanelLabel>{t('details.to')}</TypographyPanelLabel>
         <FromTo side='to' transaction={transaction} />
       </Box>
-      <Box display='flex' alignItems='center' gap={5}>
-        <Box display='flex' alignItems='center'>
-          <TypographyPanelLabel>{t('details.txHash')}</TypographyPanelLabel>
-          <TypographyAddress>{transaction.txId}</TypographyAddress>
-        </Box>
-        {transaction.note && (
-          <Box display='flex' alignItems='center'>
-            <TypographyPanelLabel>{t('details.note')}</TypographyPanelLabel>
-            <TypographyAddress>{transaction.note}</TypographyAddress>
-          </Box>
-        )}
+      <Box display='flex' alignItems='center'>
+        <TypographyPanelLabel>{t('details.txHash')}</TypographyPanelLabel>
+        <TypographyAddress>{transaction.txId}</TypographyAddress>
       </Box>
+      {transaction.message && (
+        <Box display='flex' alignItems='center'>
+          <TypographyPanelLabel>{t('details.note')}</TypographyPanelLabel>
+          <TypographyAddress>{transaction.message}</TypographyAddress>
+        </Box>
+      )}
     </TransactionPanel>
   );
 };
