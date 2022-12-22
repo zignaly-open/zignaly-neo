81de28154c30382e83c336e91eb17fa64fa2d308
servatj <jservatlorca@gmail.com>
2022-12-23 01:43:40 +0400

    Add multiple deposit test

--- packages/raffles-server/src/entities/balances/service.test.ts
+++ packages/raffles-server/src/entities/balances/service.test.ts
@@ -25,5 +25,32 @@ describe('Balance service', () => {
 
       expect(userBalance).toEqual(expectedBalance);
     });
+
+    it('should be able to deposit two times 100 zigs and get 200 zhists', async () => {
+      const expectedBalance = {
+        walletAddress: '0x001',
+        zhits: '200',
+      } as UserBalanceZhits;
+
+      // first deposit
+      await deposit({
+        walletAddress: '0x001',
+        amount: '100',
+        currency: 'zigs',
+        blockchain: 'polygon',
+      });
+
+      // second deposit
+      await deposit({
+        walletAddress: '0x001',
+        amount: '100',
+        currency: 'zigs',
+        blockchain: 'polygon',
+      });
+
+      const userBalance: UserBalanceZhits = await getUserBalance('0x001');
+
+      expect(userBalance).toEqual(expectedBalance);
+    });
   });
 });
