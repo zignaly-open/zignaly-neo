44202d60b0657fab311242021ad2ec70e75efc88
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-23 15:40:36 +0700

    fix: max 8 decimals for usd

--- packages/zignaly-ui/src/components/display/ZigPriceLabel/util.ts
+++ packages/zignaly-ui/src/components/display/ZigPriceLabel/util.ts
@@ -4,6 +4,6 @@ export function getPrecisionForCoin(coin: string, value: string | number): numbe
   if (+value === 0) return 2;
   const coinPrecision = coinPrecisions[coin];
   // Show exact decimals for stable coins with a value lower than 0.01
-  if (coinPrecision === 2 && +value > 0 && +value < 0.01) return undefined;
+  if (coinPrecision === 2 && +value > 0 && +value < 0.01) return 8;
   return coinPrecision || 8;
 }
