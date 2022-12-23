0c20eb851dcc3e8c1a7889566017de5f4060a2a2
Christophe Wagner <chris.wagn22@gmail.com>
2022-12-23 14:05:23 +0700

    feat: show exact decimals for stable coins if needed

--- packages/zignaly-ui/src/components/display/ZigPriceLabel/util.ts
+++ packages/zignaly-ui/src/components/display/ZigPriceLabel/util.ts
@@ -1,5 +1,9 @@
 import { coinPrecisions } from "./constants";
 
 export function getPrecisionForCoin(coin: string, value: string | number): number {
-  return (+value === 0 && 2) || coinPrecisions[coin] || 8;
+  if (+value === 0) return 2;
+  const coinPrecision = coinPrecisions[coin];
+  // Show exact decimals for stable coins with a value lower than 0.01
+  if (coinPrecision === 2 && +value > 0 && +value < 0.01) return undefined;
+  return coinPrecision || 8;
 }
