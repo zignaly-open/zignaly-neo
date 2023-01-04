c367344b0e3120b4030228ddddc155d377b4b120
Alexander Mikhalchenko <alex@xfuturum.com>
2023-01-04 23:07:24 +0400

    fix: dirty fix for sx not being passed
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
+++ packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
@@ -3,18 +3,24 @@ import { styled } from "@mui/system";
 import React from "react";
 import { Tooltip } from "@mui/material";
 
+type ButtonProps = LoadingButtonProps & { ctaId?: string; tooltip?: string; active?: boolean };
+
+type ButtonPropsSxFix = Omit<ButtonProps, "sx"> & { __sx: ButtonProps["sx"] };
+
 const ZigButton = styled(
   ({
     active,
     tooltip,
     ctaId,
     color,
+    __sx, // somehow in this scenario the sx prop is not passed properly
     ...props
-  }: LoadingButtonProps & { ctaId?: string; tooltip?: string; active?: boolean }) => {
+  }: ButtonPropsSxFix) => {
     const button = (
       <LoadingButton
         data-tack-cta={ctaId}
         {...props}
+        sx={__sx}
         // hack to preserve old behavior but allow for normal mui theming
         color={props.variant === "outlined" && !color ? "secondary" : color}
         className={active ? "MuiButton-active" : ""}
@@ -31,4 +37,4 @@ const ZigButton = styled(
   },
 )``;
 
-export default ZigButton;
+export default ({ sx, ...props }: ButtonProps) => <ZigButton {...props} __sx={sx} />;
