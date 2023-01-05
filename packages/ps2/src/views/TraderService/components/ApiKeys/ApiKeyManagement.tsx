d7efd4ac0fdaadc07a3fc7cfceb5ef685967c81d
Alexander Mikhalchenko <alex@xfuturum.com>
2023-01-06 02:26:25 +0400

    fix: fixed same mutation being used for different deletions
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/views/TraderService/components/ApiKeys/ApiKeyManagement.tsx
+++ packages/ps2/src/views/TraderService/components/ApiKeys/ApiKeyManagement.tsx
@@ -1,50 +1,29 @@
 import React from 'react';
-import {
-  CloneIcon,
-  dark,
-  InputText,
-  ZigButton,
-  ZigTypography,
-} from '@zignaly-open/ui';
+import { ZigButton, ZigTypography } from '@zignaly-open/ui';
 import { Box } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import PencilIcon from '@mui/icons-material/Create';
 import { Trans, useTranslation } from 'react-i18next';
 import { ROUTE_TRADING_SERVICE_POSITIONS } from '../../../../routes';
 import AnchorLink from '../../../../components/AnchorLink';
-import { ApiKey, ApiKeysContainer, TextWrapperRow, TitleBox } from './atoms';
+import { ApiKeysContainer, TitleBox } from './atoms';
 import { generatePath, useParams } from 'react-router-dom';
-import {
-  useServiceApiKeyDeleteMutation,
-  useServiceApiKeysQuery,
-} from '../../../../apis/serviceApiKey/api';
+import { useServiceApiKeysQuery } from '../../../../apis/serviceApiKey/api';
 import CenteredLoader from '../../../../components/CenteredLoader';
-import copy from 'copy-to-clipboard';
-import { useToast } from '../../../../util/hooks/useToast';
-import { addReadIfMissing } from './util';
-import { useZConfirm, useZModal } from '../../../../components/ZModal/use';
+import { useZModal } from '../../../../components/ZModal/use';
 import CreateApiKey from './modals/CreateApiKey';
 import EditApiKey from './modals/EditApiKey';
 import { ServiceApiKey } from 'apis/serviceApiKey/types';
 import Stub from '../../../../components/Stub';
-import { useCheck2FA } from '../../../../apis/user/use';
+import ApiKeyEntry from './components/ApiKeyEntry';
 
 const ApiKeyManagement: React.FC = () => {
-  const { t, i18n } = useTranslation(['management', 'actions']);
+  const { t } = useTranslation(['management', 'actions']);
   const { serviceId } = useParams();
   const { showModal } = useZModal();
-  const askConfirm = useZConfirm();
-  const toast = useToast();
   const {
     isLoading,
     isFetching,
     data: keys,
   } = useServiceApiKeysQuery({ serviceId });
-  const [deleteKey, deleteStatus] = useServiceApiKeyDeleteMutation();
-  const { isLoading: isDeleting } = deleteStatus;
-  const delete2FA = useCheck2FA({
-    status: deleteStatus,
-  });
 
   return (
     <>
@@ -92,7 +71,7 @@ const ApiKeyManagement: React.FC = () => {
         </Box>
       </TitleBox>
 
-      {isLoading || isDeleting || isFetching ? (
+      {isLoading || isFetching ? (
         <CenteredLoader />
       ) : (
         <ApiKeysContainer>
@@ -109,94 +88,7 @@ const ApiKeyManagement: React.FC = () => {
             />
           )}
           {keys.map((apiKey) => (
-            <ApiKey key={apiKey.id}>
-              <ZigTypography variant='h3' sx={{ mb: 1.5 }}>
-                {apiKey.alias}
-              </ZigTypography>
-              <Box sx={{ flexDirection: 'row', display: 'flex', gap: 3 }}>
-                <Box sx={{ flex: 5, mr: 2 }}>
-                  <InputText
-                    placeholder={t('api-keys.api-key')}
-                    label={t('api-keys.api-key')}
-                    readOnly={true}
-                    value={apiKey.key}
-                    rightSideElement={
-                      <CloneIcon
-                        width={40}
-                        height={40}
-                        color={dark.neutral300}
-                      />
-                    }
-                    onClickRightSideElement={() => {
-                      copy(apiKey.key);
-                      toast.success(t('action:copied'));
-                    }}
-                  />
-                </Box>
-                <Box sx={{ flex: 2 }}>
-                  <ZigTypography color={'neutral200'}>
-                    {t('api-keys.permission-label')}
-                  </ZigTypography>
-                  <TextWrapperRow>
-                    <ZigTypography color={'neutral100'}>
-                      {addReadIfMissing(apiKey.permissions)
-                        .map((p) =>
-                          i18n.exists(`management:api-keys.permissions.${p}`)
-                            ? t(`api-keys.permissions.${p}`)
-                            : p,
-                        )
-                        .join(', ')}
-                    </ZigTypography>
-                  </TextWrapperRow>
-                </Box>
-                <Box sx={{ flex: 2 }}>
-                  <ZigTypography color={'neutral200'}>
-                    {t('api-keys.ip-restrictions')}
-                  </ZigTypography>
-                  <TextWrapperRow>
-                    <ZigTypography color={'neutral100'}>
-                      {apiKey.ips.join(', ') ||
-                        t('api-keys.ip-restrictions-none')}
-                    </ZigTypography>
-                  </TextWrapperRow>
-                </Box>
-                <Box sx={{ alignSelf: 'center' }}>
-                  <ZigButton
-                    sx={{ mr: 2 }}
-                    onClick={() => showModal(EditApiKey, { apiKey, serviceId })}
-                    startIcon={<PencilIcon />}
-                    variant={'outlined'}
-                  >
-                    {t('action:edit')}
-                  </ZigButton>
-                  <ZigButton
-                    sx={{ mr: 2 }}
-                    onClick={() =>
-                      askConfirm({
-                        title: t('api-keys.delete-title', {
-                          title: apiKey.alias,
-                        }),
-                        yesLabel: t('action:delete'),
-                        yesButtonProps: {
-                          color: 'danger',
-                          variant: 'outlined',
-                        },
-                        description: t('api-keys.delete-description'),
-                        yesAction: () =>
-                          delete2FA((code) =>
-                            deleteKey({ serviceId, keyId: apiKey.id, code }),
-                          ),
-                      })
-                    }
-                    color={'danger'}
-                    startIcon={<DeleteIcon />}
-                    variant={'outlined'}
-                  >
-                    {t('action:delete')}
-                  </ZigButton>
-                </Box>
-              </Box>
-            </ApiKey>
+            <ApiKeyEntry key={apiKey.id} apiKey={apiKey} />
           ))}
         </ApiKeysContainer>
       )}
