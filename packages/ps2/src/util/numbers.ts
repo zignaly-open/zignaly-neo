74525dfe43e10d8fa0e5f0549c57c6af419b67b0
Alexander Mikhalchenko <alex@xfuturum.com>
2023-01-10 03:51:42 +0800

    fix: more minor fixes for numbers
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/util/numbers.ts
+++ packages/ps2/src/util/numbers.ts
@@ -19,9 +19,14 @@ export const sortBigNumbers = (
 export const precisionNumberToDecimals = (integerMultiple: string) => {
   // https://www.reddit.com/r/BinanceExchange/comments/995jra/getting_atomic_withdraw_unit_from_api/e4mi63w/
   const integerMultipleFloat = parseFloat(integerMultiple);
-  const maxDecimals =
-    integerMultipleFloat > 1 ? 0 : Math.abs(Math.log10(integerMultipleFloat));
-  return maxDecimals;
+  return integerMultipleFloat > 1
+    ? 0
+    : Math.abs(Math.log10(integerMultipleFloat));
 };
 
 export const stringSort = (a: string, b: string) => a.localeCompare(b);
+
+export const getColorForNumber = (
+  value: number | string,
+): undefined | 'redGraphOrError' | 'greenGraph' =>
+  +value === 0 ? undefined : +value > 0 ? 'greenGraph' : 'redGraphOrError';
--- packages/ps2/src/views/ProfitSharing/components/ServiceCard/types.ts
+++
@@ -1,7 +0,0 @@
-export interface BalanceSummaryProps {
-  totalValue?: string;
-  profit: string;
-  coin: string;
-  onClickEdit?: () => void;
-  dashboardType?: 'user' | 'investor' | 'marketplace';
-}
