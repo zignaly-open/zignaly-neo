44404344a235116b021031026c70fd7cfcf929df
Christophe Wagner <chris.wagn22@gmail.com>
2023-01-17 20:05:29 +0700

    feat: show deleted exchange in table

--- packages/ps2/src/views/Balance/components/TransactionsHistoryTable/atoms/TransferZigLabel.tsx
+++ packages/ps2/src/views/Balance/components/TransactionsHistoryTable/atoms/TransferZigLabel.tsx
@@ -1,12 +1,16 @@
 import { ZignalyLogo } from '@zignaly-open/ui';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { TypographyPanelName } from './TransactionDetails/styles';
 
-const ZignalyAccount = ({ name }: { name?: string }) => (
-  <>
-    <ZignalyLogo width={24} height={24} style={{ marginRight: '16px' }} />
-    <TypographyPanelName>{name}</TypographyPanelName>
-  </>
-);
+const ZignalyAccount = ({ name }: { name?: string }) => {
+  const { t } = useTranslation('transactions-history');
+  return (
+    <>
+      <ZignalyLogo width={24} height={24} style={{ marginRight: '16px' }} />
+      <TypographyPanelName>{name || t('deleted')}</TypographyPanelName>
+    </>
+  );
+};
 
 export default ZignalyAccount;
