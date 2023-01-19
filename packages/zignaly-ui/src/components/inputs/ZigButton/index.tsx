29ea2c309a4a6e0e8dbd15115b89c0c790486c67
Christophe Wagner <chris.wagn22@gmail.com>
2023-01-19 18:47:55 +0700

    chore: fix typescript error

--- packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
+++ packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
@@ -1,44 +1,32 @@
-import { LoadingButton, LoadingButtonProps } from "@mui/lab";
-import { styled } from "@mui/system";
 import React from "react";
+import { LoadingButton } from "@mui/lab";
 import { Tooltip } from "@mui/material";
 
-export type ZigButtonProps = LoadingButtonProps & {
+export type ZigButtonProps = React.ComponentProps<typeof LoadingButton> & {
+  // export type ZigButtonProps = LoadingButtonProps & {
   ctaId?: string;
   tooltip?: string;
   active?: boolean;
 };
 
-type ZigButtonPropsSxFix = Omit<ZigButtonProps, "sx"> & { __sx: ZigButtonProps["sx"] };
+const ZigButton = ({ loading, active, tooltip, ctaId, color, ...props }: ZigButtonProps) => {
+  const button = (
+    <LoadingButton
+      data-tack-cta={ctaId}
+      {...props}
+      // hack to preserve old behavior but allow for normal mui theming
+      color={props.variant === "outlined" && !color ? "secondary" : color}
+      className={active ? "MuiButton-active" : ""}
+    />
+  );
+  return tooltip ? (
+    <Tooltip title={tooltip}>
+      {/* if we want to have a tooltip and have the button be disabled, we need a new container */}
+      {props.disabled ? <span>{button}</span> : button}
+    </Tooltip>
+  ) : (
+    button
+  );
+};
 
-const ZigButton = styled(
-  ({
-    active,
-    tooltip,
-    ctaId,
-    color,
-    __sx, // somehow in this scenario the sx prop is not passed properly
-    ...props
-  }: ZigButtonPropsSxFix) => {
-    const button = (
-      <LoadingButton
-        data-tack-cta={ctaId}
-        {...props}
-        sx={__sx}
-        // hack to preserve old behavior but allow for normal mui theming
-        color={props.variant === "outlined" && !color ? "secondary" : color}
-        className={active ? "MuiButton-active" : ""}
-      />
-    );
-    return tooltip ? (
-      <Tooltip title={tooltip}>
-        {/* if we want to have a tooltip and have the button be disabled, we need a new container */}
-        {props.disabled ? <span>{button}</span> : button}
-      </Tooltip>
-    ) : (
-      button
-    );
-  },
-)``;
-
-export default ({ sx, ...props }: ZigButtonProps) => <ZigButton {...props} __sx={sx} />;
+export default ZigButton as typeof ZigButton & typeof LoadingButton;
