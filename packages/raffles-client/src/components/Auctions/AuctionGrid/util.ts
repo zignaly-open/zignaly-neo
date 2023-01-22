7733621a97b9edba2e9b76d446719c926e0b1ca9
Alexander Mikhalchenko <alex@xfuturum.com>
2023-01-22 23:17:45 +0800

    fix: proper bidding
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/raffles-client/src/components/Auctions/AuctionGrid/util.ts
+++ packages/raffles-client/src/components/Auctions/AuctionGrid/util.ts
@@ -4,16 +4,41 @@ import {
 } from '@zignaly-open/raffles-shared/types';
 import BN from 'bignumber.js';
 
+type BidSubscriptionData = { auctionId: number; user: AuctionBidType['user'] };
+
+const fixBid = (bid: BidSubscriptionData): BidSubscriptionData => ({
+  ...bid,
+  user: {
+    ...bid.user,
+    id: +bid.user.id,
+  },
+});
+
 export const extendAuctionListWithNewBid = (
   auctions: AuctionType[],
   bidMade: { auctionId: number; user: AuctionBidType['user'] },
 ): AuctionType[] => {
+  const bid = fixBid(bidMade);
   return auctions.map((x: AuctionType) => {
-    if (x.id === bidMade?.auctionId) {
+    if (x.id === bid?.auctionId) {
+      let { bids } = x;
+      const winningBid = bids[0];
+      debugger;
+      if (winningBid.user.id === bid.user.id) {
+        // same user, do nothing
+      } else {
+        bids = [
+          {
+            position: 1,
+            user: bid.user,
+          },
+          ...x.bids.map((b) => ({ ...b, position: b.position + 1 })),
+        ];
+      }
       return {
         ...x,
         currentBid: new BN(x.currentBid).plus(new BN(x.bidStep)).toString(),
-        // add bid to the bids list and maybe check for dates
+        bids,
       };
     } else {
       return x;
