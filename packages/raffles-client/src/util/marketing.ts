3d6db8cc51de0468c44008ef971f415f6912f42f
servatj <jservatlorca@gmail.com>
2023-02-03 00:11:50 +0400

    install sendinblue

--- packages/raffles-client/src/util/marketing.ts
+++ packages/raffles-client/src/util/marketing.ts
@@ -1,25 +1,25 @@
 import SibApiV3Sdk from 'sib-api-v3-typescript';
 
-const apiInstance = new SibApiV3Sdk.ContactsApi();
+export const confirmEmail = async (userId: string) => {
+  const apiInstance = new SibApiV3Sdk.ContactsApi();
 
-apiInstance.setApiKey(
-  SibApiV3Sdk.ContactsApiApiKeys.apiKey,
-  process.env.SENDINBLUE_API_KEY,
-);
+  apiInstance.setApiKey(
+    SibApiV3Sdk.ContactsApiApiKeys.apiKey,
+    process.env.SENDINBLUE_API_KEY,
+  );
 
-const createDoiContact = new SibApiV3Sdk.CreateDoiContact(); // CreateDoiContact | Values to create the Double opt-in (DOI) contact
+  const createDoiContact = new SibApiV3Sdk.CreateDoiContact(); // CreateDoiContact | Values to create the Double opt-in (DOI) contact
 
-createDoiContact.email = 'jservatlorca@gmail.com';
-createDoiContact.attributes = { FNAME: 'Josep', LNAME: 'Test' };
-createDoiContact.includeListIds = [2];
-createDoiContact.templateId = 4;
-createDoiContact.redirectionUrl = 'https://zigbids.zignaly.com/?confirm=USERID';
+  createDoiContact.email = 'jservatlorca@gmail.com';
+  createDoiContact.attributes = { FNAME: 'Josep', LNAME: 'Test' };
+  createDoiContact.includeListIds = [2];
+  createDoiContact.templateId = 4;
+  createDoiContact.redirectionUrl = `https://zigbids.zignaly.com/?confirm=${userId}}`;
 
-apiInstance.createDoiContact(createDoiContact).then(
-  function () {
-    console.log('API called successfully.');
-  },
-  function (error) {
+  try {
+    const response = await apiInstance.createDoiContact(createDoiContact);
+    console.log(response);
+  } catch (error) {
     console.error(error);
-  },
-);
+  }
+};
