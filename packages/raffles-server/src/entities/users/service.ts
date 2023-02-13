85e1f8805eb6c4f22dbf0711d24adcc7985fe4f6
servatj <jservatlorca@gmail.com>
2023-02-13 22:10:51 +0400

    Verify Email

--- packages/raffles-server/src/entities/users/service.ts
+++ packages/raffles-server/src/entities/users/service.ts
@@ -220,6 +220,7 @@ export const generateService = (user: ContextUser) => {
       await sendEmailVerification(`${userId}`, email);
       User.update(
         {
+          email,
           emailVerificationSent: true,
         },
         {
