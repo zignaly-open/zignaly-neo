f9888db44f08e6f80106fb1ce968f10ec5ff3aea
Alexander Mikhalchenko <alex@xfuturum.com>
2023-02-14 19:27:58 +0800

    Revert "chore: more small fixes"
    This reverts commit 969fb9fc61f6296b6bd571a9de7dcf8a096162cc.

--- packages/raffles-client/src/contract.ts
+++ packages/raffles-client/src/contract.ts
@@ -1,8 +1,8 @@
-import { Interface } from 'ethers/lib/utils';
+import { utils } from 'ethers';
 import { Contract } from '@ethersproject/contracts';
 import { IERC20 } from '@zignaly-open/raffles-shared/abis';
 
-const wethInterface = new Interface(IERC20);
+const wethInterface = new utils.Interface(IERC20);
 const wethContractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
 const contract = new Contract(wethContractAddress, wethInterface);
 
--- packages/zignaly-ui/src/components/navigation/Header/components/ZigsBalance/index.tsx
+++ packages/zignaly-ui/src/components/navigation/Header/components/ZigsBalance/index.tsx
@@ -5,14 +5,14 @@ import { ZigBalanceProps } from "./types";
 
 import { Layout, StyledWalletIcon, Balance } from "./styles";
 
-import { formatUnits } from "ethers/lib/utils";
+import { utils } from "ethers";
 
 function ZigBalance({ balance, className }: ZigBalanceProps) {
   return (
     <Layout className={className}>
       <StyledWalletIcon />
       <NumericFormat
-        value={formatUnits((balance || "0").toString())}
+        value={utils.formatUnits((balance || "0").toString())}
         displayType={"text"}
         thousandSeparator={true}
         renderText={(value) => <Balance>{value} ZIG</Balance>}
