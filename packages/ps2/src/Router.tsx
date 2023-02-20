99cb45bf68ee3649cb30a68a7769852f8c4219d7
Alexander Mikhalchenko <alex@xfuturum.com>
2023-02-20 20:44:40 +0800

    feat: lazy-loading & chunks
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/Router.tsx
+++ packages/ps2/src/Router.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import ProfitSharing from './views/ProfitSharing';
-import Dashboard from './views/Dashboard';
-import Login from './views/Auth/Login';
-import Signup from './views/Auth/Signup';
-import ForgotPassword from './views/Auth/ForgotPassword';
-import MyBalances from './views/Balance';
-import Wallet from './views/Wallet';
+import AuthenticatedWall from 'util/walls/AuthenticatedWall.tsx';
+import UnauthenticatedWall from './util/walls/UnauthenticatedWall';
+import ServiceOwnerWall from './util/walls/ServiceOwnerWall';
 
 import {
   ROUTE_DASHBOARD,
@@ -31,61 +27,76 @@ import {
   ROUTE_RESET_PASSWORD,
 } from './routes';
 
-import Management from './views/TraderService/Management';
-import Investors from './views/TraderService/Investors';
-import BecomeTrader from './views/TraderService/BecomeTrader';
-import Positions from './views/TraderService/Positions';
-import Coins from './views/TraderService/Coins';
-import ServiceProfile from './views/TraderService/ServiceProfile';
-import ServiceApi from './views/TraderService/ServiceApi';
-import Manual from 'views/TraderService/Manual';
-import Signals from './views/TraderService/Signals';
-import EditService from 'views/TraderService/EditService';
-import AuthenticatedWall from 'util/walls/AuthenticatedWall.tsx';
-import UnauthenticatedWall from './util/walls/UnauthenticatedWall';
-import ServiceOwnerWall from './util/walls/ServiceOwnerWall';
-import HelpInvestor from './views/Help/HelpInvestor';
-import NotFound from 'views/404';
-import ResetPassword from 'views/Auth/ResetPassword';
+const ProfitSharing = lazy(() => import('./views/ProfitSharing'));
+const Dashboard = lazy(() => import('./views/Dashboard'));
+const Login = lazy(() => import('./views/Auth/Login'));
+const Signup = lazy(() => import('./views/Auth/Signup'));
+const ForgotPassword = lazy(() => import('./views/Auth/ForgotPassword'));
+const MyBalances = lazy(() => import('./views/Balance'));
+const Wallet = lazy(() => import('./views/Wallet'));
+const Management = lazy(() => import('./views/TraderService/Management'));
+const Investors = lazy(() => import('./views/TraderService/Investors'));
+const BecomeTrader = lazy(() => import('./views/TraderService/BecomeTrader'));
+const Positions = lazy(() => import('./views/TraderService/Positions'));
+const Coins = lazy(() => import('./views/TraderService/Coins'));
+const ServicePage = lazy(() => import('./views/TraderService/ServiceProfile'));
+const ServiceApi = lazy(() => import('./views/TraderService/ServiceApi'));
+const Manual = lazy(() => import('views/TraderService/Manual'));
+const Signals = lazy(() => import('./views/TraderService/Signals'));
+const EditService = lazy(() => import('views/TraderService/EditService'));
+const HelpInvestor = lazy(() => import('./views/Help/HelpInvestor'));
+const NotFound = lazy(() => import('views/404'));
+const ResetPassword = lazy(() => import('views/Auth/ResetPassword'));
 
 const Router: React.FC = () => (
-  <Routes>
-    <Route element={<AuthenticatedWall />}>
-      <Route path={ROUTE_DASHBOARD} element={<Dashboard />} />
-      <Route path={ROUTE_MY_BALANCES} element={<MyBalances />} />
-      <Route path={ROUTE_WALLET} element={<Wallet />} />
-    </Route>
-
-    <Route path={ROUTE_TRADING_SERVICE}>
-      <Route index element={<ServiceProfile />} />
-      <Route element={<ServiceOwnerWall />}>
-        <Route path={ROUTE_TRADING_SERVICE_MANAGE} element={<Management />} />
-        <Route path={ROUTE_TRADING_SERVICE_INVESTORS} element={<Investors />} />
-        <Route path={ROUTE_TRADING_SERVICE_POSITIONS} element={<Positions />} />
-        <Route path={ROUTE_TRADING_SERVICE_COINS} element={<Coins />} />
-        <Route path={ROUTE_TRADING_SERVICE_MANUAL} element={<Manual />} />
-        <Route path={ROUTE_TRADING_SERVICE_API} element={<ServiceApi />} />
-        <Route path={ROUTE_TRADING_SERVICE_SIGNALS} element={<Signals />} />
-        <Route path={ROUTE_TRADING_SERVICE_EDIT} element={<EditService />} />
+  <Suspense fallback={<div>Loading...</div>}>
+    <Routes>
+      <Route element={<AuthenticatedWall />}>
+        <Route path={ROUTE_DASHBOARD} element={<Dashboard />} />
+        <Route path={ROUTE_MY_BALANCES} element={<MyBalances />} />
+        <Route path={ROUTE_WALLET} element={<Wallet />} />
       </Route>
-    </Route>
 
-    <Route path={ROUTE_BECOME_TRADER} element={<BecomeTrader />} />
-    <Route path={ROUTE_HELP_INVESTOR} element={<HelpInvestor />} />
+      <Route path={ROUTE_TRADING_SERVICE}>
+        <Route index element={<ServicePage />} />
+        <Route element={<ServiceOwnerWall />}>
+          <Route path={ROUTE_TRADING_SERVICE_MANAGE} element={<Management />} />
+          <Route
+            path={ROUTE_TRADING_SERVICE_INVESTORS}
+            element={<Investors />}
+          />
+          <Route
+            path={ROUTE_TRADING_SERVICE_POSITIONS}
+            element={<Positions />}
+          />
+          <Route path={ROUTE_TRADING_SERVICE_COINS} element={<Coins />} />
+          <Route path={ROUTE_TRADING_SERVICE_MANUAL} element={<Manual />} />
+          <Route path={ROUTE_TRADING_SERVICE_API} element={<ServiceApi />} />
+          <Route path={ROUTE_TRADING_SERVICE_SIGNALS} element={<Signals />} />
+          <Route path={ROUTE_TRADING_SERVICE_EDIT} element={<EditService />} />
+        </Route>
+      </Route>
 
-    <Route element={<UnauthenticatedWall />}>
-      <Route path={ROUTE_LOGIN} element={<Login />} />
-      <Route path={ROUTE_SIGNUP} element={<Signup />} />
-      <Route path={ROUTE_FORGOT_PASSWORD} element={<ForgotPassword />} />
-      <Route path={ROUTE_RESET_PASSWORD} element={<ResetPassword />} />
-    </Route>
+      <Route path={ROUTE_BECOME_TRADER} element={<BecomeTrader />} />
+      <Route path={ROUTE_HELP_INVESTOR} element={<HelpInvestor />} />
 
-    <Route path={ROUTE_PROFIT_SHARING} element={<ProfitSharing />} />
-    <Route path={ROUTE_404} element={<NotFound />} />
+      <Route element={<UnauthenticatedWall />}>
+        <Route path={ROUTE_LOGIN} element={<Login />} />
+        <Route path={ROUTE_SIGNUP} element={<Signup />} />
+        <Route path={ROUTE_FORGOT_PASSWORD} element={<ForgotPassword />} />
+        <Route path={ROUTE_RESET_PASSWORD} element={<ResetPassword />} />
+      </Route>
 
-    <Route path='/' element={<Navigate to={ROUTE_PROFIT_SHARING} replace />} />
-    <Route path='*' element={<Navigate to={ROUTE_404} replace />} />
-  </Routes>
+      <Route path={ROUTE_PROFIT_SHARING} element={<ProfitSharing />} />
+      <Route path={ROUTE_404} element={<NotFound />} />
+
+      <Route
+        path='/'
+        element={<Navigate to={ROUTE_PROFIT_SHARING} replace />}
+      />
+      <Route path='*' element={<Navigate to={ROUTE_404} replace />} />
+    </Routes>
+  </Suspense>
 );
 
 export default Router;
