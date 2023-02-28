09e0251048328cb95859cacd9341640690dad627
Alexander Mikhalchenko <alex@xfuturum.com>
2023-03-01 05:37:47 +0800

    feat: referral dashbaord - design imnplemented
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/views/Referrals/index.tsx
+++ packages/ps2/src/views/Referrals/index.tsx
@@ -1,20 +1,315 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useMemo } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 import { useTitle } from 'react-use';
-import ComingSoon from '../../components/Stub/ComingSoon';
 import {
   useReferralHistoryQuery,
   useReferralRewardsQuery,
 } from '../../apis/referrals/api';
+import { Box, Grid } from '@mui/material';
+import {
+  CloneIcon,
+  createColumnHelper,
+  dark,
+  InputText,
+  PageContainer,
+  ZigCoinIcon,
+  ZigPriceLabel,
+  ZigTable,
+  ZigTypography,
+} from '@zignaly-open/ui';
+import GroupIcon from '@mui/icons-material/Group';
+import { GetWhatYouDeserveLabel, Hr, TotalBoxValue } from './styles';
+import LayoutContentWrapper from '../../components/LayoutContentWrapper';
+
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import { useCurrentUser } from '../../apis/user/use';
+import copy from 'copy-to-clipboard';
+import { useToast } from '../../util/hooks/useToast';
+import { generatePath } from 'react-router-dom';
+import { ROUTE_REFERRALS_INVITE } from '../../routes';
+import { TotalBox } from './atoms';
+import {
+  ReferralHistory,
+  ReferralHistoryEntry,
+  ReferralRewards,
+} from '../../apis/referrals/types';
 
 const Referrals: React.FC = () => {
-  const { t } = useTranslation('pages');
+  const { t } = useTranslation(['referrals', 'pages']);
   const rewards = useReferralRewardsQuery();
   const history = useReferralHistoryQuery();
-  console.error(rewards, history);
+  const { refCode } = useCurrentUser();
+  const toast = useToast();
 
-  useTitle(t('referrals'));
-  return <ComingSoon />;
+  useTitle(t('pages:referrals'));
+
+  const columnHelper = createColumnHelper<ReferralHistoryEntry>();
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor('date', {
+        header: t('table.time-and-date'),
+        cell: ({ getValue }) => (
+          <ZigTypography
+            display='flex'
+            alignItems='center'
+            justifyContent='center'
+            gap={1}
+          >
+            <ZigCoinIcon coin={getValue()} size='small' bucket='coins' />
+            {getValue()}
+          </ZigTypography>
+        ),
+      }),
+      columnHelper.accessor('amount', {
+        header: t('common:amount'),
+        cell: ({ getValue }) => <ZigPriceLabel value={getValue()} usd />,
+      }),
+      columnHelper.accessor('email', {
+        header: t('table.user-trader'),
+        cell: ({ getValue }) => <ZigPriceLabel value={getValue()} usd />,
+      }),
+      columnHelper.accessor('type', {
+        header: t('table.user-trader'),
+        cell: ({ getValue }) => <ZigTypography>{getValue()}</ZigTypography>,
+      }),
+      columnHelper.accessor('status', {
+        header: t('table.user-trader'),
+        cell: ({ getValue }) => <ZigTypography>{getValue()}</ZigTypography>,
+      }),
+    ],
+    [],
+  );
+
+  const link =
+    window.location.protocol +
+    '//' +
+    window.location.host +
+    generatePath(ROUTE_REFERRALS_INVITE, { key: refCode });
+
+  return (
+    <PageContainer style={{ maxWidth: '960px' }}>
+      <LayoutContentWrapper
+        endpoint={[rewards, history]}
+        content={([rewardsData, referrals]: [
+          ReferralRewards,
+          ReferralHistory,
+        ]) => (
+          <>
+            <Box
+              sx={{
+                mt: 5,
+                justifyContent: 'center',
+                mb: 6,
+                display: 'flex',
+                flexDirection: 'row',
+              }}
+            >
+              <Box>
+                <CardGiftcardIcon
+                  sx={{
+                    width: 200,
+                    height: 200,
+                    mr: 4,
+                    ml: 4,
+                  }}
+                />
+              </Box>
+              <Box
+                sx={{
+                  maxWidth: 700,
+                  justifyContent: 'center',
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <ZigTypography
+                  sx={{
+                    mb: 1,
+                  }}
+                  variant={'h1'}
+                >
+                  {t('title')}
+                </ZigTypography>
+                <ZigTypography
+                  sx={{
+                    mb: 3,
+                  }}
+                >
+                  {t('description')}
+                </ZigTypography>
+
+                <InputText
+                  label={t('share-your-link')}
+                  readOnly={true}
+                  value={link}
+                  rightSideElement={
+                    <CloneIcon width={40} height={40} color={dark.neutral300} />
+                  }
+                  onClickRightSideElement={() => {
+                    copy(link);
+                    toast.success(t('action:copied'));
+                  }}
+                />
+              </Box>
+            </Box>
+
+            <Hr />
+
+            <Grid
+              container
+              sx={{
+                mt: 4,
+                mb: 4,
+              }}
+            >
+              <Grid item xs={12} md={8}>
+                <ZigTypography
+                  variant={'h2'}
+                  textAlign={'center'}
+                  sx={{ mt: 2, mb: 2.5 }}
+                >
+                  {t('what-you-get')}
+                </ZigTypography>
+                <Grid container>
+                  <Grid item xs={12} md={6}>
+                    <GetWhatYouDeserveLabel>
+                      <Trans
+                        i18nKey='referrals:invite-friends'
+                        t={t}
+                        values={{
+                          value: rewardsData.configuration.rewardSignupAmount,
+                          coin: rewardsData.configuration.rewardSignupSymbol,
+                          threshold:
+                            rewardsData.configuration.rewardMinDepositAmount,
+                        }}
+                      >
+                        <TotalBoxValue
+                          sx={{
+                            mt: 1.5,
+                            mb: 1.5,
+                          }}
+                        />
+                      </Trans>
+                    </GetWhatYouDeserveLabel>
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <GetWhatYouDeserveLabel>
+                      <Trans
+                        i18nKey='referrals:invite-traders'
+                        t={t}
+                        values={{
+                          value: rewardsData.configuration.zignalySuccessFee,
+                        }}
+                      >
+                        <TotalBoxValue
+                          sx={{
+                            mt: 1.5,
+                            mb: 1.5,
+                          }}
+                        />
+                      </Trans>
+                    </GetWhatYouDeserveLabel>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <ZigTypography
+                  variant={'h2'}
+                  textAlign={'center'}
+                  sx={{ mt: 5, mb: 5 }}
+                >
+                  {t('what-they-get')}
+                </ZigTypography>
+                <Grid container>
+                  <Grid item xs={12} md={12}>
+                    <GetWhatYouDeserveLabel>
+                      <Trans
+                        i18nKey='referrals:they-get-rewards'
+                        t={t}
+                        values={{
+                          value: rewardsData.configuration.rewardSignupAmount,
+                          coin: rewardsData.configuration.rewardSignupSymbol,
+                          threshold:
+                            rewardsData.configuration.rewardMinDepositAmount,
+                        }}
+                      >
+                        <TotalBoxValue
+                          sx={{
+                            mt: 1.5,
+                            mb: 1.5,
+                          }}
+                        />
+                      </Trans>
+                    </GetWhatYouDeserveLabel>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Hr />
+
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+                pt: 5,
+                pb: 5,
+              }}
+            >
+              <TotalBox
+                label={t('total-invitees')}
+                value={
+                  <>
+                    {rewardsData.invitedCount} <GroupIcon />
+                  </>
+                }
+              />
+              <TotalBox
+                label={t('total-rewards')}
+                value={<ZigPriceLabel usd value={rewardsData.usdtEarned} />}
+              />
+              <TotalBox
+                label={t('pending-rewards')}
+                value={<ZigPriceLabel usd value={rewardsData.usdtPending} />}
+              />
+            </Box>
+
+            <Box sx={{ mb: 6, mt: 3 }}>
+              <Grid container>
+                <Grid item xs={12} sm={6}>
+                  <ZigTypography
+                    sx={{
+                      mb: 3,
+                    }}
+                    variant={'h2'}
+                  >
+                    {t('table.title')}
+                  </ZigTypography>
+                </Grid>
+                <Grid item xs={12} sm={6}></Grid>
+              </Grid>
+              <ZigTable
+                initialState={{
+                  sorting: [
+                    {
+                      id: 'time',
+                      desc: true,
+                    },
+                  ],
+                }}
+                columns={columns}
+                data={referrals.history}
+                columnVisibility={false}
+                enableSortingRemoval={false}
+                emptyMessage={t('table.no-referrals')}
+              />
+            </Box>
+          </>
+        )}
+      />
+    </PageContainer>
+  );
 };
 
 export default Referrals;
---
+++ packages/ps2/src/views/Referrals/styles.ts
@@ -0,0 +1,35 @@
+import { Paper, styled } from '@mui/material';
+import { ZigTypography } from '@zignaly-open/ui';
+
+export const Hr = styled('hr')`
+  border-color: ${({ theme }) => theme.palette.neutral600};
+  border-width: 0;
+  border-bottom-width: 0.5px;
+`;
+
+export const TotalBoxBox = styled(Paper)`
+  border: 1px solid ${({ theme }) => theme.palette.neutral600};
+  width: 200px;
+  min-height: 120px;
+  margin: 16px;
+  padding: 16px 16px 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const TotalBoxValue = styled(ZigTypography)`
+  display: block;
+
+  &,
+  span {
+    color: ${({ theme }) => theme.palette.neutral000};
+    font-size: 30px;
+  }
+`;
+
+export const GetWhatYouDeserveLabel = styled(ZigTypography)`
+  display: block;
+  text-align: center;
+`;
