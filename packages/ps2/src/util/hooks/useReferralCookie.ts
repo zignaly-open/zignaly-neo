4404365285d7a95c0b2d8677dcf1fea28ab30e34
Christophe Wagner <chris.wagn22@gmail.com>
2023-03-01 14:17:55 +0700

    Move to useffect

--- packages/ps2/src/util/hooks/useReferralCookie.ts
+++ packages/ps2/src/util/hooks/useReferralCookie.ts
@@ -1,15 +1,18 @@
 import Cookies from 'js-cookie';
+import { useEffect } from 'react';
 
 /**
  * Save ref in cookies
  */
 const useReferralCookie = () => {
-  const params = new URLSearchParams(window.location.search);
-  const ref = params.get('invite');
-  const subtrack = params.get('subtrack');
-  if (ref) {
-    Cookies.set('ref', subtrack ? `${ref}:${subtrack}` : ref);
-  }
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const ref = params.get('invite');
+    const subtrack = params.get('subtrack');
+    if (ref) {
+      Cookies.set('ref', subtrack ? `${ref}:${subtrack}` : ref);
+    }
+  }, []);
 };
 
 export default useReferralCookie;
