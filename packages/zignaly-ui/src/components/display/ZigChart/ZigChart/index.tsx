fa3e7e32d20262c4cab0e5f6c3d46a70af11e57e
Christophe Wagner <chris.wagn22@gmail.com>
2023-03-20 19:33:58 +0700

    feat: restore ydomain adjustment

--- packages/zignaly-ui/src/components/display/ZigChart/ZigChart/index.tsx
+++ packages/zignaly-ui/src/components/display/ZigChart/ZigChart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useLayoutEffect, useMemo, useReducer, useRef } from "react";
+import React, { useCallback, useLayoutEffect, useReducer, useRef } from "react";
 import {
   VictoryArea,
   VictoryAxis,
@@ -29,7 +29,7 @@ const ZigChart = ({
   chartProps = {},
 }: ChartLargeProps) => {
   const theme = useTheme();
-  const { data: processedData, color, gradient } = useChartData(data, "full");
+  const { data: processedData, color, gradient, yDomain } = useChartData(data, "full");
   const wrapperRef = useRef<HTMLDivElement>(null);
   const width = wrapperRef?.current?.getBoundingClientRect().width;
   const pureChartWidth = width ? width - 70 - 2 : 0;
@@ -41,11 +41,6 @@ const ZigChart = ({
   useLayoutEffect(() => {
     forceUpdate();
   }, [width]);
-
-  const yDomain = useMemo(() => {
-    const values = processedData.map((s) => s.y);
-    return [Math.min(0, ...values), Math.max(1, ...values)];
-  }, [processedData]);
 
   const getChartLabel = useCallback(
     ({ datum = 0 }: { datum?: number }): string =>
