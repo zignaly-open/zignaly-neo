ee8f8b1aea4638342b74d1801d92615d095f8051
Christophe Wagner <chris.wagn22@gmail.com>
2023-03-27 12:53:29 +0700

    feat: drawer locale and exchange

--- packages/ps2/src/components/Navigation/Drawer/index.tsx
+++ packages/ps2/src/components/Navigation/Drawer/index.tsx
@@ -1,8 +1,13 @@
 import {
+  AccountCircle,
   CardGiftcardOutlined,
+  ChevronRight,
   CurrencyBitcoinOutlined,
+  ExpandLess,
+  ExpandMore,
   GifOutlined,
   Menu,
+  StarBorder,
   Storefront,
   WorkOutline,
 } from '@mui/icons-material';
@@ -21,9 +26,28 @@ import {
   Drawer,
   IconButton,
   Button,
+  Collapse,
+  ListItemIcon,
+  ListSubheader,
 } from '@mui/material';
-import { UserIcon, ZigButton } from '@zignaly-open/ui';
-import { useIsAuthenticated, useLogout } from 'apis/user/use';
+import {
+  Avatar,
+  DropDown,
+  UserIcon,
+  ZigButton,
+  ZigTypography,
+  IconButton as ZigIconButton,
+  GlobeLanguages,
+} from '@zignaly-open/ui';
+import { useFirstOwnedService, useTraderServices } from 'apis/service/use';
+import {
+  useActiveExchange,
+  useCurrentUser,
+  useIsAuthenticated,
+  useLogout,
+  useSelectExchange,
+} from 'apis/user/use';
+import { useZModal } from 'components/ZModal/use';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { generatePath, Link } from 'react-router-dom';
@@ -34,24 +58,64 @@ import {
   ROUTE_MY_BALANCES,
   ROUTE_LOGIN,
   ROUTE_SIGNUP,
+  ROUTE_TRADING_SERVICE_MANAGE,
+  ROUTE_BECOME_TRADER,
 } from 'routes';
 import theme from 'theme';
-import { LoginButton } from '../AccountMenu/styles';
+import { HELP_URL } from 'util/constants';
+import { supportedLanguages } from 'util/i18next';
+import { getImageOfAccount } from 'util/images';
+import { LocalizationLanguages } from 'util/languages';
+import socialNetworksLinks from 'util/socialNetworks';
+import Enable2FAModal from 'views/Settings/Enable2FAModal';
+import UpdatePasswordModal from 'views/Settings/UpdatePasswordModal';
+import { AccountName, LoginButton } from '../AccountMenu/styles';
+import { NavLink, Networks } from '../ExtraNavigationDropdown/styles';
 import { NavigationLink } from '../Header/atoms';
+import { AccountDropdown } from './styles';
 
 const drawerWidth = 240;
 const navItems = ['Home', 'About', 'Contact'];
 
 const ZigDrawer = () => {
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
   const logout = useLogout();
-  const { t } = useTranslation('common');
+  const { t, i18n } = useTranslation('common');
   const isAuthenticated = useIsAuthenticated();
+  const [open, setOpen] = useState(false);
+  const [languageOpen, setLanguageOpen] = useState(false);
+  const { showModal } = useZModal();
+  const { data: traderServices, isFetching } = useTraderServices();
+  const service = useFirstOwnedService();
+  const activeExchange = useActiveExchange();
+  const { exchanges, email, imageUrl } = useCurrentUser();
 
+  const selectExchange = useSelectExchange();
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const languageMap = supportedLanguages
+    ? supportedLanguages.map((x) => LocalizationLanguages[x])
+    : Object.values(LocalizationLanguages);
+
+  const onSelectLocale = (locale: string) => {
+    changeLocale(locale);
+    onClose();
+  };
+
+  const handleSelectLanguage = (locale: string) => {
+    onSelectLocale(locale);
+  };
+
+  const setActiveExchange = (exchangeInternalId: string) => {
+    selectExchange(exchangeInternalId);
+  };
   return (
     <>
       <IconButton
@@ -80,28 +144,176 @@ const ZigDrawer = () => {
           }}
         >
           <Box
-            onClick={handleDrawerToggle}
             display='flex'
             flexDirection='column'
             textAlign='center'
             gap={2}
+            justifyContent='center'
           >
-            <Typography variant='h6' sx={{ my: 2 }}>
-              MUI
-            </Typography>
+            <Box
+              sx={{ my: 2 }}
+              display='flex'
+              flexDirection='column'
+              alignItems='center'
+              gap={2}
+            >
+              {true ? (
+                <Avatar
+                  image={
+                    'https://cdn-icons-png.flaticon.com/512/685/685681.png'
+                  }
+                  size={'large'}
+                />
+              ) : (
+                <AccountCircle />
+              )}
+              <ZigTypography variant='h6'>{email}</ZigTypography>
+            </Box>
+            <DropDown
+              component={({ open }) => (
+                // <ZigIconButton
+                //   id={'menu__dropdown-account'}
+                //   variant={'flat'}
+                //   icon={
+                //     <Avatar size={'medium'} image={activeExchange?.image} />
+                //   }
+                //   key={'user'}
+                //   isFocused={open}
+                // />
+                <AccountDropdown>
+                  <Avatar size={'medium'} image={activeExchange?.image} />
+                  <AccountName variant={'body1'} color={'neutral100'}>
+                    {activeExchange?.internalName}
+                  </AccountName>
+                  {open ? <ExpandLess /> : <ChevronRight />}
+                </AccountDropdown>
+              )}
+              options={(exchanges?.length > 1 ? exchanges : []).map(
+                (exchange, index) => ({
+                  onClick: () => setActiveExchange(exchange.internalId),
+                  id: `account-switcher-dropdown__account-${index}`,
+                  label: (
+                    <>
+                      <Avatar
+                        size={'medium'}
+                        image={getImageOfAccount(index)}
+                      />
+                      <AccountName
+                        variant={'body1'}
+                        color={
+                          activeExchange?.internalId === exchange.internalId
+                            ? 'highlighted'
+                            : 'neutral200'
+                        }
+                      >
+                        {exchange.internalName}
+                      </AccountName>
+                    </>
+                  ),
+                }),
+              )}
+            />
+
             <Divider />
             <List>
-              {navItems.map((item) => (
-                <ListItem key={item} disablePadding>
-                  <ListItemButton sx={{ textAlign: 'center' }}>
-                    <ListItemText primary={item} />
+              {isAuthenticated && (
+                <>
+                  {/* <ListItemButton>
+                      <ListItemIcon>
+                        <SendIcon />
+                      </ListItemIcon>
+                      <ListItemText primary='Sent mail' />
+                    </ListItemButton> */}
+                  {/* <ListItemButton>
+                      <ListItemIcon>
+                        <DraftsIcon />
+                      </ListItemIcon>
+                      <ListItemText primary='Drafts' />
+                    </ListItemButton> */}
+                  <ListItemButton
+                    onClick={handleClick}
+                    sx={{ textAlign: 'center', pl: 5 }}
+                  >
+                    {/* <ListItemIcon>
+                        <InboxIcon />
+                      </ListItemIcon> */}
+                    <ListItemText
+                      primary={t('account-menu.notAuth-dropdown-link-settings')}
+                    />
+                    {open ? <ExpandLess /> : <ChevronRight />}
                   </ListItemButton>
-                </ListItem>
-              ))}
+                  <Collapse in={open} timeout='auto' unmountOnExit>
+                    <List component='div' disablePadding>
+                      <ListItemButton
+                        onClick={() => showModal(UpdatePasswordModal)}
+                        sx={{ textAlign: 'center' }}
+                      >
+                        <ListItemText
+                          primary={t(
+                            'account-menu.notAuth-dropdown-link-password',
+                          )}
+                        />
+                      </ListItemButton>
+                      <ListItemButton
+                        onClick={() => showModal(Enable2FAModal)}
+                        sx={{ textAlign: 'center' }}
+                      >
+                        <ListItemText
+                          primary={t('account-menu.notAuth-dropdown-link-2fa')}
+                        />
+                      </ListItemButton>
+                    </List>
+                  </Collapse>
+                  <ListItem disablePadding>
+                    <ListItemButton
+                      target='_blank'
+                      href={ROUTE_BECOME_TRADER}
+                      sx={{ textAlign: 'center' }}
+                      id='drawer__become-trader'
+                    >
+                      <ListItemText
+                        primary={t('navigation-menu.become-trader')}
+                      />
+                    </ListItemButton>
+                  </ListItem>
+                  {service && (
+                    <ListItem disablePadding>
+                      <ListItemButton
+                        target='_blank'
+                        href={generatePath(ROUTE_TRADING_SERVICE_MANAGE, {
+                          serviceId: service.serviceId?.toString(),
+                        })}
+                        sx={{ textAlign: 'center' }}
+                        id='drawer__for-trading'
+                      >
+                        <ListItemText
+                          primary={t('main-menu.dropdown-link-forTrading')}
+                        />
+                      </ListItemButton>
+                    </ListItem>
+                  )}
+                </>
+              )}
+              <ListItem disablePadding>
+                <ListItemButton
+                  target='_blank'
+                  href={HELP_URL}
+                  sx={{ textAlign: 'center' }}
+                >
+                  <ListItemText
+                    primary={t('main-menu.dropdown-link-helpDocs')}
+                  />
+                </ListItemButton>
+              </ListItem>
             </List>
 
             {isAuthenticated ? (
-              <ZigButton onClick={logout}>
+              <ZigButton
+                onClick={() => {
+                  logout();
+                  handleClick();
+                }}
+              >
                 {t('account-menu.notAuth-button-logOut')}
               </ZigButton>
             ) : (
@@ -129,6 +341,64 @@ const ZigDrawer = () => {
                 </Link>
               </>
             )}
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={() => setLanguageOpen(!languageOpen)}
+                  sx={{ textAlign: 'center' }}
+                >
+                  <ListItemIcon>
+                    <GlobeLanguages
+                      color={theme.palette.neutral300}
+                      width={'26px'}
+                      height={'26px'}
+                    />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={
+                      LocalizationLanguages[i18n.language?.split('_')[0]]?.label
+                    }
+                  />
+
+                  {languageOpen ? <ExpandLess /> : <ChevronRight />}
+                </ListItemButton>
+              </ListItem>
+              <Collapse in={languageOpen} timeout='auto' unmountOnExit>
+                <List component='div' disablePadding>
+                  {languageMap.map((language, index) => (
+                    <ListItemButton
+                      key={language.locale}
+                      onClick={() => handleSelectLanguage(language.locale)}
+                      sx={{ textAlign: 'center' }}
+                      id={`drawer-languages__${index.toString()}`}
+                    >
+                      <ListItemText primary={language.label} />
+                    </ListItemButton>
+                  ))}
+                </List>
+              </Collapse>
+            </List>
+            <ZigTypography mt={4} variant='body2'>
+              {t('follow-us')}
+            </ZigTypography>
+            <Box display='flex' justifyContent='center' mt={-1}>
+              <Networks>
+                {socialNetworksLinks.map((socialNetwork, index) => {
+                  const IconComponent = socialNetwork.image;
+                  return (
+                    <NavLink
+                      onClick={handleClick}
+                      href={socialNetwork.path}
+                      key={`--social-network-nav-link-${index.toString()}`}
+                      id={`drawer__social-network-${index.toString()}`}
+                      target={'_blank'}
+                    >
+                      <IconComponent height={'22px'} width={'22px'} />
+                    </NavLink>
+                  );
+                })}
+              </Networks>
+            </Box>
           </Box>
         </Drawer>
       </Box>
---
+++ packages/ps2/src/components/Navigation/Drawer/styles.ts
@@ -0,0 +1,20 @@
+import { styled } from '@mui/system';
+
+export const AccountDropdown = styled('div')`
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+  /* display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+  padding-top: 7px;
+  padding-bottom: 7px;
+  overflow: hidden;
+
+  span {
+    overflow: hidden;
+    text-overflow: ellipsis;
+  } */
+`;
