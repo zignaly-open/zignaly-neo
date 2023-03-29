b1c4492adcda2916d2dee7e048afa2b0410a67ea
Alexander Mikhalchenko <alex@xfuturum.com>
2023-03-29 04:23:00 +0400

    feat: rewards - types
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

---
+++ packages/ps2/src/apis/rewards/store.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const rewardApiSlice = createSlice({
+  name: 'rewardApi',
+  initialState,
+  reducers: {},
+});
+
+export default rewardApiSlice.reducer;
---
+++ packages/ps2/src/apis/rewards/types.ts
@@ -0,0 +1,5 @@
+export type RewardsState = Record<string, never>;
+
+export type Reward = {};
+
+export type RewardClaimed = {};
