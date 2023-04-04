71aa6b34091889f5d5d7dfb897419019c028c113
Alexander Mikhalchenko <alex@xfuturum.com>
2023-04-05 03:02:57 +0400

    chore: add suspense to the test wrapper
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/util/test.tsx
+++ packages/ps2/src/util/test.tsx
@@ -2,11 +2,15 @@ import { render } from '@testing-library/react';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { WrappedInProviders } from '../App';
 import { store } from '../apis/store';
+import { Suspense } from 'react';
 
 export function renderWithProviders(ui: JSX.Element, renderOptions = {}) {
   setupListeners(store.dispatch);
   return {
     store,
-    ...render(ui, { wrapper: WrappedInProviders, ...renderOptions }),
+    ...render(<Suspense fallback={null}>{ui}</Suspense>, {
+      wrapper: WrappedInProviders,
+      ...renderOptions,
+    }),
   };
 }
