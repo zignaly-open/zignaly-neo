ef9af0a5f1ef0ae6a952efbcf0e2d9209611ce39
Alexander Mikhalchenko <alex@xfuturum.com>
2023-04-04 06:24:36 +0400

    chore: test setup
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/setupTests.ts
+++ packages/ps2/src/setupTests.ts
@@ -3,3 +3,24 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// import * as lodash from 'lodash';
+jest.mock('react-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  },
+}));
+
+// console.error(lodash);
+//
+jest.mock('react-image-webp/dist/utils', () => ({
+  isWebpSupported: () => () => true,
+}));
