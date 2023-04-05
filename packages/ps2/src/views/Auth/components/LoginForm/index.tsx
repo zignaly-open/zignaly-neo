351003c02c48a8824df209c4d5f935ca0ccd1910
Alexander Mikhalchenko <alex@xfuturum.com>
2023-04-05 04:12:42 +0400

    chore: remove commented
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/views/Auth/components/LoginForm/index.tsx
+++ packages/ps2/src/views/Auth/components/LoginForm/index.tsx
@@ -7,13 +7,7 @@ import { LoginValidation } from './validations';
 import { useAuthenticate } from '../../../../apis/user/use';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { ROUTE_FORGOT_PASSWORD, ROUTE_SIGNUP } from '../../../../routes';
-import {
-  // Button,
-  TextButton,
-  Typography,
-  ZigButton,
-  ZigInput,
-} from '@zignaly-open/ui';
+import { TextButton, Typography, ZigButton, ZigInput } from '@zignaly-open/ui';
 import { Box } from '@mui/material';
 import { LoginPayload } from '../../../../apis/user/types';
 import PasswordVisibilityAdornment from '../atoms/PasswordVisibilityAdornment';
