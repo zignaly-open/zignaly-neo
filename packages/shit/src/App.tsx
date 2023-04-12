731c35bc966984403f129d65fa29be436a152bec
Alexander Mikhalchenko <alex@xfuturum.com>
2023-04-12 05:39:11 +0400

    chore: tree-shaking works sorta
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/shit/src/App.tsx
+++ packages/shit/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { dummy } from '@zignaly-open/ui';
+import { littleShit } from '@zignaly-open/ui2';
 
-console.error(dummy);
+console.error(littleShit);
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          {littleShit}
         </a>
       </header>
     </div>
