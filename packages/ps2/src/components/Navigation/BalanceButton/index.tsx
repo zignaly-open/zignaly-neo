d690e375a41c3889c9a15615bfe95c5a79921b75
Christophe Wagner <chris.wagn22@gmail.com>
2023-05-10 13:42:31 +0200

    Revert "fix(PS2): add periodic call to /investments"

--- packages/ps2/src/components/Navigation/BalanceButton/index.tsx
+++ packages/ps2/src/components/Navigation/BalanceButton/index.tsx
@@ -13,20 +13,16 @@ import { useZModal } from 'components/ZModal/use';
 import DepositModal from 'views/Dashboard/components/ManageInvestmentModals/DepositModal';
 import { GradientBorderButtonWrapper } from '../ReferralButton/atoms';
 
-const UPDATE_INTERVAL = 60 * 1000;
-
 const BalanceButton = () => {
   const { t } = useTranslation('common');
   const { internalId } = useActiveExchange();
-  const { data: investments } = useInvestmentsQuery(internalId, {
-    pollingInterval: UPDATE_INTERVAL,
-  });
+  const { data: investments } = useInvestmentsQuery(internalId);
   const { data: balance } = useBalanceQuery(
     {
       exchangeInternalId: internalId,
     },
     {
-      pollingInterval: UPDATE_INTERVAL,
+      pollingInterval: 60 * 1000,
     },
   );
   const { showModal } = useZModal();
