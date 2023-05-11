4715bcf630caf827d77990ede1e9836788b25705
Christophe Wagner <chris.wagn22@gmail.com>
2023-05-11 19:35:02 +0700

    fix: restore wallet margin Not enough margin on medium screens

--- packages/ps2/src/views/Wallet/index.tsx
+++ packages/ps2/src/views/Wallet/index.tsx
@@ -34,7 +34,7 @@ const Wallet = () => {
           WalletBalances,
           TotalSavings,
         ]) => (
-          <>
+          <Box mx='22px'>
             <Box
               display='flex'
               gap={1}
@@ -54,7 +54,7 @@ const Wallet = () => {
             />
             <WalletCoinsTable balances={balances} coins={coins} />
             <WalletTransactions />
-          </>
+          </Box>
         )}
       />
     </PageContainer>
