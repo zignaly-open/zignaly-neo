2817ccaf26967c75c76a71b9b714fb4b888afc00
Christophe Wagner <chris.wagn22@gmail.com>
2023-05-12 20:52:06 +0700

    fix: improve ZigButtonGroup styles

--- packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
+++ packages/zignaly-ui/src/components/inputs/ZigButton/index.tsx
@@ -64,17 +64,19 @@ const ZigButton = ({
 };
 
 export const ZigButtonGroup = styled(ButtonGroup)`
+  outline: 1px solid ${({ theme }) => theme.palette.neutral600};
+  outline-offset: -1px;
+
   .MuiButton-root {
-    border-right-width: 0 !important;
-
-    &:last-child {
-      border-right-width: 1px !important;
-    }
-
     &:hover,
     &.MuiButton-active {
       z-index: 3;
-      box-shadow: 1px 0 0 ${dark.neutral400};
+      border-color: #464a85 !important;
+      border-radius: 5px;
+
+      + button {
+        border-left-color: transparent;
+      }
     }
 
     &.Mui-disabled {
