534c730622df33ef04f9ac33393a88cf1a4d9b89
Christophe Wagner <chris.wagn22@gmail.com>
2023-05-16 14:06:31 +0700

    fix: move title more to the right

--- packages/ps2/src/views/TraderService/components/ServiceProfileContainer/index.tsx
+++ packages/ps2/src/views/TraderService/components/ServiceProfileContainer/index.tsx
@@ -29,10 +29,11 @@ const ServiceProfileContainer: React.FC<{ service: Service }> = ({
       }}
     >
       <Grid container>
-        <Grid item sx={{ display: 'flex' }} xs={12} md={8} pb={4} pr={[0, 7.5]}>
+        <Grid item md={1} />
+        <Grid item sx={{ display: 'flex' }} xs={12} md={9} pb={4}>
           <ServiceProfileHeader service={service} />
         </Grid>
-        <Grid item xs={12} md={4} pb={4}>
+        <Grid item xs={12} md={2} pb={4}>
           <RightSideActions service={service} />
         </Grid>
         <Grid
