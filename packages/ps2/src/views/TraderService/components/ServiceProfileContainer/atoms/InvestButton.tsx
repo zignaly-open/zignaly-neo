49f92cf24ae0d5b4e0f76c0766907478122ee2df
Nikita9901 <nikitichsn2@gmail.com>
2023-05-17 12:36:09 +0300

    chore: lowercase text invest button

--- packages/ps2/src/views/TraderService/components/ServiceProfileContainer/atoms/InvestButton.tsx
+++ packages/ps2/src/views/TraderService/components/ServiceProfileContainer/atoms/InvestButton.tsx
@@ -90,6 +90,7 @@ const InvestButton: React.FC<{
               component='p'
               color='neutral150'
               fontWeight={500}
+              textTransform={'lowercase'}
             >
               {t('invest-button.x-success-fee', {
                 fee: service.successFee,
