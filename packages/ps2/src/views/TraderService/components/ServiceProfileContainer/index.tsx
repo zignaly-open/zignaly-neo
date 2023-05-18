a5e36ba082b5be877afbd6034c7fbb97ae33a793
Christophe Wagner <chris.wagn22@gmail.com>
2023-05-18 13:32:14 +0700

    fix: padding service fixes

--- packages/ps2/src/views/TraderService/components/ServiceProfileContainer/index.tsx
+++ packages/ps2/src/views/TraderService/components/ServiceProfileContainer/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Service } from '../../../../apis/service/types';
 import { useCoinBalances } from '../../../../apis/coin/use';
 import ServiceProfileHeader from './atoms/ServiceProfileHeader';
-import { useIsServiceOwner } from '../../../../apis/service/use';
 import { Box, Grid, useMediaQuery } from '@mui/material';
 import theme from '../../../../theme';
 import RightSideActions from './atoms/RightSideActions';
@@ -16,16 +15,12 @@ const ServiceProfileContainer: React.FC<{ service: Service }> = ({
 }) => {
   // we do not use the results of this till before the modal
   useCoinBalances();
-  const isOwner = useIsServiceOwner(service.id);
-  const md = useMediaQuery(theme.breakpoints.up('sm'));
-
   return (
     <Box
       sx={{
         p: 2,
-        pt: { xs: 4, md: isOwner ? '11px' : 0 },
-        pl: md ? 6 : 2,
-        pr: md ? 6 : 2,
+        pt: 0,
+        mt: { xs: '-15px', md: '-45px' },
       }}
     >
       <Grid container>
