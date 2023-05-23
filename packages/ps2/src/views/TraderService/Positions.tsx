3d4e545caddc745a14695f0ee51df0f210f60296
Alexander Mikhalchenko <alex@xfuturum.com>
2023-05-23 08:14:03 +0300

    fix: fixed spinner jerking vertically
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/views/TraderService/Positions.tsx
+++ packages/ps2/src/views/TraderService/Positions.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ComingSoon from '../../components/Stub/ComingSoon';
-import { TraderServicePageContainer } from './components/styles';
 import { useParams } from 'react-router-dom';
 import { useTraderServiceTitle } from '../../apis/service/use';
+import { PageContainer } from '@zignaly-open/ui';
 
 const Positions: React.FC = () => {
   const { serviceId } = useParams();
   useTraderServiceTitle('profit-sharing.positions', serviceId);
   return (
-    <TraderServicePageContainer>
+    <PageContainer>
       <ComingSoon />
-    </TraderServicePageContainer>
+    </PageContainer>
   );
 };
 
