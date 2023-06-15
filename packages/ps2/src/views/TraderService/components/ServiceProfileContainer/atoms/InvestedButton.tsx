291f5851153a7f87cef51f28776449beec6ed87e
Nikita9901 <nikitichsn2@gmail.com>
2023-06-15 13:44:13 +0300

    feat: edit investment button design

--- packages/ps2/src/views/TraderService/components/ServiceProfileContainer/atoms/InvestedButton.tsx
+++ packages/ps2/src/views/TraderService/components/ServiceProfileContainer/atoms/InvestedButton.tsx
@@ -27,7 +27,7 @@ const BigNumber: React.FC<{
         coin={ssc}
         shorten={shorten}
         color={green ? 'greenGraph' : red ? 'redGraphOrError' : undefined}
-        variant={'h1'}
+        variant={'h2'}
         coinProps={{
           paddingTop: '6px',
         }}
@@ -87,7 +87,7 @@ export const InvestedButtonBase: React.FC<{
       </TopDivider>
 
       <Box id={prefixId && `${prefixId}__invested-${service.id}`}>
-        <BigNumber ssc={service.ssc} value={investedAmount} green shorten />
+        <BigNumber ssc={service.ssc} value={investedAmount} green />
       </Box>
       <Box
         sx={{
