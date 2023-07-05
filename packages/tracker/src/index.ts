b3f291c4942a678089d693f8f38b9b0ad4e22ca8
Alexander Mikhalchenko <alex@xfuturum.com>
2023-07-06 01:21:37 +0300

    feat:  the new tracking system
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/tracker/src/index.ts
+++ packages/tracker/src/index.ts
@@ -6,11 +6,6 @@ type tzData = {
   tzid?: string;
 };
 
-const lastDelayedTrack = {
-  event: null as tzData,
-  timeout: null as ReturnType<typeof setTimeout>,
-};
-
 const sendTz = (data: tzData) => {
   const options = {
     method: 'POST',
@@ -21,41 +16,6 @@ const sendTz = (data: tzData) => {
   };
 
   return fetch('https://zignaly.com/new_api/tz', options);
-};
-
-const delayTimeout = 100;
-
-/**
- * We have a problem with delayed track
- * because we track buttons and links AND we track url changed
- * @param data
- */
-const sendTzDelayed = (data: tzData) => {
-  const to = data.urlDestination;
-  const from = lastDelayedTrack.event?.urlDestination;
-  const sameUser = lastDelayedTrack.event?.userId === data?.userId;
-
-  if (sameUser && to === from)
-    // this is mainly intended to fix the react 18 double mount in dev mode
-    return;
-
-  // here we want tio check if we're opening a new view or no
-  // like, profit-sharing#edit-investment -> profit-sharing#edit-investment?ctaId=marketplace-table__edit-62fe886f8aedc6579c1d275f" within 1 s is no new view
-  if (
-    sameUser &&
-    (to?.indexOf(from + '#') === 0 || to?.indexOf(from + '?ctaId=') === 0)
-  ) {
-    // if the timeout has already executed, no harm no foul
-    clearTimeout(lastDelayedTrack.timeout);
-    // we effectively merge the two events
-    data.urlReferer = lastDelayedTrack.event?.urlReferer;
-  }
-
-  lastDelayedTrack.event = data;
-  lastDelayedTrack.timeout = setTimeout(() => {
-    lastDelayedTrack.event = null;
-    sendTz(data);
-  }, delayTimeout);
 };
 
 /**
@@ -89,30 +49,34 @@ const triggerTz = async (
     localStorage.setItem('tid', json);
   }
 
-  await sendTzDelayed(data);
+  return sendTz(data);
 };
 
 let referrer = document.referrer;
 
 export const track = ({
   location = '',
-  ctaId = '',
   hash = '',
   userId = '',
 }: {
   location?: string;
   hash?: string;
-  ctaId?: string;
   userId?: string;
 }) => {
   const url = new URL(location || window.location.href);
-  url.hash =
-    (hash || url.hash?.split('?')[0]) + (ctaId ? `?ctaId=${ctaId}` : '');
-  triggerTz(url.toString(), userId, referrer);
-  referrer = url.toString();
+  url.hash = hash || url.hash?.split('?')[0];
+  // we need a timeout to make sure thaat when a click and a track are triggered simultaneously, click always comes first
+  // this is not the case with the click event, so gotta use mousedown
+  // mousedown does come first, but these 50ms are here just in case
+  setTimeout(() => {
+    // this is to prevent the double track event sent thanks to react 18 double mount in dev mode
+    if(url.toString() !== referrer)
+      triggerTz(url.toString(), userId, referrer);
+    referrer = url.toString();
+  }, 50);
 };
 
-export const trackCta = ({
+export const trackClick = ({
   ctaId = '',
   userId,
 }: {
