d775e938646cee714c34e5f75884b1c1ca76b214
Alexander Mikhalchenko <alex@xfuturum.com>
2023-07-11 16:14:25 +0300

    chore: deploy without the wl config
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/theme/index.ts
+++ packages/zignaly-ui/src/theme/index.ts
@@ -1,3 +1,3 @@
-// export { default as dark, darkMui } from "./dark";
-export { default as dark, exampleMui as darkMui } from "./example";
+export { default as dark, darkMui } from "./dark";
+// export { default as dark, exampleMui as darkMui } from "./example";
 export type { default as ThemeType } from "./theme";
