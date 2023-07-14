1a0eb3eabf0a6b39eb3deb8dfe856112a997b585
Christophe Wagner <chris.wagn22@gmail.com>
2023-07-14 17:21:20 +0700

    chore: update invite button

--- packages/ps2/src/views/TraderService/components/ServiceProfileContainer/atoms/RightSideActions.tsx
+++ packages/ps2/src/views/TraderService/components/ServiceProfileContainer/atoms/RightSideActions.tsx
@@ -9,16 +9,18 @@ import { Service } from '../../../../../apis/service/types';
 import { useMediaQuery } from '@mui/material';
 import theme from '../../../../../theme';
 import { RightSideActionWrapper } from '../styles';
-import { Loader, ZigButton } from '@zignaly-open/ui';
+import { Loader, ZigButton, ZigInviteIcon } from '@zignaly-open/ui';
 import { ROUTE_PROFIT_SHARING_SERVICE_INVEST } from '../../../../../routes';
 import { useZModal } from 'components/ZModal/use';
 import ReferralsInviteModal from '../../ReferralsInviteModal';
+import { useTranslation } from 'react-i18next';
 
 const RightSideActions: React.FC<{ service: Service }> = ({ service }) => {
   const isAuthenticated = useIsAuthenticated();
   const isInvested = useIsInvestedInService(service.id);
   const md = useMediaQuery(theme.breakpoints.up('sm'));
   const { showModal } = useZModal();
+  const { t } = useTranslation('service');
 
   return (
     <RightSideActionWrapper>
@@ -29,10 +31,9 @@ const RightSideActions: React.FC<{ service: Service }> = ({ service }) => {
       )}
 
       {!isInvested.isLoading && !service.liquidated && (
-        <Box sx={{ mt: md ? 0 : 3 }}>
+        <Box sx={{ mt: md ? 0 : 3 }} display='flex' gap={3}>
           <ZigButton
-            // modalRoute={ROUTE_PROFIT_SHARING_SERVICE_INVEST}
-            // ctaId={'service-profile-invest-button'}
+            ctaId={'service-profile-invite-button'}
             onClick={() =>
               showModal(ReferralsInviteModal, {
                 service,
@@ -40,7 +41,15 @@ const RightSideActions: React.FC<{ service: Service }> = ({ service }) => {
               })
             }
           >
-            Invite
+            <Box
+              display={'flex'}
+              flexDirection={'row'}
+              gap={1}
+              alignItems={'center'}
+            >
+              <ZigInviteIcon width={20} height={'auto'} />
+              {t('invite')}
+            </Box>
           </ZigButton>
           {isAuthenticated && isInvested.thisAccount ? (
             <InvestedButton prefixId={'service-profile'} service={service} />
