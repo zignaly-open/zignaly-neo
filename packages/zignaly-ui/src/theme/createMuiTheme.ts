79b8b5d0835346a740e531c9f3d3d78ebf65eac5
Alexander Mikhalchenko <alex@xfuturum.com>
2023-07-18 14:20:06 +0300

    chore: adjusted colors hopefully one last time in this branch
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/theme/createMuiTheme.ts
+++ packages/zignaly-ui/src/theme/createMuiTheme.ts
@@ -403,7 +403,7 @@ export default ({ palette, mode, chart, backgrounds, boxShadows, fontFamily }: T
             },
             "&:hover, &.MuiButton-active": {
               color: palette.neutral000,
-              borderColor: backgrounds.greyedOutBorder,
+              borderColor: palette.neutral400,
             },
           },
         },
