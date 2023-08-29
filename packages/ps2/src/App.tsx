91b5cfe03cf14b85650f6270e41b1b93805c2e70
Christophe Wagner <chris.wagn22@gmail.com>
2023-08-29 19:17:36 +0200

    fix: restore UserKycChecker

--- packages/ps2/src/App.tsx
+++ packages/ps2/src/App.tsx
@@ -87,6 +87,7 @@ function App() {
           <>
             <Tracker />
             <UpdateChecker />
+            <UserKycChecker />
             <DateLocaleFixer />
             <ChartGradients />
             <Router />
