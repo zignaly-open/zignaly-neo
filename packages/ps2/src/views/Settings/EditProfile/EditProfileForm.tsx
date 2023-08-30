41776d05c4976e9d0fda2f3b41b908bc3681bba3
Christophe Wagner <chris.wagn22@gmail.com>
2023-08-30 11:08:33 +0200

    fix: add username error message

--- packages/ps2/src/views/Settings/EditProfile/EditProfileForm.tsx
+++ packages/ps2/src/views/Settings/EditProfile/EditProfileForm.tsx
@@ -199,7 +199,7 @@ const EditProfileForm = () => {
                     wide
                     label={t('edit-profile.username')}
                     placeholder={t('edit-profile.username')}
-                    error={t(errors.username?.message)}
+                    error={t(errors.username?.message, { maxLength: 60 })}
                     {...field}
                   />
                 )}
