46bf17609e693b26947d1f86887665fd767eac28
Nikita <nikitichsn2@gmail.com>
2023-08-30 13:47:58 +0300

    fix: invite modal title design

--- packages/ps2/src/components/ZModal/ModalContainer/types.ts
+++ packages/ps2/src/components/ZModal/ModalContainer/types.ts
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import { SxProps } from '@mui/system';
 
 export type ModalContainerProps = {
   children: React.ReactElement[] | React.ReactElement;
@@ -8,4 +9,5 @@ export type ModalContainerProps = {
   padding?: number;
   onClickClose?: () => void;
   onGoBack?: () => void;
+  titleStyles?: SxProps;
 };
