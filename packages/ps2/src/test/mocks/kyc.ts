d0e48e677653d6f5307ec00a37f4e69cf2329db2
Nikita <nikitichsn2@gmail.com>
2023-09-08 12:14:57 +0300

    feat: add tests kyc

---
+++ packages/ps2/src/test/mocks/kyc.ts
@@ -0,0 +1,26 @@
+export const mockKycStatuses = [
+  {
+    status: 'init',
+    level: 'QUA_individual_sandbox',
+    order: 1,
+    category: 'KYC',
+  },
+  {
+    status: 'init',
+    level: 'QUA_individual_sandbox_2',
+    order: 2,
+    category: 'KYC',
+  },
+  {
+    status: 'init',
+    level: 'QUA_individual_sandbox_KYB',
+    order: 1,
+    category: 'KYB',
+  },
+  {
+    status: null,
+    level: 'QUA_individual_sandbox_KYB_2',
+    order: 2,
+    category: 'KYB',
+  },
+];
---
+++ packages/ps2/src/views/Settings/Kyc/index.test.tsx
@@ -0,0 +1,53 @@
+import { fireEvent, waitFor } from '@testing-library/react';
+import React from 'react';
+import { default as KycComponent } from './index'; // Import your Kyc component
+import { renderWithProviders } from '../../../util/test';
+import { mockKycStatuses } from '../../../test/mocks/kyc';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => key, // Mock translation function
+  }),
+}));
+
+// Mock the currentUser and related functions
+const mockCurrentUser = {
+  KYCMonitoring: true, // Adjust as needed based on your component's logic
+};
+
+const mockLoadUser = jest.fn(); // Mock the loadUser function
+
+jest.mock('../../../apis/user/api', () => ({
+  useCurrentUser: () => mockCurrentUser,
+  useLazyUserQuery: () => [mockLoadUser],
+  useKycStatusesQuery: () => ({
+    data: mockKycStatuses,
+  }),
+}));
+
+test('renders the Kyc component', () => {
+  const { getByText } = renderWithProviders(<KycComponent />);
+  const titleElement = getByText('title');
+  const descriptionElement = getByText('description');
+  expect(titleElement).toBeInTheDocument();
+  expect(descriptionElement).toBeInTheDocument();
+});
+
+test('switches between KYC and KYB tabs', async () => {
+  const { getByText } = renderWithProviders(<KycComponent />);
+
+  // Initially, the KYC tab should be active
+  const kycTab = getByText('KYC');
+  const kybTab = getByText('KYB');
+  expect(kycTab).toHaveClass('Mui-selected');
+  expect(kybTab).not.toHaveClass('Mui-selected');
+
+  // Click on the KYB tab
+  fireEvent.click(kybTab);
+
+  await waitFor(() => {
+    // Now, the KYB tab should be active
+    expect(kycTab).not.toHaveClass('Mui-selected');
+    expect(kybTab).toHaveClass('Mui-selected');
+  });
+});
