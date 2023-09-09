f9b56fc5ecde097ef96147c31621278931be2e26
Alexander Mikhalchenko <alex@xfuturum.com>
2023-09-09 18:40:21 +0300

    feat(Backoffice): minor header fixes
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/backoffice/src/components/BackofficeHeader/index.tsx
+++ packages/backoffice/src/components/BackofficeHeader/index.tsx
@@ -4,7 +4,7 @@ import { NavigationLink } from './atoms';
 import { Box, Toolbar } from '@mui/material';
 import { Container, StyledAppBar } from './styles';
 import { ROUTE_LOGIN, ROUTE_TRANSACTIONS, ROUTE_USERS } from '../../routes';
-import { useIsAuthenticated, useLogout } from '../../apis/user/use';
+import { useIsAuthenticated, useLogout } from '../../apis/session/use';
 import { useTranslation } from 'react-i18next';
 
 const BackofficeHeader: React.FC = () => {
@@ -19,17 +19,11 @@ const BackofficeHeader: React.FC = () => {
           <Container>
             <Box display='flex' alignItems='center' gap='28px'>
               <NavigationLink
-                id='menu__logo'
                 style={{ display: 'flex' }}
                 to={ROUTE_USERS}
                 key='--route-main'
               >
-                <BrandImage
-                  id='menu__logo'
-                  height='32px'
-                  type='isotype'
-                  width='32px'
-                />
+                <BrandImage height='32px' type='isotype' width='32px' />
               </NavigationLink>
 
               <HeaderLinksContainer key='links'>
@@ -41,19 +35,20 @@ const BackofficeHeader: React.FC = () => {
                     <NavigationLink to={ROUTE_TRANSACTIONS} key='--route-users'>
                       {t('navigation.transactions')}
                     </NavigationLink>
-                    <a
-                      id='logout'
-                      onClick={() => logout()}
-                      key='--route-logout'
-                    >
-                      {t('navigation.logout')}
-                    </a>
                   </>
                 ) : (
                   <NavigationLink to={ROUTE_LOGIN} key='--route-ps'>
                     {t('navigation.login')}
                   </NavigationLink>
                 )}
+              </HeaderLinksContainer>
+            </Box>
+
+            <Box display='flex' alignItems='center' gap='28px'>
+              <HeaderLinksContainer>
+                <a id='logout' onClick={() => logout()} key='--route-logout'>
+                  {t('navigation.logout')}
+                </a>
               </HeaderLinksContainer>
             </Box>
           </Container>
