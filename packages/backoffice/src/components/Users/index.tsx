2abe57524abd4e7b4d945b5d458ef92bdb802e2e
Alexander Mikhalchenko <alex@xfuturum.com>
2023-09-12 02:48:18 +0300

    feat(Backoffice): ban users
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/backoffice/src/components/Users/index.tsx
+++ packages/backoffice/src/components/Users/index.tsx
@@ -7,13 +7,19 @@ import {
   ZigTable,
   ZigTypography,
 } from '@zignaly-open/ui';
-import { useLazyUsersQuery } from '../../apis/users/api';
+import {
+  useBanMutation,
+  useDisable2FAMutation,
+  useLazyUsersQuery,
+  useUnbanMutation,
+} from '../../apis/users/api';
 import { useTranslation } from 'react-i18next';
 import { UserData } from '../../apis/users/types';
 import TogglerButton from '../TableUtils/TogglerButton';
 import { ValueOrDash } from '../TableUtils/ValueOrDash';
 import { Box } from '@mui/material';
 import { useDebounce } from 'react-use';
+import useConfirmActionModal from '../TableUtils/useConfirmAction';
 
 export default function Users() {
   const { t } = useTranslation('users');
@@ -24,6 +30,10 @@ export default function Users() {
     subscriptionCode: '',
   });
   const [fetchUsers, { data: users, isFetching }] = useLazyUsersQuery();
+  const [banUser] = useBanMutation();
+  const [unbanUser] = useUnbanMutation();
+  const [disable2fa] = useDisable2FAMutation();
+  const showConfirmAction = useConfirmActionModal();
 
   useDebounce(
     async () => {
@@ -74,14 +84,32 @@ export default function Users() {
       columnHelper.accessor((v) => v.accessLevel === -100, {
         id: 'isBanned',
         header: t('table.isBanned'),
-        cell: ({ getValue }) => (
-          <TogglerButton
-            text={getValue() ? t('common:yes') : t('common:no')}
-            isPositive={!getValue()}
-            buttonText={getValue() ? t('unban') : t('ban')}
-            action={() => {}}
-          />
-        ),
+        cell: ({ getValue, row }) => {
+          const value = getValue();
+          return (
+            <TogglerButton
+              text={getValue() ? t('common:yes') : t('common:no')}
+              isPositive={!getValue()}
+              buttonText={getValue() ? t('actions.unban') : t('actions.ban')}
+              action={() =>
+                showConfirmAction({
+                  title: t(`actions.${value ? 'unban' : 'ban'}-confirm`, {
+                    email: row.original.email,
+                  }),
+                  description: t('common:generic-confirm'),
+                  yesLabel: value ? t('actions.unban') : t('actions.ban'),
+                  action: async () => {
+                    await (value
+                      ? unbanUser({ userId: row.original.userId })
+                      : banUser({ userId: row.original.userId })
+                    ).unwrap();
+                    await fetchUsers(filters);
+                  },
+                })
+              }
+            />
+          );
+        },
       }),
     ];
   }, [filters]);
