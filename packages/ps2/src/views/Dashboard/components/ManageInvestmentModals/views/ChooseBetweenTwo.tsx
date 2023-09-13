35ee7461a132ea58e8d84343899bccf189324da4
Nikita <nikitichsn2@gmail.com>
2023-09-13 16:30:23 +0200

    feat: choose between two modal improvement

--- packages/ps2/src/views/Dashboard/components/ManageInvestmentModals/views/ChooseBetweenTwo.tsx
+++ packages/ps2/src/views/Dashboard/components/ManageInvestmentModals/views/ChooseBetweenTwo.tsx
@@ -11,10 +11,14 @@ export type ChooseBetweenTwoProps = {
   explainer1Props?: Partial<ExtendedTypographyProps>;
   explainer2?: string | JSX.Element;
   explainer2Props?: Partial<ExtendedTypographyProps>;
-  cta1: string | JSX.Element;
+  cta1?: string | JSX.Element;
   button1Props?: ZigButtonProps;
-  cta2: string | JSX.Element;
+  cta2?: string | JSX.Element;
   button2Props?: ZigButtonProps;
+  rightOption?: JSX.Element;
+  leftOption?: JSX.Element;
+  rightOptionSize?: number;
+  leftOptionSize?: number;
 };
 
 // we kinda don't reuse this shit except 1 case when we do
@@ -29,6 +33,10 @@ const ChooseBetweenTwo: React.FC<ChooseBetweenTwoProps> = ({
   explainer1Props,
   explainer2,
   explainer2Props,
+  rightOption,
+  leftOption,
+  leftOptionSize = 5,
+  rightOptionSize = 5,
 }) => {
   return (
     <>
@@ -42,34 +50,38 @@ const ChooseBetweenTwo: React.FC<ChooseBetweenTwoProps> = ({
           item
           container
           justifyContent='center'
-          sx={{ height: '160px', paddingTop: '25px' }}
+          sx={{ paddingTop: '25px' }}
         >
-          <Grid item xs={12} md={5}>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                width: '100%',
-              }}
-            >
+          <Grid item xs={12} md={leftOptionSize}>
+            {leftOption ? (
+              leftOption
+            ) : (
               <Box
                 sx={{
-                  textAlign: 'center',
-                  height: '90px',
-                  paddingTop: '10px',
-                  lineHeight: 2,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  width: '100%',
                 }}
               >
-                <ZigTypography variant={'h3'} {...(explainer1Props || {})}>
-                  {explainer1}
-                </ZigTypography>
-              </Box>
+                <Box
+                  sx={{
+                    textAlign: 'center',
+                    height: '90px',
+                    paddingTop: '10px',
+                    lineHeight: 2,
+                  }}
+                >
+                  <ZigTypography variant={'h3'} {...(explainer1Props || {})}>
+                    {explainer1}
+                  </ZigTypography>
+                </Box>
 
-              <ZigButton variant='contained' size={'large'} {...button1Props}>
-                {cta1}
-              </ZigButton>
-            </Box>
+                <ZigButton variant='contained' size={'large'} {...button1Props}>
+                  {cta1}
+                </ZigButton>
+              </Box>
+            )}
           </Grid>
 
           <Grid item container xs={false} md={1} justifyContent='center'>
@@ -81,21 +93,25 @@ const ChooseBetweenTwo: React.FC<ChooseBetweenTwoProps> = ({
             />
           </Grid>
 
-          <Grid item xs={12} md={5} marginLeft={'5px'}>
-            <Box
-              sx={{
-                textAlign: 'center',
-              }}
-            >
-              <Box textAlign={'center'} height={90} paddingTop={'10px'}>
-                <ZigTypography variant={'h3'} {...(explainer2Props || {})}>
-                  {explainer2}
-                </ZigTypography>
+          <Grid item xs={12} md={rightOptionSize} marginLeft={'5px'}>
+            {rightOption ? (
+              rightOption
+            ) : (
+              <Box
+                sx={{
+                  textAlign: 'center',
+                }}
+              >
+                <Box textAlign={'center'} height={90} paddingTop={'10px'}>
+                  <ZigTypography variant={'h3'} {...(explainer2Props || {})}>
+                    {explainer2}
+                  </ZigTypography>
+                </Box>
+                <ZigButton variant='contained' size={'large'} {...button2Props}>
+                  {cta2}
+                </ZigButton>
               </Box>
-              <ZigButton variant='contained' size={'large'} {...button2Props}>
-                {cta2}
-              </ZigButton>
-            </Box>
+            )}
           </Grid>
         </Grid>
       </Grid>
