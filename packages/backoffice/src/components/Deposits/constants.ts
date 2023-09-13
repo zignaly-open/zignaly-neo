619247f1445fa251c50d8bc322292fb5f0285dbb
Alexander Mikhalchenko <alex@xfuturum.com>
2023-09-13 16:34:49 +0300

    feat(Backoffice): fixed types
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/backoffice/src/components/Deposits/constants.ts
+++ packages/backoffice/src/components/Deposits/constants.ts
@@ -1,12 +1,11 @@
 import { DepositStatuses } from '../../apis/transfers/types';
+import { TypographyProps } from '@mui/material';
 
-export const statusColorMap: Record<
-  DepositStatuses,
-  'redGraphOrError' | 'greenGraph' | 'yellow'
-> = {
-  [DepositStatuses.STATUS_IN_REVIEW]: 'yellow',
-  [DepositStatuses.STATUS_BLOCKED_BY_RISK]: 'redGraphOrError',
-  [DepositStatuses.STATUS_BLOCKED_BY_LIMIT]: 'redGraphOrError',
-  [DepositStatuses.STATUS_BLOCKED_BY_ERROR]: 'redGraphOrError',
-  [DepositStatuses.STATUS_COMPLETED]: 'greenGraph',
-};
+export const statusColorMap: Record<DepositStatuses, TypographyProps['color']> =
+  {
+    [DepositStatuses.STATUS_IN_REVIEW]: 'yellow',
+    [DepositStatuses.STATUS_BLOCKED_BY_RISK]: 'redGraphOrError',
+    [DepositStatuses.STATUS_BLOCKED_BY_LIMIT]: 'redGraphOrError',
+    [DepositStatuses.STATUS_BLOCKED_BY_ERROR]: 'redGraphOrError',
+    [DepositStatuses.STATUS_COMPLETED]: 'greenGraph',
+  };
