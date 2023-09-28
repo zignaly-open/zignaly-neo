34af545609e9173955d4e97ad871246ed0b59f70
Nikita <nikitichsn2@gmail.com>
2023-09-28 13:31:04 +0300

    fix: remove fragment

--- packages/ps2/src/views/Dashboard/components/ManageInvestmentModals/views/SwapDepositPurchase.tsx
+++ packages/ps2/src/views/Dashboard/components/ManageInvestmentModals/views/SwapDepositPurchase.tsx
@@ -30,71 +30,69 @@ const SwapDepositPurchase: React.FC<SwapDepositPurchaseProps> = ({
   }
 
   return (
-    <>
-      <ChooseBetweenTwo
-        leftOptionSize={6}
-        rightOptionSize={4}
-        leftOption={
-          <SwapForm
-            coinSwapTo={coin}
-            coinsAllowedSwapFrom={coinOptionsAllowedSwapFrom}
-            setView={setView}
-            setConfirmSwapData={setConfirmSwapData}
-            setConvertPreviewData={setConvertPreviewData}
-          />
-        }
-        rightOption={
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              gap: 3,
-            }}
+    <ChooseBetweenTwo
+      leftOptionSize={6}
+      rightOptionSize={4}
+      leftOption={
+        <SwapForm
+          coinSwapTo={coin}
+          coinsAllowedSwapFrom={coinOptionsAllowedSwapFrom}
+          setView={setView}
+          setConfirmSwapData={setConfirmSwapData}
+          setConvertPreviewData={setConvertPreviewData}
+        />
+      }
+      rightOption={
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: 3,
+          }}
+        >
+          <ZigTypography whiteSpace={'nowrap'}>
+            {t('service-swap.deposit-purchase', { coin })}
+          </ZigTypography>
+          <ZigButton
+            onClick={() => setView(ChooseDepositTypeViews.DepositView)}
+            id={'modal-swap-deposit-purchase__deposit'}
+            variant={'outlined'}
+            sx={{ minWidth: '133px' }}
+            size={'medium'}
+            startIcon={
+              <ZigPlusIcon
+                width={'7px'}
+                height={'7px'}
+                style={{ marginBottom: '2px' }}
+                color={theme.palette.neutral300}
+              />
+            }
           >
-            <ZigTypography whiteSpace={'nowrap'}>
-              {t('service-swap.deposit-purchase', { coin })}
-            </ZigTypography>
-            <ZigButton
-              onClick={() => setView(ChooseDepositTypeViews.DepositView)}
-              id={'modal-swap-deposit-purchase__deposit'}
-              variant={'outlined'}
-              sx={{ minWidth: '133px' }}
-              size={'medium'}
-              startIcon={
-                <ZigPlusIcon
-                  width={'7px'}
-                  height={'7px'}
-                  style={{ marginBottom: '2px' }}
-                  color={theme.palette.neutral300}
-                />
-              }
-            >
-              {t('service-deposit.buttons.deposit', { coin })}
-            </ZigButton>
-            <ZigButton
-              size={'medium'}
-              variant={'outlined'}
-              href={BUY_CRYPTO_URL}
-              id={'modal-swap-deposit-purchase__purchase'}
-              endIcon={
-                <ZigArrowOutIcon
-                  width={'7px'}
-                  height={'7px'}
-                  style={{ marginBottom: '1px' }}
-                  color={theme.palette.neutral300}
-                />
-              }
-              sx={{ padding: '8px 20px' }}
-            >
-              {t('service-deposit.buttons.purchase', { coin })}
-            </ZigButton>
-          </Box>
-        }
-        description={t('service-swap.description', { coin })}
-        descriptionProps={{ id: 'modal-swap-deposit-purchase__description' }}
-      />
-    </>
+            {t('service-deposit.buttons.deposit', { coin })}
+          </ZigButton>
+          <ZigButton
+            size={'medium'}
+            variant={'outlined'}
+            href={BUY_CRYPTO_URL}
+            id={'modal-swap-deposit-purchase__purchase'}
+            endIcon={
+              <ZigArrowOutIcon
+                width={'7px'}
+                height={'7px'}
+                style={{ marginBottom: '1px' }}
+                color={theme.palette.neutral300}
+              />
+            }
+            sx={{ padding: '8px 20px' }}
+          >
+            {t('service-deposit.buttons.purchase', { coin })}
+          </ZigButton>
+        </Box>
+      }
+      description={t('service-swap.description', { coin })}
+      descriptionProps={{ id: 'modal-swap-deposit-purchase__description' }}
+    />
   );
 };
 
