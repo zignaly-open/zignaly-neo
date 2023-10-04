d0a27d31cc7f304830459aa73f9e7b51c4a68b9e
Christophe Wagner <chris.wagn22@gmail.com>
2023-10-04 20:21:52 +0200

    replace fetch

--- packages/ps2/src/test/server.ts
+++ packages/ps2/src/test/server.ts
@@ -1,4 +1,8 @@
 import { setupServer } from 'msw/node';
 import { handlers } from './handlers';
-
+// eslint-disable-next-line import/no-extraneous-dependencies
+import fetch from 'node-fetch';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+window.fetch = fetch;
 export const server = setupServer(...handlers);
