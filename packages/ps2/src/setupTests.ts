ac1e48f3037fc3f16ae05a08aa8fa87f5c15de63
Christophe Wagner <chris.wagn22@gmail.com>
2023-10-04 18:28:02 +0200

    debug ci

--- packages/ps2/src/setupTests.ts
+++ packages/ps2/src/setupTests.ts
@@ -4,6 +4,8 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import { store } from 'apis/store';
+import { fetch } from 'cross-fetch';
+global.fetch = fetch;
 import { server } from './test/server';
 import ps2Api from './apis/baseApiPs2';
 import { api as walletApi } from './apis/wallet/api';
