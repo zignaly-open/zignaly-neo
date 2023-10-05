f1d547a46077cc427ae5c54d491e15d206e22355
Christophe Wagner <chris.wagn22@gmail.com>
2023-10-05 14:01:45 +0200

    chore: upgrade msw 2

--- packages/ps2/src/test/handlers.ts
+++ packages/ps2/src/test/handlers.ts
@@ -1,4 +1,4 @@
-import { rest } from 'msw';
+import { HttpResponse, http } from 'msw';
 import {
   loginMockEmail,
   loginMockPassword,
@@ -14,56 +14,42 @@ import {
 import { userDataResponseMock } from './mocks/user';
 
 const ps2 = [
-  rest.get(
-    process.env.REACT_APP_BASE_API + '/user/session',
-    async (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({ userId: 'aa', validUntil: '2024-08-18T17:05:44+00:00' }),
-      );
-    },
-  ),
-  rest.get(
-    process.env.REACT_APP_BASE_API + '/user/data',
-    async (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(userDataResponseMock));
-    },
-  ),
-  rest.post<LoginPayload>(
+  http.get(process.env.REACT_APP_BASE_API + '/user/session', () => {
+    return HttpResponse.json({
+      userId: 'aa',
+      validUntil: '2024-08-18T17:05:44+00:00',
+    });
+  }),
+  http.get(process.env.REACT_APP_BASE_API + '/user/data', () => {
+    return HttpResponse.json(userDataResponseMock);
+  }),
+  http.post<LoginPayload>(
     process.env.REACT_APP_BASE_API + '/login',
-    async (req, res, ctx) => {
-      const { email, password } = (await req.json()) as LoginPayload;
+    async ({ request }) => {
+      const { email, password } = (await request.json()) as LoginPayload;
       if (email === loginMockEmail && password === loginMockPassword) {
-        return res(ctx.status(200), ctx.json(loginResponseMockSuccess));
+        return HttpResponse.json(loginResponseMockSuccess);
       } else {
-        return res(
-          ctx.status(401),
-          ctx.json(loginResponseMockWrongCredentials),
-        );
+        return HttpResponse.json(loginResponseMockWrongCredentials, {
+          status: 401,
+        });
       }
     },
   ),
 ];
 
 const referrals = [
-  rest.get(
-    process.env.REACT_APP_REFERRALS_API + '/v1/referrer/data',
-    async (req, res, ctx) =>
-      res(ctx.status(200), ctx.json(referrerDataResponseMockSuccess)),
-  ),
-  rest.get(
-    process.env.REACT_APP_REFERRALS_API + '/v1/referrer/history',
-    async (req, res, ctx) =>
-      res(ctx.status(200), ctx.json(referrerHistoryResponseMockSuccess)),
-  ),
-  rest.get(
-    process.env.REACT_APP_REFERRALS_API + '/v1/tier-levels',
-    async (req, res, ctx) =>
-      res(ctx.status(200), ctx.json(tierLevelsResponseMockSuccess)),
-  ),
-  rest.put(
-    process.env.REACT_APP_REFERRALS_API + '/v1/referrer',
-    async (req, res, ctx) => res(ctx.status(200)),
+  http.get(process.env.REACT_APP_REFERRALS_API + '/v1/referrer/data', () => {
+    return HttpResponse.json(referrerDataResponseMockSuccess);
+  }),
+  http.get(process.env.REACT_APP_REFERRALS_API + '/v1/referrer/history', () => {
+    return HttpResponse.json(referrerHistoryResponseMockSuccess);
+  }),
+  http.get(process.env.REACT_APP_REFERRALS_API + '/v1/tier-levels', () => {
+    return HttpResponse.json(tierLevelsResponseMockSuccess);
+  }),
+  http.put(process.env.REACT_APP_REFERRALS_API + '/v1/referrer', () =>
+    HttpResponse.text('', { status: 200 }),
   ),
 ];
 
