0b55d311164a43707c5e620154462aa01db959b2
Alexander Mikhalchenko <alex@xfuturum.com>
2023-10-07 05:07:49 +0300

    feat(TableRefactor): fixed CI
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/components/display/ZigTable/types.ts
+++ packages/zignaly-ui/src/components/display/ZigTable/types.ts
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Row, SortingState, TableOptions, TableState } from "@tanstack/react-table";
+import { Row, TableOptions, TableState } from "@tanstack/react-table";
 import { RtkQueryLike } from "../../../utils/rtk";
 
 interface ZigTablePropsBase<T extends object>
