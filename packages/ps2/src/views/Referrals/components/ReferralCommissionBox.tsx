1bad1abafd16d4a76a0d37d1c0d570d7ad453c34
Christophe Wagner <chris.wagn22@gmail.com>
2023-10-19 17:53:20 +0200

    fix: handle 0 as slider max commission

--- packages/ps2/src/views/Referrals/components/ReferralCommissionBox.tsx
+++ packages/ps2/src/views/Referrals/components/ReferralCommissionBox.tsx
@@ -67,9 +67,9 @@ const ReferralCommissionBox = ({
         <ShareCommissionSlider
           discountPct={referral.discountPct}
           max={
-            referral.invitedCount > 0
+            (referral.invitedCount > 0
               ? commissionWithoutTraderBoost
-              : maxCommission
+              : maxCommission) || 100
           }
         />
       </Wrapper>
