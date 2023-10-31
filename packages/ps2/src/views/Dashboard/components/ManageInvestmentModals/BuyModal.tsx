3fb7988e8b3b2bba390abab03489edce82374073
Christophe Wagner <chris.wagn22@gmail.com>
2023-10-31 13:20:24 +0100

    chore: clean PR

--- packages/ps2/src/views/Dashboard/components/ManageInvestmentModals/BuyModal.tsx
+++ packages/ps2/src/views/Dashboard/components/ManageInvestmentModals/BuyModal.tsx
@@ -2,31 +2,26 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { DialogProps } from '@mui/material/Dialog';
 import ZModal from '../../../../components/ZModal';
-import { DepositModalProps } from './types';
 import { useZModal } from '../../../../components/ZModal/use';
 import { Loader, ZigTypography } from '@zignaly-open/ui';
 import { Box } from '@mui/material';
 import { useAsync } from 'react-use';
 import { useDepositInfo } from 'apis/coin/use';
 
-function BuyModal({
-  close,
-  onClose,
-  selectedCoin,
-  allowedCoins,
-  ...props
-}: {
-  onClose: () => void;
-} & DepositModalProps &
-  DialogProps): React.ReactElement {
-  const { t } = useTranslation(['deposit-crypto']);
-  const { data: depositInfo } = useDepositInfo('USDT', 'TRX', true);
-
+function useCurrency() {
   const { value: currency } = useAsync(async () => {
     const response = await fetch('http://www.geoplugin.net/json.gp');
     const data = await response.json();
     return data.geoplugin_currencyCode;
   }, []);
+
+  return currency;
+}
+
+function BuyModal(props: DialogProps): React.ReactElement {
+  const { t } = useTranslation(['deposit-crypto']);
+  const { data: depositInfo } = useDepositInfo('USDT', 'TRX', true);
+  const currency = useCurrency();
 
   return (
     <ZModal
@@ -59,9 +54,7 @@ function BuyModal({
 
 BuyModal.trackId = 'buy';
 
-export const useOpenBuyModal = (): ((
-  props?: Partial<DepositModalProps & { onClose: () => void }>,
-) => void) => {
+export const useOpenBuyModal = (): ((props?: Partial<DialogProps>) => void) => {
   const { showModal } = useZModal({ disableAutoDestroy: true });
   return (props) => {
     showModal(BuyModal, props);
