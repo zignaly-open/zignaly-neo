92555e2bf4adf06d8cba4a85617acfeaa9449940
Alexander Mikhalchenko <alex@xfuturum.com>
2023-11-16 21:25:13 +0400

    feat(CodeDeploy): fixed permissions
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2-wl-server/scripts/AfterInstall.sh
+++ packages/ps2-wl-server/scripts/AfterInstall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-DEPLOYMENTPATH="/zignaly/{directory}/deploy"
+DEPLOYMENTPATH="/zignaly/{directory}"
 \. "$HOME/.nvm/nvm.sh"
+sudo chown -R admin:admin $DEPLOYMENTPATH
 cd $DEPLOYMENTPATH
+cd deploy
 #ls -lah
 #cat package.json
 #exit 1
