ea2a7d21b4427e7fa3a5799750e9b07bdbcacd1b
Nikita <nikitichsn2@gmail.com>
2023-11-17 02:50:32 +0300

    test: add tests for ZigButton

---
+++ packages/zignaly-ui/src/components/inputs/ZigButton/index.test.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import ZigButton from ".";
+import { renderWithProvidersUi } from "../../../utils/test";
+import { fireEvent, waitFor } from "@testing-library/react";
+
+describe("components/inputs/ZigButton", () => {
+  describe("renders", () => {
+    it("ZigButton small size", async () => {
+      const { container } = renderWithProvidersUi(<ZigButton size={"small"} />);
+
+      const button = container.querySelector("button") as Element;
+      expect(button).toBeVisible();
+      expect(button).toHaveClass("MuiButton-sizeSmall");
+      const buttonStyles = getComputedStyle(button);
+      expect(buttonStyles?.minWidth).toBe("76px");
+      expect(buttonStyles?.minHeight).toBe("30px");
+    });
+    it("ZigButton medium size", async () => {
+      const { container } = renderWithProvidersUi(<ZigButton size={"medium"} />);
+
+      const button = container.querySelector("button") as Element;
+      expect(button).toBeVisible();
+      expect(button).toHaveClass("MuiButton-sizeMedium");
+      const buttonStyles = getComputedStyle(button);
+      expect(buttonStyles?.minWidth).toBe("76px");
+      expect(buttonStyles?.minHeight).toBe("36px");
+    });
+    it("ZigButton large size", async () => {
+      const { container } = renderWithProvidersUi(<ZigButton size={"large"} />);
+
+      const button = container.querySelector("button") as Element;
+      expect(button).toBeVisible();
+      expect(button).toHaveClass("MuiButton-sizeLarge");
+      const buttonStyles = getComputedStyle(button);
+      expect(buttonStyles?.minWidth).toBe("110px");
+      expect(buttonStyles?.minHeight).toBe("48px");
+    });
+    it("ZigButton xlarge size", async () => {
+      const { container } = renderWithProvidersUi(<ZigButton size={"xlarge"} />);
+
+      const button = container.querySelector("button") as Element;
+      expect(button).toBeVisible();
+      expect(button).toHaveClass("MuiButton-sizeXlarge");
+      const buttonStyles = getComputedStyle(button);
+      expect(buttonStyles?.minWidth).toBe("127px");
+      expect(buttonStyles?.minHeight).toBe("60px");
+    });
+    it("ZigButton contained variant by default", async () => {
+      const { container } = renderWithProvidersUi(<ZigButton />);
+
+      const button = container.querySelector("button") as Element;
+      expect(button).toBeVisible();
+      expect(button).toHaveClass("MuiButton-contained");
+      const buttonStyles = getComputedStyle(button);
+      expect(buttonStyles?.backgroundColor).not.toBe("transparent");
+    });
+    it("ZigButton outlined variant", async () => {
+      const { container } = renderWithProvidersUi(<ZigButton variant={"outlined"} />);
+
+      const button = container.querySelector("button") as Element;
+      expect(button).toBeVisible();
+      expect(button).not.toHaveClass("MuiButton-contained");
+      expect(button).not.toHaveClass("MuiButton-contained");
+      expect(button).toHaveClass("MuiButton-outlined");
+      const buttonStyles = getComputedStyle(button);
+      expect(buttonStyles?.backgroundColor).toBe("transparent");
+    });
+    it("ZigButton text variant", async () => {
+      const { container } = renderWithProvidersUi(<ZigButton variant={"text"} />);
+
+      const button = container.querySelector("button") as Element;
+      expect(button).toBeVisible();
+      expect(button).not.toHaveClass("MuiButton-contained");
+      expect(button).not.toHaveClass("MuiButton-outlined");
+      expect(button).toHaveClass("MuiButton-text");
+      const buttonStyles = getComputedStyle(button);
+      expect(buttonStyles?.backgroundColor).toBe("transparent");
+    });
+  });
+  describe("disable", () => {
+    it("ZigButton active test", async () => {
+      const { container } = renderWithProvidersUi(<ZigButton disabled={false} />);
+      const onClick = jest.fn();
+      const button = container.querySelector("button") as Element;
+      expect(button).not.toHaveClass("Mui-disabled");
+      fireEvent.click(button);
+      await waitFor(() => {
+        expect(onClick).toHaveBeenCalledTimes(1);
+      });
+    });
+    it("ZigButton disabled test", async () => {
+      const { container } = renderWithProvidersUi(<ZigButton disabled />);
+      const onClick = jest.fn();
+      const button = container.querySelector("button") as Element;
+      expect(button).toHaveClass("Mui-disabled");
+      fireEvent.click(button);
+      await waitFor(() => {
+        expect(onClick).toHaveBeenCalledTimes(0);
+      });
+    });
+  });
+});
---
+++ packages/zignaly-ui/src/utils/test.tsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from "@testing-library/react";
+import { ThemeProvider as ThemeProviderMui } from "@mui/material";
+import { ThemeProvider, ThemeProviderMui as ThemeInheritorMui } from "../index";
+import React from "react";
+import { getZignalyThemeExport } from "../theme";
+
+export const renderWithProvidersUi = (ui: JSX.Element, renderOptions = {}) => {
+  const { mui, legacyStyledComponentsDoNotUse } = getZignalyThemeExport();
+  return render(ui, {
+    wrapper: ({ children }: { children: JSX.Element }) => (
+      <ThemeProvider theme={legacyStyledComponentsDoNotUse}>
+        <ThemeInheritorMui theme={mui}>
+          <ThemeProviderMui theme={mui}>{children}</ThemeProviderMui>
+        </ThemeInheritorMui>
+      </ThemeProvider>
+    ),
+    ...renderOptions,
+  });
+};
