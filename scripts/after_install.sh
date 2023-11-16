951f517274db9545ee365a9e7b97de71a09efccf
Alexander Mikhalchenko <alex@xfuturum.com>
2023-11-16 18:44:04 +0400

    feat(CodeDeploy): debugging deploy
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,6 +8,11 @@ sudo chown -R admin:admin $DEPLOYMENTPATH/deploy
 
 # Install server
 cd $DEPLOYMENTPATH/deploy/packages/ps2
+which pm2
+which npm
+nvm use 20
+which pm2
+which npm
 npm i express serve-static
 
 
