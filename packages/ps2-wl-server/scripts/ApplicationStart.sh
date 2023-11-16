ebaa4d4bc1f23c3644588c0a86ca4d9f10c4a7bb
Alexander Mikhalchenko <alex@xfuturum.com>
2023-11-16 20:00:12 +0400

    feat(CodeDeploy): debugging deploy
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2-wl-server/scripts/ApplicationStart.sh
+++ packages/ps2-wl-server/scripts/ApplicationStart.sh
@@ -2,10 +2,8 @@
 
 DEPLOYMENTPATH="$HOME/{directory}"
 \. "$HOME/.nvm/nvm.sh"
-# Change permissions
-sudo chown -R admin:admin $DEPLOYMENTPATH/deploy
 
-cd $DEPLOYMENTPATH/deploy/packages/ps2
-npm i express serve-static
+#cd $DEPLOYMENTPATH/deploy/packages/ps2
+#npm i express serve-static
 
 
