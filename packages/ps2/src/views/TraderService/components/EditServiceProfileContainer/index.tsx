de28e2db9d45cc52e295d73be07b9482db9a57be
Christophe Wagner <chris.wagn22@gmail.com>
2023-11-21 10:47:42 +0700

    fix: fix lint

--- packages/ps2/src/views/TraderService/components/EditServiceProfileContainer/index.tsx
+++ packages/ps2/src/views/TraderService/components/EditServiceProfileContainer/index.tsx
@@ -26,7 +26,7 @@ import { ROUTE_TRADING_SERVICE } from 'routes';
 import { useCurrentUser } from 'apis/user/use';
 import SuccessFeeInputWrapper from '../BecomeTraderLanding/modals/forms/SuccessFeeInputWrapper';
 import CommissionReferralSharing from './atoms/CommissionReferralSharing';
-import { isFeatureOn, whitelabel } from 'whitelabel';
+import { isFeatureOn } from 'whitelabel';
 import { Features } from 'whitelabel/type';
 import { useUpdateServiceCommissionMutation } from 'apis/referrals/api';
 
