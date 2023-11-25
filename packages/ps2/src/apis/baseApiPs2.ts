33877238f5a7b54f0218499f7899376d0cc790c8
Alexander Mikhalchenko <alex@xfuturum.com>
2023-11-25 21:09:26 +0400

    chore: removed now unnecessary ts ignores
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/apis/baseApiPs2.ts
+++ packages/ps2/src/apis/baseApiPs2.ts
@@ -1,6 +1,3 @@
-// TODO: should be fixed with RTK update
-// https://github.com/reduxjs/redux-toolkit/issues/2485
-// @ts-ignore
 import { createApi } from '@reduxjs/toolkit/query/react';
 import baseQuery from 'apis/baseQuery';
 
