15f86aaabb85d1ba3fd34f92acf5b16571d71c14
Alexander Mikhalchenko <alex@xfuturum.com>
2023-11-27 02:44:31 +0400

    chore: fixed global style
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/theme/GlobalAppStyle.tsx
+++ packages/zignaly-ui/src/theme/GlobalAppStyle.tsx
@@ -1,45 +1,55 @@
 import React from "react";
 import GlobalStyles from "@mui/material/GlobalStyles";
 import { NiceScrollbar, ToasterStyle } from "../utils/css";
-import { Theme } from "@mui/system";
 import { createGlobalStyle } from "styled-components";
+import { useTheme } from "@mui/material";
 
 const NiceScrollGlobalStyle = createGlobalStyle`${NiceScrollbar}`;
 const ToasterGlobalStyle = createGlobalStyle`${ToasterStyle}`;
+const Reset = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 100%;
+    overflow: overlay;
+    color-scheme: dark;
+  }
+  
+   a {
+     color: inherit;
+     text-decoration: none;
+   }
+  
+  * {
+    box-sizing: border-box;
+  }
+  
+  ul, li, ol {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+  }
+`;
 
 export const GlobalAppStyle: React.FC = () => {
+  const theme = useTheme();
   return (
     <>
+      <Reset />
+      <ToasterGlobalStyle />
+
       {
         // @ts-ignore
         window.isSafari && <NiceScrollGlobalStyle />
       }
-      <ToasterGlobalStyle />
+
       <GlobalStyles
         styles={{
-          body: `
-          padding: 0;
-          margin: 0;
-          background: ${({ theme }: { theme: Theme }) => theme.palette.backgrounds.body};
-          background-repeat: no-repeat;
-          background-size: cover;
-          font-size: 100%;
-          color: #fff;
-          overflow: overlay;
-          color-scheme: dark;
-        `,
-          a: `
-          color: inherit;
-          text-decoration: none;
-        `,
-          "*": `
-          box-sizing: border-box;
-        `,
-          "ul, li, ol": `
-         padding: 0;
-          margin: 0;
-          list-style-type: none;
-        `,
+          body: {
+            background: theme.palette.backgrounds.body,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          },
         }}
       />
     </>
