2e817bbb0b52d648ad9ec809d1b893026557d251
Nikita <nikitichsn2@gmail.com>
2023-12-04 14:16:37 +0300

    tests: add tests ZigProgressBar ui

---
+++ packages/zignaly-ui/src/components/display/ZigProgressBar/index.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ZigProgressBar from "./index";
+import { renderWithProvidersUi } from "../../../utils/testConfig";
+
+describe("components/display/ZigProgressBar", () => {
+  describe("renders", () => {
+    it("ZigProgressBar render", async () => {
+      const { container } = renderWithProvidersUi(
+        <ZigProgressBar className={"test-class"} value={10} />,
+      );
+
+      const progressBar = container.querySelector(".test-class") as Element;
+      expect(progressBar).toBeVisible();
+      const progressBarStyles = getComputedStyle(progressBar);
+      expect(progressBar).toMatchSnapshot();
+      expect(progressBarStyles).toMatchSnapshot();
+    });
+  });
+});
