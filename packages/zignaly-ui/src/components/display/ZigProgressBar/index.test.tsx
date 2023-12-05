1e81d429bb3c486cb51e34793d0730c35bf8ccbd
Nikita <nikitichsn2@gmail.com>
2023-12-05 16:43:33 +0300

    fix: tests names

--- packages/zignaly-ui/src/components/display/ZigProgressBar/index.test.tsx
+++ packages/zignaly-ui/src/components/display/ZigProgressBar/index.test.tsx
@@ -3,17 +3,15 @@ import ZigProgressBar from "./index";
 import { renderWithProvidersUi } from "../../../utils/testConfig";
 
 describe("components/display/ZigProgressBar", () => {
-  describe("renders", () => {
-    it("ZigProgressBar render", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigProgressBar className={"test-class"} value={10} />,
-      );
+  it("renders", async () => {
+    const { container } = renderWithProvidersUi(
+      <ZigProgressBar className={"test-class"} value={10} />,
+    );
 
-      const progressBar = container.querySelector(".test-class") as Element;
-      expect(progressBar).toBeVisible();
-      const progressBarStyles = getComputedStyle(progressBar);
-      expect(progressBar).toMatchSnapshot();
-      expect(progressBarStyles).toMatchSnapshot();
-    });
+    const progressBar = container.querySelector(".test-class") as Element;
+    expect(progressBar).toBeVisible();
+    const progressBarStyles = getComputedStyle(progressBar);
+    expect(progressBar).toMatchSnapshot();
+    expect(progressBarStyles).toMatchSnapshot();
   });
 });
