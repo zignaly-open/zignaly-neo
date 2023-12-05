1bd27270de5a25810e2df55227e714ed5fce6f8a
AndWa <andrej.saric.du@gmail.com>
2023-12-05 20:50:59 +0100

    fix: pwa balance view buttons

--- packages/ps2/src/views/Balance/components/MyBalancesTable/styles.ts
+++ packages/ps2/src/views/Balance/components/MyBalancesTable/styles.ts
@@ -3,7 +3,7 @@ import { Box, styled } from '@mui/material';
 export const ButtonsWrapper = styled(Box)`
   display: flex;
   position: fixed;
-  bottom: 56px;
+  bottom: calc(68px + env(safe-area-inset-bottom));
   height: 60px;
   width: 100%;
   padding: 10px 3px 5px 3px;
