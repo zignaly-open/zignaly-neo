65422284437e1fac584d9182ad99015dbdedfe6a
Nikita <nikitichsn2@gmail.com>
2023-12-06 12:48:37 +0300

    fix: tests names

--- packages/zignaly-ui/src/components/display/ZigTypography/index.test.tsx
+++ packages/zignaly-ui/src/components/display/ZigTypography/index.test.tsx
@@ -3,39 +3,37 @@ import ZigTypography from "./index";
 import { renderWithProvidersUi } from "../../../utils/testConfig";
 
 describe("components/display/ZigTypography", () => {
-  describe("renders", () => {
-    it("ZigTypography render", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigTypography id={"typography"}></ZigTypography>,
-      );
+  it("renders with default props", async () => {
+    const { container } = renderWithProvidersUi(<ZigTypography id={"typography"}></ZigTypography>);
 
-      const typography = container.querySelector("#typography") as Element;
-      expect(typography).toBeVisible();
-      const typographyStyles = getComputedStyle(typography);
-      expect(typography).toMatchSnapshot();
-      expect(typographyStyles).toMatchSnapshot();
-    });
-    it("ZigTypography render custom variant", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigTypography id={"typography"} variant={"h2"}></ZigTypography>,
-      );
+    const typography = container.querySelector("#typography") as Element;
+    expect(typography).toBeVisible();
+    const typographyStyles = getComputedStyle(typography);
+    expect(typography).toMatchSnapshot();
+    expect(typographyStyles).toMatchSnapshot();
+  });
 
-      const typography = container.querySelector("#typography") as Element;
-      expect(typography).toBeVisible();
-      const typographyStyles = getComputedStyle(typography);
-      expect(typography).toMatchSnapshot();
-      expect(typographyStyles).toMatchSnapshot();
-    });
-    it("ZigTypography render with sx prop", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigTypography id={"typography"} sx={{ mt: "15px", fontSize: "25px" }}></ZigTypography>,
-      );
+  it("renders with custom variant", async () => {
+    const { container } = renderWithProvidersUi(
+      <ZigTypography id={"typography"} variant={"h2"}></ZigTypography>,
+    );
 
-      const typography = container.querySelector("#typography") as Element;
-      expect(typography).toBeVisible();
-      const typographyStyles = getComputedStyle(typography);
-      expect(typography).toMatchSnapshot();
-      expect(typographyStyles).toMatchSnapshot();
-    });
+    const typography = container.querySelector("#typography") as Element;
+    expect(typography).toBeVisible();
+    const typographyStyles = getComputedStyle(typography);
+    expect(typography).toMatchSnapshot();
+    expect(typographyStyles).toMatchSnapshot();
+  });
+
+  it("renders with sx prop", async () => {
+    const { container } = renderWithProvidersUi(
+      <ZigTypography id={"typography"} sx={{ mt: "15px", fontSize: "25px" }}></ZigTypography>,
+    );
+
+    const typography = container.querySelector("#typography") as Element;
+    expect(typography).toBeVisible();
+    const typographyStyles = getComputedStyle(typography);
+    expect(typography).toMatchSnapshot();
+    expect(typographyStyles).toMatchSnapshot();
   });
 });
