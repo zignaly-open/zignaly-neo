804bbfb4b1f06b8e79a4476bd93858211ece5fb8
Nikita <nikitichsn2@gmail.com>
2023-12-06 12:40:34 +0300

    fix: remove second describe

--- packages/zignaly-ui/src/components/display/ZigQrCode/index.test.tsx
+++ packages/zignaly-ui/src/components/display/ZigQrCode/index.test.tsx
@@ -3,48 +3,49 @@ import ZigQrCode from ".";
 import { renderWithProvidersUi } from "../../../utils/testConfig";
 
 describe("components/display/ZigQrCode", () => {
-  describe("ZigQrCode rendering", () => {
-    it("renders with default url", async () => {
-      const { container } = renderWithProvidersUi(<ZigQrCode id={"qr-code-test-id"} />);
+  it("renders with default url", async () => {
+    const { container } = renderWithProvidersUi(<ZigQrCode id={"qr-code-test-id"} />);
 
-      const qrCode = container.querySelector("#qr-code-test-id") as Element;
-      expect(qrCode).toBeVisible();
-      const qrCodeStyles = getComputedStyle(qrCode);
-      expect(qrCode).toMatchSnapshot();
-      expect(qrCodeStyles).toMatchSnapshot();
-    });
-    it("renders with custom url", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigQrCode id={"qr-code-test-id"} url={"https://github.com/zignaly-open/zignaly-neo/"} />,
-      );
+    const qrCode = container.querySelector("#qr-code-test-id") as Element;
+    expect(qrCode).toBeVisible();
+    const qrCodeStyles = getComputedStyle(qrCode);
+    expect(qrCode).toMatchSnapshot();
+    expect(qrCodeStyles).toMatchSnapshot();
+  });
 
-      const qrCode = container.querySelector("#qr-code-test-id") as Element;
-      expect(qrCode).toBeVisible();
-      const qrCodeStyles = getComputedStyle(qrCode);
-      expect(qrCode).toMatchSnapshot();
-      expect(qrCodeStyles).toMatchSnapshot();
-    });
-    it("renders with custom size", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigQrCode id={"qr-code-test-id"} width={500} height={500} />,
-      );
+  it("renders with custom url", async () => {
+    const { container } = renderWithProvidersUi(
+      <ZigQrCode id={"qr-code-test-id"} url={"https://github.com/zignaly-open/zignaly-neo/"} />,
+    );
 
-      const qrCode = container.querySelector("#qr-code-test-id") as Element;
-      expect(qrCode).toBeVisible();
-      const qrCodeStyles = getComputedStyle(qrCode);
-      expect(qrCode).toMatchSnapshot();
-      expect(qrCodeStyles).toMatchSnapshot();
-    });
-    it("renders with label", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigQrCode id={"qr-code-test-id"} label={"test-label"} className={"test-class"} />,
-      );
+    const qrCode = container.querySelector("#qr-code-test-id") as Element;
+    expect(qrCode).toBeVisible();
+    const qrCodeStyles = getComputedStyle(qrCode);
+    expect(qrCode).toMatchSnapshot();
+    expect(qrCodeStyles).toMatchSnapshot();
+  });
 
-      const qrCode = container.querySelector(".test-class") as Element;
-      expect(qrCode).toBeVisible();
-      const qrCodeStyles = getComputedStyle(qrCode);
-      expect(qrCode).toMatchSnapshot();
-      expect(qrCodeStyles).toMatchSnapshot();
-    });
+  it("renders with custom size", async () => {
+    const { container } = renderWithProvidersUi(
+      <ZigQrCode id={"qr-code-test-id"} width={500} height={500} />,
+    );
+
+    const qrCode = container.querySelector("#qr-code-test-id") as Element;
+    expect(qrCode).toBeVisible();
+    const qrCodeStyles = getComputedStyle(qrCode);
+    expect(qrCode).toMatchSnapshot();
+    expect(qrCodeStyles).toMatchSnapshot();
+  });
+
+  it("renders with label", async () => {
+    const { container } = renderWithProvidersUi(
+      <ZigQrCode id={"qr-code-test-id"} label={"test-label"} className={"test-class"} />,
+    );
+
+    const qrCode = container.querySelector(".test-class") as Element;
+    expect(qrCode).toBeVisible();
+    const qrCodeStyles = getComputedStyle(qrCode);
+    expect(qrCode).toMatchSnapshot();
+    expect(qrCodeStyles).toMatchSnapshot();
   });
 });
