652f9ebc436794dd95590126277d5cff8d0f9c1f
AndWa <andrej.saric.du@gmail.com>
2023-12-12 11:31:09 +0100

    fix: extract declaration into separate file

--- packages/ps2/src/util/pwaInstall.tsx
+++ packages/ps2/src/util/pwaInstall.tsx
@@ -1,18 +1,6 @@
 import '@khmyznikov/pwa-install';
 import React from 'react';
 
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace JSX {
-    interface IntrinsicElements {
-      'pwa-install': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >;
-    }
-  }
-}
-
 const PwaInstall = (): JSX.Element => {
   return (
     <pwa-install id='pwa-install' manifest-url='manifest.json'></pwa-install>
