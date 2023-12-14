2f45da9b8d321e59be08a7bf94905e98089f9628
Nikita <nikitichsn2@gmail.com>
2023-12-14 13:28:34 +0300

    tests: remove unnecessary describes

--- packages/zignaly-ui/src/components/inputs/ZigButtonGroupInput/index.test.tsx
+++ packages/zignaly-ui/src/components/inputs/ZigButtonGroupInput/index.test.tsx
@@ -15,6 +15,7 @@ const testOptions = [
     label: "button 2",
   },
 ];
+
 const fiveTestOptions = [
   {
     id: "test-button1",
@@ -45,33 +46,46 @@ const fiveTestOptions = [
 let mainValue = "test1";
 
 describe("components/inputs/ZigButtonGroupInput", () => {
-  describe("ZigButtonGroup rendering", () => {
-    it("renders with 2 options", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigButtonGroupInput value={mainValue} options={testOptions} onChange={() => {}} />,
-      );
+  it("should render with 2 options without crashing", async () => {
+    const { container } = renderWithProvidersUi(
+      <ZigButtonGroupInput value={mainValue} options={testOptions} onChange={() => {}} />,
+    );
 
-      const button1 = container.querySelector("#test-button1") as Element;
-      const button2 = container.querySelector("#test-button2") as Element;
-      expect(container).toMatchSnapshot();
-      expect(button1).toBeVisible();
-      expect(button2).toBeVisible();
-    });
+    const button1 = container.querySelector("#test-button1") as Element;
+    const button2 = container.querySelector("#test-button2") as Element;
+    expect(container).toMatchSnapshot();
+    expect(button1).toBeVisible();
+    expect(button2).toBeVisible();
+  });
 
-    it("renders with more than 2 options", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigButtonGroupInput value={mainValue} options={fiveTestOptions} onChange={() => {}} />,
-      );
-      testOptions.forEach(({ id }) => {
-        const button = container.querySelector(`#${id}`) as Element;
-        expect(button).toBeVisible();
-      });
+  it("should render with more than 2 options without crashing", async () => {
+    const { container } = renderWithProvidersUi(
+      <ZigButtonGroupInput value={mainValue} options={fiveTestOptions} onChange={() => {}} />,
+    );
+    testOptions.forEach(({ id }) => {
+      const button = container.querySelector(`#${id}`) as Element;
+      expect(button).toBeVisible();
     });
   });
 
-  describe("ZigButtonGroup actions", () => {
-    it("changes active value", async () => {
-      const { container, rerender } = renderWithProvidersUi(
+  it("should change active value on click", async () => {
+    const { container, rerender } = renderWithProvidersUi(
+      <ZigButtonGroupInput
+        value={mainValue}
+        options={testOptions}
+        onChange={(v) => {
+          mainValue = v;
+        }}
+      />,
+    );
+
+    const button1 = container.querySelector("#test-button1") as Element;
+    const button2 = container.querySelector("#test-button2") as Element;
+    expect(button1).toHaveClass("MuiButton-active");
+    expect(button2).not.toHaveClass("MuiButton-active");
+    fireEvent.click(button2);
+    await waitFor(() => {
+      rerender(
         <ZigButtonGroupInput
           value={mainValue}
           options={testOptions}
@@ -80,27 +94,10 @@ describe("components/inputs/ZigButtonGroupInput", () => {
           }}
         />,
       );
-
-      const button1 = container.querySelector("#test-button1") as Element;
-      const button2 = container.querySelector("#test-button2") as Element;
-      expect(button1).toHaveClass("MuiButton-active");
-      expect(button2).not.toHaveClass("MuiButton-active");
-      fireEvent.click(button2);
-      await waitFor(() => {
-        rerender(
-          <ZigButtonGroupInput
-            value={mainValue}
-            options={testOptions}
-            onChange={(v) => {
-              mainValue = v;
-            }}
-          />,
-        );
-        const updatedButton1 = container.querySelector("#test-button1") as Element;
-        const updatedButton2 = container.querySelector("#test-button2") as Element;
-        expect(updatedButton2).toHaveClass("MuiButton-active");
-        expect(updatedButton1).not.toHaveClass("MuiButton-active");
-      });
+      const updatedButton1 = container.querySelector("#test-button1") as Element;
+      const updatedButton2 = container.querySelector("#test-button2") as Element;
+      expect(updatedButton2).toHaveClass("MuiButton-active");
+      expect(updatedButton1).not.toHaveClass("MuiButton-active");
     });
   });
 });
