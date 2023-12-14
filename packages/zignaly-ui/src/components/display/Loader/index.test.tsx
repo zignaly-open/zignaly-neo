42c397e5890da34ed92808bf6b5df0af12eaa742
Nikita <nikitichsn2@gmail.com>
2023-12-14 15:24:12 +0300

    tests: remove unnecessary describes

--- packages/zignaly-ui/src/components/display/Loader/index.test.tsx
+++ packages/zignaly-ui/src/components/display/Loader/index.test.tsx
@@ -3,37 +3,33 @@ import { Loader, CenteredLoader } from "./index";
 import { renderWithProvidersUi } from "../../../utils/testConfig";
 
 describe("components/display/Loader", () => {
-  describe("Loader", () => {
-    it("should render with default props without crashing", () => {
-      const { container } = renderWithProvidersUi(<Loader />);
+  it("should render with default props without crashing", () => {
+    const { container } = renderWithProvidersUi(<Loader />);
 
-      const loader = container.querySelector(".simple-loader") as Element;
-      expect(loader).toBeVisible();
-      const loaderStyles = getComputedStyle(loader);
-      expect(loader).toMatchSnapshot();
-      expect(loaderStyles).toMatchSnapshot();
-    });
-
-    it("should render with custom width and height without crashing", () => {
-      const { container } = renderWithProvidersUi(<Loader width={50} height={50} />);
-
-      const loader = container.querySelector(".simple-loader") as Element;
-      expect(loader).toBeVisible();
-      const loaderStyles = getComputedStyle(loader);
-      expect(loader).toMatchSnapshot();
-      expect(loaderStyles).toMatchSnapshot();
-    });
+    const loader = container.querySelector(".simple-loader") as Element;
+    expect(loader).toBeVisible();
+    const loaderStyles = getComputedStyle(loader);
+    expect(loader).toMatchSnapshot();
+    expect(loaderStyles).toMatchSnapshot();
   });
 
-  describe("CenteredLoader", () => {
-    it("should render with default props without crashing", () => {
-      const { container } = renderWithProvidersUi(<CenteredLoader />);
+  it("should render with custom width and height without crashing", () => {
+    const { container } = renderWithProvidersUi(<Loader width={50} height={50} />);
 
-      const loader = container.querySelector(".centered-loader-wrapper") as Element;
-      expect(loader).toBeVisible();
-      const loaderStyles = getComputedStyle(loader);
-      expect(loader).toMatchSnapshot();
-      expect(loaderStyles).toMatchSnapshot();
-    });
+    const loader = container.querySelector(".simple-loader") as Element;
+    expect(loader).toBeVisible();
+    const loaderStyles = getComputedStyle(loader);
+    expect(loader).toMatchSnapshot();
+    expect(loaderStyles).toMatchSnapshot();
+  });
+
+  it("should render with default props without crashing", () => {
+    const { container } = renderWithProvidersUi(<CenteredLoader />);
+
+    const loader = container.querySelector(".centered-loader-wrapper") as Element;
+    expect(loader).toBeVisible();
+    const loaderStyles = getComputedStyle(loader);
+    expect(loader).toMatchSnapshot();
+    expect(loaderStyles).toMatchSnapshot();
   });
 });
