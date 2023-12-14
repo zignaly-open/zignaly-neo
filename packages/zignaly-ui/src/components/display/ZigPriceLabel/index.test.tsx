fb4f236b568cf72e6636f6501849884412da5165
Nikita <nikitichsn2@gmail.com>
2023-12-14 15:27:24 +0300

    tests: remove unnecessary describes

--- packages/zignaly-ui/src/components/display/ZigPriceLabel/index.test.tsx
+++ packages/zignaly-ui/src/components/display/ZigPriceLabel/index.test.tsx
@@ -3,31 +3,27 @@ import ZigPriceLabel, { ZigTablePriceLabel } from "./index";
 import { renderWithProvidersUi } from "../../../utils/testConfig";
 
 describe("components/display/ZigPriceLabel", () => {
-  describe("ZigPriceLabel", () => {
-    it("should render without crashing", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigPriceLabel id={"test-id"} value={500} coin={"usdt"} />,
-      );
+  it("should render ZigPriceLabel without crashing", async () => {
+    const { container } = renderWithProvidersUi(
+      <ZigPriceLabel id={"test-id"} value={500} coin={"usdt"} />,
+    );
 
-      const priceLabel = container.querySelector("#test-id") as Element;
-      expect(priceLabel).toBeVisible();
-      const priceLabelStyles = getComputedStyle(priceLabel);
-      expect(priceLabel).toMatchSnapshot();
-      expect(priceLabelStyles).toMatchSnapshot();
-    });
+    const priceLabel = container.querySelector("#test-id") as Element;
+    expect(priceLabel).toBeVisible();
+    const priceLabelStyles = getComputedStyle(priceLabel);
+    expect(priceLabel).toMatchSnapshot();
+    expect(priceLabelStyles).toMatchSnapshot();
   });
 
-  describe("ZigTablePriceLabel", () => {
-    it("should render without crashing", async () => {
-      const { container } = renderWithProvidersUi(
-        <ZigTablePriceLabel id={"test-id"} value={500} coin={"btc"} />,
-      );
+  it("should render ZigTablePriceLabel without crashing", async () => {
+    const { container } = renderWithProvidersUi(
+      <ZigTablePriceLabel id={"test-id"} value={500} coin={"btc"} />,
+    );
 
-      const priceLabel = container.querySelector("#test-id") as Element;
-      expect(priceLabel).toBeVisible();
-      const priceLabelStyles = getComputedStyle(priceLabel);
-      expect(priceLabel).toMatchSnapshot();
-      expect(priceLabelStyles).toMatchSnapshot();
-    });
+    const priceLabel = container.querySelector("#test-id") as Element;
+    expect(priceLabel).toBeVisible();
+    const priceLabelStyles = getComputedStyle(priceLabel);
+    expect(priceLabel).toMatchSnapshot();
+    expect(priceLabelStyles).toMatchSnapshot();
   });
 });
