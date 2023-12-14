81a7ce9eaafd88b2fb031f58ce1d1a2053508f93
Nikita <nikitichsn2@gmail.com>
2023-12-14 13:16:04 +0300

    tests: remove unnecessary describes

--- packages/zignaly-ui/src/components/inputs/ZigLink/index.test.tsx
+++ packages/zignaly-ui/src/components/inputs/ZigLink/index.test.tsx
@@ -4,30 +4,26 @@ import { renderWithProvidersUi } from "../../../utils/testConfig";
 import { fireEvent, waitFor } from "@testing-library/react";
 
 describe("components/inputs/ZigLink", () => {
-  describe("rendering", () => {
-    it("renders with default props", async () => {
-      const { container } = renderWithProvidersUi(<ZigLink href={"#"} id={"link-test"} />);
+  it("renders with default props", async () => {
+    const { container } = renderWithProvidersUi(<ZigLink href={"#"} id={"link-test"} />);
 
-      const link = container.querySelector("#link-test") as Element;
-      expect(link).toBeVisible();
-      const linkStyles = getComputedStyle(link);
-      expect(link).toMatchSnapshot();
-      expect(linkStyles).toMatchSnapshot();
-    });
+    const link = container.querySelector("#link-test") as Element;
+    expect(link).toBeVisible();
+    const linkStyles = getComputedStyle(link);
+    expect(link).toMatchSnapshot();
+    expect(linkStyles).toMatchSnapshot();
   });
 
-  describe("actions", () => {
-    it("should handle click", async () => {
-      const handleClick = jest.fn();
-      const { container } = renderWithProvidersUi(
-        <ZigLink href={"#"} id={"link-test"} onClick={handleClick} />,
-      );
+  it("should handle click", async () => {
+    const handleClick = jest.fn();
+    const { container } = renderWithProvidersUi(
+      <ZigLink href={"#"} id={"link-test"} onClick={handleClick} />,
+    );
 
-      const link = container.querySelector("#link-test") as Element;
-      fireEvent.click(link);
-      await waitFor(() => {
-        expect(handleClick).toHaveBeenCalledTimes(1);
-      });
+    const link = container.querySelector("#link-test") as Element;
+    fireEvent.click(link);
+    await waitFor(() => {
+      expect(handleClick).toHaveBeenCalledTimes(1);
     });
   });
 });
