c698daccb69f5046ba96c8813b39a616662aef93
Nikita <nikitichsn2@gmail.com>
2023-12-18 14:41:48 +0300

    tests: test names

--- packages/zignaly-ui/src/components/inputs/ZigSwitch/index.test.tsx
+++ packages/zignaly-ui/src/components/inputs/ZigSwitch/index.test.tsx
@@ -3,7 +3,7 @@ import { renderWithProvidersUi } from "../../../utils/testConfig";
 import { fireEvent, screen, waitFor } from "@testing-library/react";
 import ZigSwitch from ".";
 
-describe("components/ZigSwitch", () => {
+describe("components/inputs/ZigSwitch", () => {
   it("should render without crashing", () => {
     const { container } = renderWithProvidersUi(<ZigSwitch />);
     const switchContainer = container.querySelector(".MuiSwitch-root") as HTMLElement;
