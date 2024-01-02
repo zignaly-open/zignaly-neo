40ab049cc0d1483a6d00ca62751ce2747300bd18
Christophe Wagner <chris.wagn22@gmail.com>
2024-01-02 10:28:35 +0800

    add zscore icon

--- packages/zignaly-ui/src/components/display/ZScore/index.tsx
+++ packages/zignaly-ui/src/components/display/ZScore/index.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { Chip } from "@mui/material";
 import { ZScoreProps } from "./types";
+import { ZScoreIcon } from "icons";
+
+const sx = {
+  "&": {
+    gap: "6px",
+    padding: "0 16px",
+    borderRadius: "18px",
+    backgroundImage: "linear-gradient(to top, #3e3177, #263e6f)",
+    height: "36px",
+  },
+  "&&> *": {
+    margin: 0,
+    padding: 0,
+  },
+};
 
 const ZScore = ({ value, ...rest }: ZScoreProps) => {
-  return <Chip label={Math.round(value)} {...rest} />;
+  return <Chip sx={sx} icon={<ZScoreIcon />} label={Math.round(value)} {...rest} />;
 };
 export default ZScore;
