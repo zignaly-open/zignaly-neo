9055bef5c881411733008e20b1f12fcfdbd31af1
Christophe Wagner <chris.wagn22@gmail.com>
2024-01-08 09:12:07 +0800

    fix: fix zscore color

--- packages/zignaly-ui/src/components/display/ZScore/index.tsx
+++ packages/zignaly-ui/src/components/display/ZScore/index.tsx
@@ -1,9 +1,10 @@
 import React, { useMemo } from "react";
-import { Chip } from "@mui/material";
+import { Chip, useTheme } from "@mui/material";
 import { ZScoreProps } from "./types";
 import { ZScoreIcon } from "icons";
 
 const ZScore = ({ value, mini = false, ...rest }: ZScoreProps) => {
+  const theme = useTheme();
   const sx = useMemo(
     () => ({
       "&": {
@@ -17,8 +18,12 @@ const ZScore = ({ value, mini = false, ...rest }: ZScoreProps) => {
         margin: 0,
         padding: 0,
       },
+      "& .MuiChip-label": {
+        fontSize: `${16}px`,
+        color: theme.palette.neutral000,
+      },
     }),
-    [mini],
+    [mini, theme],
   );
 
   return (
