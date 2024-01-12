7bdeff9aa10b080028169cf425ba675c7fd15047
Nikita <nikitichsn2@gmail.com>
2024-01-12 13:52:25 +0300

    tests: add tests global ZigTable

---
+++ packages/zignaly-ui/src/components/display/ZigTable/index.test.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import ZigTable from ".";
+import { renderWithProvidersUi } from "../../../utils/testConfig";
+import { createColumnHelper } from "../../../index";
+
+const testData = [
+  { id: 1, name: "Ethereum", symbol: "ETH", price: 800000 },
+  { id: 2, name: "Bitcoin", symbol: "BTC", price: 5000 },
+];
+
+const columnHelper = createColumnHelper<{
+  id: number;
+  name: string;
+  symbol: string;
+  price: number;
+}>();
+const columns = [
+  columnHelper.accessor("name", {
+    header: "Name",
+    cell: ({ getValue }) => getValue(),
+  }),
+  columnHelper.accessor("symbol", {
+    header: "Symbol",
+    cell: ({ getValue }) => getValue(),
+  }),
+  columnHelper.accessor("price", {
+    header: "Price",
+    cell: ({ getValue }) => getValue(),
+  }),
+];
+
+describe("components/display/ZigTable", () => {
+  it("should render ZigTableData without crashing", () => {
+    const { container } = renderWithProvidersUi(
+      <ZigTable data={testData} columns={columns} prefixId={"test"} />,
+    );
+    const tableElement = container.querySelector("#test__table");
+    expect(tableElement).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render ZigTableRtkFiniteQuery without crashing", () => {
+    const useQuery = jest.fn();
+    const { container } = renderWithProvidersUi(
+      <ZigTable useQuery={useQuery} columns={columns} prefixId={"test"} />,
+    );
+    const tableElement = container.querySelector("#test__table");
+    expect(tableElement).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render ZigTableRtkInfiniteQuery without crashing", () => {
+    const useQuery = jest.fn();
+    const { container } = renderWithProvidersUi(
+      <ZigTable useInfiniteQuery={useQuery} data={testData} columns={columns} prefixId={"test"} />,
+    );
+    const tableElement = container.querySelector("#test__table");
+    expect(tableElement).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
