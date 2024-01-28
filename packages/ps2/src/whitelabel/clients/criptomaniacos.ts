6ca7baea4ddeac44410aeb20e41bf09e59858fbe
Alexander Mikhalchenko <alex@xfuturum.com>
2024-01-28 23:32:01 +0300

    feat(WlServer): local configs for CM ans Zignaly
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/whitelabel/clients/criptomaniacos.ts
+++ packages/ps2/src/whitelabel/clients/criptomaniacos.ts
@@ -1,5 +1,6 @@
 import { Features, WhitelabelOverride } from '../type';
 
+/*
 export default {
   title: 'Lastra',
   name: 'Lastra',
@@ -159,5 +160,160 @@ export default {
   `,
   tools: {
     google_tag_manager: 'GTM-5JS4ZRXZ',
+  },
+} as WhitelabelOverride;
+*/
+
+export default {
+  slug: 'criptomaniacos',
+  name: 'Lastra',
+  domain: 'use.lastra.app',
+  zignalySuccessFee: 10,
+  title: 'Lastra - Your last Crypto app',
+  description: 'A global platform to invest in winning crypto portfolios.',
+  social: {
+    twitter: 'https://cmania.co/zig-xtwitter',
+    youtube: 'https://cmania.co/zig-yt',
+    linkedin: 'https://cmania.co/zig-linkedin',
+    telegram: 'https://cmania.co/zig-tg',
+    instagram: 'https://cmania.co/zig-ig',
+  },
+  supportHelpCenter:
+    'https://api.whatsapp.com/send/?text=Ol%C3%A1!%20Gostaria%20de%20conversar%20com%20o%20time%20de%20atendimento.&phone=554888387787',
+  tools: {
+    google_tag_manager: 'GTM-5JS4ZRXZ',
+  },
+  minInvestment: {
+    BNB: 0,
+    BTC: 0,
+    ETH: 0,
+    BUSD: 0,
+    USDT: 0,
+  },
+  marketplaceMinScore: 0,
+  locales: ['en', 'pt'],
+  logo: 'https://imagedelivery.net/qNg0fDlw9b2DximxcnB4cA/225397a6-b05c-42c2-69f3-ca46141f5d00/public',
+  favicon:
+    'https://imagedelivery.net/qNg0fDlw9b2DximxcnB4cA/423b16d8-3abe-4813-bc80-7cabf4fa8600/public',
+  imageDeliveryImages: {
+    logo: 'https://imagedelivery.net/qNg0fDlw9b2DximxcnB4cA/225397a6-b05c-42c2-69f3-ca46141f5d00',
+    favicon:
+      'https://imagedelivery.net/qNg0fDlw9b2DximxcnB4cA/423b16d8-3abe-4813-bc80-7cabf4fa8600',
+    banner:
+      'https://imagedelivery.net/qNg0fDlw9b2DximxcnB4cA/7b4644c8-b06c-4698-f196-865302f51400',
+  },
+  baseTheme: 'dark',
+  themeOverrides: {
+    chart: {
+      green: '#1298C9',
+      greenCard: [
+        'rgba(16, 33, 33, 0.68)',
+        'rgba(0, 45, 20, 0.44)',
+        'rgba(7, 47, 30, 0.44)',
+        'rgba(56, 235, 170, 0.69)',
+      ],
+      greenGradient: ['rgba(18, 33, 59, 0.52)', 'rgba(33, 70, 78, 0.69)'],
+      greenMiniGradient: [
+        'rgba(17, 27, 47, 0)',
+        'rgba(22, 41, 67, 0.5)',
+        'rgba(39, 110, 117, 1)',
+      ],
+    },
+    palette: {
+      links: '#1298C9',
+      secondary: '#515AAC',
+      neutral175: '#6aaac2',
+      neutral500: '#4A4958',
+      neutral600: '#353337',
+      neutral700: '#252329',
+      neutral750: '#1b1b1b',
+      neutral800: '#121512',
+      neutral900: '#090909',
+      highlighted: '#1298C9',
+    },
+    boxShadows: {
+      tableHeader: 'rgba(0,0,0,.5)',
+    },
+    fontFamily: ['DM Sans', 'Helvetica', 'Arial', 'sans-serif'],
+    backgrounds: {
+      input:
+        'linear-gradient(90deg, rgb(16 18 18) 0%, rgb(16 18 17) 35%, rgb(16 18 17) 100%)',
+      modal: '#161717',
+      header: '#161717',
+      input2fa: '#0f1124',
+      tableRow: '#171A1B',
+      activeTab: '#181B2F',
+      tableHeader: '#161717',
+      buttonPrimary:
+        'linear-gradient(108deg, #FF9EF9 -6.8%, #6A00FF 37.46%, #13BFFF 93.24%)',
+      investorsIcon: '#757581',
+      selectInputFill: '#171A1B',
+      dropdown2ndLevel: 'rgb(25, 26, 28)',
+      input2faGradient: 'linear-gradient(101deg, #3f3bb1 7%, #138ea0 94%)',
+      headerMenuItemHover: '#6A00FF',
+      secondaryBackground: '#121212',
+      withdrawalHighlight: '#161717',
+      input2faGradientBorder:
+        'linear-gradient(101deg, #3f3bb1 7%, #138ea0 94%)',
+      manageServiceMenuHover: '#1f1f1f',
+      body: '#0D0E0E',
+    },
+    fontFamilyH1H6: [
+      'Roboto Condensed',
+      'Roboto',
+      'Helvetica',
+      'Arial',
+      'sans-serif',
+    ],
+  },
+  featureOverrides: {
+    [Features.Referrals]: false,
+    [Features.CreateService]: false,
+    [Features.AccessLevels]: false,
+    [Features.Rewards]: false,
+    [Features.NewSignup]: false,
+    [Features.Subscriptions]: false,
+    [Features.Kyc]: false,
+    [Features.LoginOnlyAccess]: false,
+  },
+  translationOverrides: {
+    en: {
+      common: {
+        'navigation-menu': {
+          'profit-sharing': 'Explore the best investment strategies',
+        },
+        'main-menu': {
+          'dropdown-link-helpDocs': 'Support',
+        },
+      },
+      marketplace: {
+        'invest-in-services': 'Invest in the best strategies',
+        'invest-in-services-explainer':
+          'Discover the new investment model from Lastra, where you only pay when you make profit and need $1 to start.',
+      },
+    },
+    pt: {
+      common: {
+        'navigation-menu': {
+          'profit-sharing': 'Procurar Carteira',
+        },
+        'main-menu': {
+          'dropdown-link-helpDocs': 'Suporte',
+        },
+      },
+      marketplace: {
+        'invest-in-services': 'Invista nas Melhores Estratégias',
+        'invest-in-services-explainer':
+          'Conheça as carteiras da Lastra, em novo formato que você paga somente quando lucra! Comece hoje com apenas $1!',
+      },
+    },
+  },
+  links: {
+    tos: 'https://www.lastra.app/termos',
+    privacyPolicy: 'https://www.lastra.app/politica-privacidade/',
+    mainAppLink: 'https://use.lastra.app/my-portfolio',
+    helpUrl:
+      'https://api.whatsapp.com/send/?text=Ol%C3%A1!%20Gostaria%20de%20conversar%20com%20o%20time%20de%20atendimento.&phone=554888387787',
+    subscriptionPurchaseLink: null,
   },
 } as WhitelabelOverride;
--- packages/ps2/src/whitelabel/clients/zignaly.ts
+++ packages/ps2/src/whitelabel/clients/zignaly.ts
@@ -1,51 +1,57 @@
-import { WhitelabelOverride } from '../type';
+import { Features, WhitelabelOverride } from '../type';
 
 export default {
-  title: 'Zignaly',
+  slug: 'z01',
   name: 'Zignaly',
   domain: 'app.zignaly.com',
-  social: {
-    twitter: 'https://twitter.com/zignaly',
-    telegram: 'https://t.me/ZignalyHQ',
-    discord: 'https://discord.gg/9H6cEa9uRN',
-    medium: 'https://medium.com/zignaly',
-    linkedin: 'https://www.linkedin.com/company/zignaly/',
-  },
-  links: {
-    helpUrl: 'https://help.zignaly.com/hc/en-us',
-  },
-  mainAppLink: 'https://zignaly.com',
-  headContent: `
-    <title>Zignaly</title>
-    <link rel="shortcut icon" type="image/png" sizes="16x16" href="/images/whitelabel/zignaly/favicon-16x16.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/images/whitelabel/zignaly/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/images/whitelabel/zignaly/favicon-16x16.png">
-    <link rel="apple-touch-icon" href="/images/whitelabel/zignaly/logo192.png"/>
-    <link rel="manifest" href="/manifests/zignaly.json"/>
-    
-    <!-- Facebook Meta Tags -->
-    <meta property="og:url" content="https://app.zignaly.com/profit-sharing">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="Discover the best traders on Zignaly">
-    <meta property="og:description" content="Discover the best traders available to invest through Zignaly's Profit Sharing">
-    <meta property="og:image" content="https://app.zignaly.com/images/zignaly-social.png">
-    <meta property="og:image:width" content="1200"/>
-    <meta property="og:image:height" content="630"/>
-    <meta property="og:image:alt" content="zignaly.com"/>
-
-    <!-- Twitter Meta Tags -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta property="twitter:domain" content="zignaly.com">
-    <meta property="twitter:url" content="https://app.zignaly.com/profit-sharing">
-    <meta name="twitter:title" content="Discover the best traders on Zignaly">
-    <meta name="twitter:description" content="Discover the best traders available to invest through Zignaly's Profit Sharing">
-    <meta name="twitter:image" content="https://app.zignaly.com/images/zignaly-social.png">
-    <meta name="twitter:site" content="@zignaly">
-  `,
   zignalySuccessFee: 5,
+  title: 'Discover the best traders on Zignaly',
+  description:
+    "Discover the best traders available to invest through Zignaly's Profit Sharing",
+  social: {
+    medium: 'https://medium.com/zignaly',
+    discord: 'https://discord.com/invite/9H6cEa9uRN',
+    twitter: 'https://twitter.com/zignaly',
+    linkedin: 'https://www.linkedin.com/company/zignaly',
+    telegram: 'https://t.me/ZignalyHQ',
+  },
+  supportHelpCenter: 'https://help.zignaly.com/en/',
   tools: {
-    twitter_tracker: 'og0cu',
     intercom: 'nc3z6oqm',
+    twitter_tracker: 'og0cu',
     google_tag_manager: 'GTM-5ZQ4JLK',
+  },
+  minInvestment: { BNB: 0, BTC: 0, ETH: 0, BUSD: 0, USDT: 0 },
+  marketplaceMinScore: 0,
+  locales: ['en', 'es', 'tr'],
+  logo: 'https://imagedelivery.net/qNg0fDlw9b2DximxcnB4cA/30c2f30a-5bea-43f9-63c8-29506dfab200/public',
+  favicon:
+    'https://imagedelivery.net/qNg0fDlw9b2DximxcnB4cA/a482eb98-06a3-423a-cf4c-e5c201c70e00/public',
+  imageDeliveryImages: {
+    logo: 'https://imagedelivery.net/qNg0fDlw9b2DximxcnB4cA/30c2f30a-5bea-43f9-63c8-29506dfab200',
+    favicon:
+      'https://imagedelivery.net/qNg0fDlw9b2DximxcnB4cA/a482eb98-06a3-423a-cf4c-e5c201c70e00',
+    banner:
+      'https://imagedelivery.net/qNg0fDlw9b2DximxcnB4cA/6999ba83-dad6-4bd1-0f68-49eee45a6d00',
+  },
+  baseTheme: 'dark',
+  featureOverrides: {
+    [Features.Referrals]: true,
+    [Features.Rewards]: true,
+    [Features.CreateService]: true,
+    [Features.NewSignup]: true,
+    [Features.LoginOnlyAccess]: false,
+    [Features.Kyc]: false,
+    [Features.ZScore]: true,
+    [Features.AccessLevels]: false,
+    [Features.Subscriptions]: false,
+  },
+  translationOverrides: null,
+  links: {
+    tos: 'https://zignaly.com/legal/terms',
+    privacyPolicy: 'https://zignaly.com/legal/privacy',
+    mainAppLink: 'https://zignaly.com',
+    helpUrl: 'https://help.zignaly.com/en/',
+    subscriptionPurchaseLink: null,
   },
 } as WhitelabelOverride;
