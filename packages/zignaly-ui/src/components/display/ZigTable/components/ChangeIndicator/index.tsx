a04ff4faf01adcba5b34730e25f2e0096f822a8a
Christophe Wagner <chris.wagn22@gmail.com>
2024-01-29 18:53:37 +0800

    fix: fix decimals

--- packages/zignaly-ui/src/components/display/ZigTable/components/ChangeIndicator/index.tsx
+++ packages/zignaly-ui/src/components/display/ZigTable/components/ChangeIndicator/index.tsx
@@ -9,22 +9,21 @@ import ZigTypography from "../../../ZigTypography";
 import { isNil } from "lodash-es";
 
 // Fix 1 decimal issue making 1.95 until 1.99 to be 2.0 instead of 2
+// -0.95 until -0.99 to be -1.0 instead of -1
 // https://github.com/s-yadav/react-number-format/issues/820
-const checkDecimals = (value: number, decimals: number) => {
-  const decimalPart = value % 1;
+const adjustDecimals = (value: BigNumber, decimals: number) => {
+  const decimalPart = value.abs().mod(1);
 
-  if (decimals == 1) {
-    if ((value > 0 && decimalPart > 0.95) || (value < 0 && decimalPart < 0.95)) return 0;
-  }
+  if (decimals == 1 && decimalPart.gt(0.95)) return 0;
 
   return decimals;
 };
 
 // Fix -0.01 until -0.05 to be -0.0 instead of 0
 // Also remove negative sign when hideNegativeSign is true, used when showing the indicator arrow instead
-const adjustNumber = (value: number, decimals: number, hideNegativeSign?: boolean) => {
-  if (hideNegativeSign) return Math.abs(value);
-  if (decimals === 1 && value < 0 && value >= -0.05) return 0;
+const adjustNumber = (value: BigNumber, decimals: number, hideNegativeSign?: boolean) => {
+  if (hideNegativeSign) return value.abs();
+  if (decimals === 1 && value.lt(0) && value.gte(-0.05)) return 0;
   return value;
 };
 
@@ -63,11 +62,11 @@ const ChangeIndicator = ({
         >
           <NumericFormat
             style={style}
-            value={adjustNumber(+bigNumberValue, decimalScale, hideNegativeSign).toFixed()}
+            value={adjustNumber(bigNumberValue, decimalScale, hideNegativeSign).toFixed()}
             displayType={"text"}
             suffix={type === "only_number" || smallPct ? "" : "%"}
             thousandSeparator={","}
-            decimalScale={checkDecimals(+bigNumberValue, decimalScale)}
+            decimalScale={adjustDecimals(bigNumberValue, decimalScale)}
             fixedDecimalScale={stableCoinOperative}
             id={id}
           />
