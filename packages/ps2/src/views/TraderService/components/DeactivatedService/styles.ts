cb8d2f487ddde46242036b50ff1af38ba3bad546
Nikita Shybeka <nikitichsn2@gmail.com>
2024-02-09 13:47:59 +0300

    fix: warning message style deactivated service

--- packages/ps2/src/views/TraderService/components/DeactivatedService/styles.ts
+++ packages/ps2/src/views/TraderService/components/DeactivatedService/styles.ts
@@ -2,7 +2,10 @@ import { Box, styled } from '@mui/material';
 
 export const ErrorWrapper = styled(Box)`
   background-color: ${({ theme }) => theme.palette.backgrounds.toastError};
-  padding: 15px 15px;
+  margin: 30px auto;
+  padding: 15px 30px;
+  box-shadow: 4px 6px 4px ${({ theme }) => theme.palette.neutral900}44;
+  width: fit-content;
   border-radius: 5px;
   border: 1px solid ${({ theme }) => theme.palette.backgrounds.toastError};
 `;
