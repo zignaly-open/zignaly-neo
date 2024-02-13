c9381319991199ef3e1432d6eb260114c60085d9
Christophe Wagner <chris.wagn22@gmail.com>
2024-02-13 09:37:00 +0800

    fix: fix padding

--- packages/ps2/src/views/TraderService/components/ServiceProfileContainer/atoms/ServiceSummary.tsx
+++ packages/ps2/src/views/TraderService/components/ServiceProfileContainer/atoms/ServiceSummary.tsx
@@ -19,7 +19,7 @@ const ServiceSummary: React.FC<{ service: Service }> = ({ service }) => {
     <Box>
       {sm && (
         <>
-          <GridWithBottomBorder container>
+          <GridWithBottomBorder container pb={2.5}>
             <GridCell item xs={6}>
               <ZigTypography
                 color={'neutral300'}
