c70c62bf485b9e97e86fd76c43c5292146f28316
Nikita Shybeka <nikitichsn2@gmail.com>
2024-02-18 13:14:48 +0300

    fix: add custom slate types

--- packages/ps2/src/views/TraderService/components/EditServiceProfileContainer/atoms/RichDescriptionEditor/index.tsx
+++ packages/ps2/src/views/TraderService/components/EditServiceProfileContainer/atoms/RichDescriptionEditor/index.tsx
@@ -132,31 +132,28 @@ const RichDescriptionEditor = ({
     queueMicrotask(() => {
       const pendingDiffs = ReactEditor.androidPendingDiffs(editor);
 
-      const scheduleFlush = pendingDiffs?.some(
-        ({ diff, path }: { diff: any; path: any }) => {
-          if (!diff.text.endsWith(' ')) {
-            return false;
-          }
+      const scheduleFlush = pendingDiffs?.some(({ diff, path }) => {
+        if (!diff.text.endsWith(' ')) {
+          return false;
+        }
 
-          const { text } = SlateNode.leaf(editor, path);
-          const beforeText = text.slice(0, diff.start) + diff.text.slice(0, -1);
-          if (!(beforeText in SHORTCUTS)) {
-            return;
-          }
+        const { text } = SlateNode.leaf(editor, path);
+        const beforeText = text.slice(0, diff.start) + diff.text.slice(0, -1);
+        if (!(beforeText in SHORTCUTS)) {
+          return;
+        }
 
-          const blockEntry = Editor.above(editor, {
-            at: path,
-            match: (n) =>
-              SlateElement.isElement(n) && Editor.isBlock(editor, n),
-          });
-          if (!blockEntry) {
-            return false;
-          }
+        const blockEntry = Editor.above(editor, {
+          at: path,
+          match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n),
+        });
+        if (!blockEntry) {
+          return false;
+        }
 
-          const [, blockPath] = blockEntry;
-          return Editor.isStart(editor, Editor.start(editor, path), blockPath);
-        },
-      );
+        const [, blockPath] = blockEntry;
+        return Editor.isStart(editor, Editor.start(editor, path), blockPath);
+      });
 
       if (scheduleFlush) {
         ReactEditor.androidScheduleFlush(editor);
