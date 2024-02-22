5bb442d10bd2e9d50ca79a5af55cf8198f649db7
Alexander Mikhalchenko <alex@xfuturum.com>
2024-02-22 15:53:10 +0300

    feat(ZignalyUIi18n): fixed broken ci and a broken zignaly ui lol
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/i18n.ts
+++ packages/zignaly-ui/src/i18n.ts
@@ -1,16 +1,1 @@
-import en from "./i18n/static/en/en.json";
-import es from "./i18n/static/es.json";
-import tr from "./i18n/static/tr.json";
-import vi from "./i18n/static/vi.json";
-import ru from "./i18n/static/ru.json";
-import { i18n } from "i18next";
-
-export function addZignalyUiResources(i18nInstance: i18n) {
-  i18nInstance.on("loaded", () => {
-    i18nInstance.addResourceBundle("en", "zignaly-ui", en);
-    i18nInstance.addResourceBundle("es", "zignaly-ui", es);
-    i18nInstance.addResourceBundle("tr", "zignaly-ui", tr);
-    i18nInstance.addResourceBundle("vi", "zignaly-ui", vi);
-    i18nInstance.addResourceBundle("ru", "zignaly-ui", ru);
-  });
-}
+export { addZignalyUiResources } from "./i18n/addZignalyUiResources";
