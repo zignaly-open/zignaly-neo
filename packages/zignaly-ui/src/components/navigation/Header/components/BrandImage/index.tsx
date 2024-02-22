889ab75305ad5d5f6f7e814f1d531d38ed62306e
Nikita Shybeka <nikitichsn2@gmail.com>
2024-02-22 14:59:41 +0300

    chore: remove deprecated BrandImage

--- packages/zignaly-ui/src/components/navigation/Header/components/BrandImage/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { LogoProps, iconsByType } from "./types";
-
-// TODO: fix types to logo and iso and add default variables
-export default function BrandImage({ type, width, height, id }: LogoProps) {
-  const Icon = iconsByType[type];
-  return Icon ? <Icon width={width} height={height} id={id} /> : null;
-}
