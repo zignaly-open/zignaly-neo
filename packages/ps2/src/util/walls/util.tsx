516d8b152b26f0a4fbaa76b4b564aeda273a6782
Alexander Mikhalchenko <alex@xfuturum.com>
2024-03-01 19:36:19 +0300

    fix(KYC): let users invest out
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/ps2/src/util/walls/util.tsx
+++ packages/ps2/src/util/walls/util.tsx
@@ -192,7 +192,7 @@ export const useCanInsertCoupon = createUseAccessCheck(Level.NoSubscription);
 export const useCanDeposit = createUseAccessCheck(Level.Normal);
 export const useCanInvestIn = createUseAccessCheck(Level.Normal);
 export const useCanCreateService = createUseAccessCheck(Level.Normal);
-export const useCanInvestOut = createUseAccessCheck(Level.KycExpired);
+export const useCanInvestOut = createUseAccessCheck(Level.KycPending);
 export const useCanWithdraw = createUseAccessCheck(Level.KycPending);
 
 export const withModalRequiresAuthentication = <
