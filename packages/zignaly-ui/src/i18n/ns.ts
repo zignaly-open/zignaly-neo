1fc0c3e7147f5fdc6ccdc4bf66f92c8d492fc977
Alexander Mikhalchenko <alex@xfuturum.com>
2024-03-01 16:48:07 +0300

    fix(ZignalyUIi18n): test locale for zignaly ui
    Signed-off-by: Alexander Mikhalchenko <alex@xfuturum.com>

--- packages/zignaly-ui/src/i18n/ns.ts
+++ packages/zignaly-ui/src/i18n/ns.ts
@@ -3,3 +3,4 @@ export { default as es } from "./static/es/zignaly-ui.json";
 export { default as tr } from "./static/tr/zignaly-ui.json";
 export { default as vi } from "./static/vi/zignaly-ui.json";
 export { default as ru } from "./static/ru/zignaly-ui.json";
+export { default as ch } from "./static/ch/zignaly-ui.json";
