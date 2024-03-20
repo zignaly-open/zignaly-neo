101701540e0a5dd3a3d8ca393106405766657f9b
Christophe Wagner <chris.wagn22@gmail.com>
2024-03-20 14:48:07 +0700

    fix: fix types

--- packages/zignaly-ui/src/components/display/ZigRisk/types.ts
+++ packages/zignaly-ui/src/components/display/ZigRisk/types.ts
@@ -1,6 +1,11 @@
 import { AdditionalProps } from "components/types";
 
-export type ZigRiskProps = AdditionalProps & { value: number; width?: number; height?: number };
+export type ZigRiskProps = AdditionalProps & {
+  value: number;
+  width?: number;
+  height?: number;
+  short?: boolean;
+};
 
 export type ZigRisk = {
   icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
@@ -8,5 +13,4 @@ export type ZigRisk = {
   color: string;
   id: number;
   zrisk: number;
-  short?: boolean;
 };
